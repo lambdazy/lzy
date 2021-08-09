35bddf846098bdbd2f64e8b7fe946feef15bd528
tomato <tomato@yandex-team.ru>
2021-08-09 11:46:24 +0300

    lzy: fix basic fuse test

--- src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyStartupTests.java
+++ src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyStartupTests.java
@@ -3,7 +3,6 @@ package ru.yandex.cloud.ml.platform.lzy;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
@@ -16,8 +15,8 @@ public class LzyStartupTests extends LzyBaseTest {
         final String lzyPath = "/tmp/lzy";
         startTerminalAtPath(lzyPath);
         Assert.assertTrue(waitForFuse(lzyPath, DEFAULT_FUSE_TIMEOUT_SEC, TimeUnit.SECONDS));
-        Assert.assertTrue(Files.exists(Paths.get(lzyPath + File.separator + "bin")));
-        Assert.assertTrue(Files.exists(Paths.get(lzyPath + File.separator + "sbin")));
-        Assert.assertTrue(Files.exists(Paths.get(lzyPath + File.separator + "slots")));
+        Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/bin")));
+        Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/sbin")));
+        Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/dev")));
     }
 }
