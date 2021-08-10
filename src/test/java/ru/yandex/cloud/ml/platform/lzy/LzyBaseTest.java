5dfacbfd9f907f3735b1d86d2e2d263493cf40c1
tomato <tomato@yandex-team.ru>
2021-08-10 13:08:35 +0300

    lzy: more tests

--- src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyBaseTest.java
+++ src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyBaseTest.java
@@ -1,39 +1,50 @@
 package ru.yandex.cloud.ml.platform.lzy;
 
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import org.junit.After;
 import org.junit.Before;
 import ru.yandex.cloud.ml.platform.lzy.servant.LzyServant;
 import ru.yandex.cloud.ml.platform.lzy.server.LzyServer;
+import yandex.cloud.priv.datasphere.v2.lzy.LzyServerGrpc;
 
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class LzyBaseTest {
     private static final int LZY_SERVER_PORT = 7777;
-    private final List<Process> servantProcesses = new ArrayList<>();
-    protected Server lzyServer;
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    private Server lzyServer;
+    protected LzyServerGrpc.LzyServerBlockingStub lzyServerClient;
+
 
     @Before
     public void setUp() throws Exception {
         lzyServer = ServerBuilder.forPort(LZY_SERVER_PORT).addService(new LzyServer.Impl()).build();
         lzyServer.start();
+
+        final ManagedChannel channel = ManagedChannelBuilder
+            .forAddress("localhost", LZY_SERVER_PORT)
+            .usePlaintext()
+            .build();
+        lzyServerClient = LzyServerGrpc.newBlockingStub(channel);
     }
 
     @After
     public void tearDown() throws InterruptedException {
         lzyServer.shutdown();
         lzyServer.awaitTermination();
-        servantProcesses.forEach(Process::destroy);
+        executorService.shutdown();
     }
 
-    protected void startTerminalAtPath(String path) throws Exception {
+    protected void startTerminalAtPath(String path) {
         final String[] lzyArgs = {
             "terminal",
             "--lzy-address",
@@ -45,17 +56,13 @@ public class LzyBaseTest {
             "--private-key",
             "/tmp/nonexistent-key"
         };
-
-        final List<String> command = new ArrayList<>();
-        command.add(System.getProperty("java.home") + File.separator + "bin" + File.separator + "java");
-        command.add("-cp");
-        command.add(System.getProperty("java.class.path"));
-        command.add(LzyServant.class.getCanonicalName());
-        command.addAll(Arrays.asList(lzyArgs));
-
-        final ProcessBuilder builder = new ProcessBuilder(command);
-        final Process process = builder.inheritIO().start();
-        servantProcesses.add(process);
+        executorService.submit(() -> {
+            try {
+                LzyServant.main(lzyArgs);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     protected boolean waitForFuse(String path, long timeout, TimeUnit unit) throws InterruptedException {
--- src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyStartupTests.java
+++ src/test/java/ru/yandex/cloud/ml/platform/lzy/LzyStartupTests.java
@@ -1,22 +1,100 @@
 package ru.yandex.cloud.ml.platform.lzy;
 
+import io.grpc.StatusRuntimeException;
 import org.junit.Assert;
 import org.junit.Test;
+import yandex.cloud.priv.datasphere.v2.lzy.Lzy;
+import yandex.cloud.priv.datasphere.v2.lzy.Operations;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LzyStartupTests extends LzyBaseTest {
     private static final int DEFAULT_FUSE_TIMEOUT_SEC = 10;
 
     @Test
     public void testFuseWorks() throws Exception {
+        //Arrange
         final String lzyPath = "/tmp/lzy";
+
+        //Act
         startTerminalAtPath(lzyPath);
+
+        //Assert
         Assert.assertTrue(waitForFuse(lzyPath, DEFAULT_FUSE_TIMEOUT_SEC, TimeUnit.SECONDS));
         Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/bin")));
         Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/sbin")));
         Assert.assertTrue(Files.exists(Paths.get(lzyPath + "/dev")));
     }
+
+    @Test
+    public void testRegisteredZygotesAvailable() throws Exception {
+        //Arrange
+        final String lzyPath = "/tmp/lzy";
+        final List<Operations.RegisteredZygote> zygotes = IntStream.range(0, 1000)
+            .mapToObj(value -> lzyServerClient.publish(Lzy.PublishRequest.newBuilder()
+                .setOperation(Operations.Zygote.newBuilder().build())
+                .setName("test_op_" + value)
+                .build()))
+            .collect(Collectors.toList());
+
+        //Act
+        startTerminalAtPath(lzyPath);
+
+        //Assert
+        Assert.assertTrue(waitForFuse(lzyPath + "/bin", DEFAULT_FUSE_TIMEOUT_SEC, TimeUnit.SECONDS));
+        zygotes.forEach(registeredZygote -> Assert.assertTrue(Files.exists(Paths.get(
+            lzyPath + "/bin/" + registeredZygote.getName()))));
+    }
+
+    @Test
+    public void testDuplicateRegister() {
+        //Arrange
+        final String opName = "test_op";
+        //noinspection ResultOfMethodCallIgnored
+        lzyServerClient.publish(Lzy.PublishRequest.newBuilder()
+            .setOperation(Operations.Zygote.newBuilder().build())
+            .setName(opName)
+            .build());
+
+        //Act/Assert
+        //noinspection ResultOfMethodCallIgnored
+        Assert.assertThrows(StatusRuntimeException.class, () -> lzyServerClient.publish(Lzy.PublishRequest.newBuilder()
+            .setOperation(Operations.Zygote.newBuilder().build())
+            .setName(opName)
+            .build()));
+    }
+
+    //@Test
+    //public void testServantDoesNotSeeNewZygotes() throws Exception {
+    //    //Arrange
+    //    final String lzyPath = "/tmp/lzy";
+    //    final List<Operations.RegisteredZygote> zygotesBeforeStart = IntStream.range(0, 1000)
+    //        .mapToObj(value -> lzyServerClient.publish(Lzy.PublishRequest.newBuilder()
+    //            .setOperation(Operations.Zygote.newBuilder().build())
+    //            .setName("test_op_" + value)
+    //            .build()))
+    //        .collect(Collectors.toList());
+    //
+    //    //Act
+    //    startTerminalAtPath(lzyPath);
+    //    final List<Operations.RegisteredZygote> zygotesAfterStart = IntStream.range(1000, 2000)
+    //        .mapToObj(value -> lzyServerClient.publish(Lzy.PublishRequest.newBuilder()
+    //            .setOperation(Operations.Zygote.newBuilder().build())
+    //            .setName("test_op_" + value)
+    //            .build()))
+    //        .collect(Collectors.toList());
+    //
+    //
+    //    //Assert
+    //    Assert.assertTrue(waitForFuse(lzyPath + "/bin", DEFAULT_FUSE_TIMEOUT_SEC, TimeUnit.SECONDS));
+    //    zygotesBeforeStart.forEach(registeredZygote -> Assert.assertTrue(Files.exists(Paths.get(
+    //        lzyPath + "/bin/" + registeredZygote.getName()))));
+    //    zygotesAfterStart.forEach(registeredZygote -> Assert.assertFalse(Files.exists(Paths.get(
+    //        lzyPath + "/bin/" + registeredZygote.getName()))));
+    //}
 }
