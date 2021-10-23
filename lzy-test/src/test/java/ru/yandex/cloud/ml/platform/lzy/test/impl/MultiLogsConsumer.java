ed5424cb5fdf3e90214a19f8eb601288df2a9a04
tomato <tomato@yandex-team.ru>
2021-10-23 16:33:29 +0300

    add multi logs consumer

---
+++ lzy-test/src/test/java/ru/yandex/cloud/ml/platform/lzy/test/impl/MultiLogsConsumer.java
@@ -0,0 +1,21 @@
+package ru.yandex.cloud.ml.platform.lzy.test.impl;
+
+import org.testcontainers.containers.output.BaseConsumer;
+import org.testcontainers.containers.output.OutputFrame;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MultiLogsConsumer extends BaseConsumer<MultiLogsConsumer> {
+    private final List<BaseConsumer<?>> consumers = new ArrayList<>();
+
+    public MultiLogsConsumer(BaseConsumer<?>... consumers) {
+        this.consumers.addAll(Arrays.asList(consumers));
+    }
+
+    @Override
+    public void accept(OutputFrame outputFrame) {
+        consumers.forEach(baseConsumer -> baseConsumer.accept(outputFrame));
+    }
+}
