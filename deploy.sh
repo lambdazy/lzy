f780df26e60f955b3ae9c71f50756d88ee4a0974
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-08 17:50:16 +0300

    08-11-2021 state (1)

--- deploy.sh
+++ deploy.sh
@@ -39,4 +39,4 @@ echo "$LZY_SERVER_HOST"
 
 #docker run --name terminal --privileged -e USER=nick-tycoon -e LOG_FILE="terminal" -e SUSPEND_DOCKER=n -e DEBUG_PORT=5006 --mount type=bind,source=/Users/nik-tycoon/.ssh/,target=/Users/nik-tycoon/.ssh/ --mount type=bind,source=/var/log/servant/,target=/var/log/servant/ lzy-servant "--lzy-address" "http://62.84.119.7:8899" "--lzy-mount" "/tmp/lzy" "--private-key" "/Users/nik-tycoon/.ssh/id_rsa" "--host" "localhost" "--port" "9990" --internal-host localhost terminal
 
-cd lzy-python && pip install . && python examples/integration/simple_graph.py
+#cd lzy-python && pip install . && python examples/integration/simple_graph.py
