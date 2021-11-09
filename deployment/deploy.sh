d8d406e124d9c41c8c07350622c63975124f70b5
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-09 20:31:59 +0300

    deleted old lines from deploy.sh

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -17,26 +17,4 @@ docker push cr.yandex/crppns4pq490jrka0sth/lzy-server:latest
 docker push cr.yandex/crppns4pq490jrka0sth/lzy-servant:latest
 docker push cr.yandex/crppns4pq490jrka0sth/lzy-kharon:latest
 
-#kubectl delete pod lzy-server-pod
-kubectl create -f lzy-server-pod-template.yaml
-kubectl wait --for=condition=Ready pod/lzy-server-pod --timeout=60s
-
-#kubectl create -f lzy-kharon-pod-template.yaml
-
-LZY_SERVER_HOST=$(kubectl get pod lzy-server-pod -o custom-columns=IP:status.podIP)
-echo "$LZY_SERVER_HOST"
-
-#kubectl delete pod terminal-pod
-#kubectl create -f lzy-kharon-pod-template.yaml
-#kubectl wait --for=condition=Ready pod/lzy-kharon-pod --timeout=60s
-#
-#echo "executing task \"echo 44\""
-#kubectl exec terminal-pod-2 -- bash -c 'export "ZYGOTE={\"fuze\":\"echo 44\",\"provisioning\":\"not implemented\"}" && /tmp/lzy/sbin/run'
-
-#java -jar -Djava.library.path=/usr/local/lib lzy-servant/target/lzy-servant-1.0-SNAPSHOT-jar-with-dependencies.jar  "--lzy-address" "http://62.84.119.7:8899" "--lzy-mount" "/tmp/lzy" "--private-key" "/Users/nik-tycoon/.ssh/id_rsa" "--host" "localhost" "--port" "9990" "terminal"
-
-# docker run --privileged -e USER=nick-tycoon -e LOG_FILE="terminal" -e SUSPEND_DOCKER=n -e DEBUG_PORT=5006 lzy-servant "--lzy-address" "http://62.84.119.7:8899" "--lzy-mount" "/tmp/lzy" "--private-key" "/Users/nik-tycoon/.ssh/id_rsa" "--host" "localhost" "--port" "9990" --internal-host localhost terminal
-
 #docker run --name terminal --privileged -e USER=nick-tycoon -e LOG_FILE="terminal" -e SUSPEND_DOCKER=n -e DEBUG_PORT=5006 --mount type=bind,source=/Users/nik-tycoon/.ssh/,target=/Users/nik-tycoon/.ssh/ --mount type=bind,source=/var/log/servant/,target=/var/log/servant/ lzy-servant "--lzy-address" "http://62.84.119.7:8899" "--lzy-mount" "/tmp/lzy" "--private-key" "/Users/nik-tycoon/.ssh/id_rsa" "--host" "localhost" "--port" "9990" --internal-host localhost terminal
-
-#cd lzy-python && pip install . && python examples/integration/simple_graph.py
