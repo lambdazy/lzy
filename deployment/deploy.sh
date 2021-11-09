ac9daa53183429c1b58a6b2f2457549a1761b6ce
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-09 18:41:03 +0300

    deploy.sh moved to deployment/

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -18,16 +18,16 @@ docker push cr.yandex/crppns4pq490jrka0sth/lzy-servant:latest
 docker push cr.yandex/crppns4pq490jrka0sth/lzy-kharon:latest
 
 #kubectl delete pod lzy-server-pod
-kubectl create -f lzy-server-pod.yaml
+kubectl create -f lzy-server-pod-template.yaml
 kubectl wait --for=condition=Ready pod/lzy-server-pod --timeout=60s
 
-#kubectl create -f lzy-kharon-pod.yaml
+#kubectl create -f lzy-kharon-pod-template.yaml
 
 LZY_SERVER_HOST=$(kubectl get pod lzy-server-pod -o custom-columns=IP:status.podIP)
 echo "$LZY_SERVER_HOST"
 
 #kubectl delete pod terminal-pod
-#kubectl create -f lzy-kharon-pod.yaml
+#kubectl create -f lzy-kharon-pod-template.yaml
 #kubectl wait --for=condition=Ready pod/lzy-kharon-pod --timeout=60s
 #
 #echo "executing task \"echo 44\""
