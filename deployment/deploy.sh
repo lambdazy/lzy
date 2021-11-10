04ecf298d4a75508f22cc7cc8fe8bdd30076ac39
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-10 16:08:06 +0300

    changed docker repository for images + fixed forkjoin pool parallelism flag

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-PODS=$(kubectl get pods | sed 1d | awk '{printf $1 " "}')
-printf "deleting pods: %s\n" "$PODS"
-for POD in $PODS
-do
-  kubectl delete POD "$POD"
-done
+if [[ $1 == "rebuild" ]]
+then
+    docker build -t celdwind/lzy:lzy-servant-base -f lzy-servant/BaseDockerfile .
+    docker tag lzy-servant-base celdwind/lzy:lzy-servant-base
+    docker push celdwind/lzy:lzy-servant-base
+    mvn clean install -DskipTests
+fi
 
-mvn clean install -DskipTests
+docker tag lzy-server celdwind/lzy:lzy-server
+docker tag lzy-servant celdwind/lzy:lzy-servant
+docker tag lzy-kharon celdwind/lzy:lzy-kharon
 
-docker tag lzy-server cr.yandex/crppns4pq490jrka0sth/lzy-server:latest
-docker tag lzy-servant cr.yandex/crppns4pq490jrka0sth/lzy-servant:latest
-docker tag lzy-kharon cr.yandex/crppns4pq490jrka0sth/lzy-kharon:latest
-
-docker push cr.yandex/crppns4pq490jrka0sth/lzy-server:latest
-docker push cr.yandex/crppns4pq490jrka0sth/lzy-servant:latest
-docker push cr.yandex/crppns4pq490jrka0sth/lzy-kharon:latest
-
-#docker run --name terminal --privileged -e USER=nick-tycoon -e LOG_FILE="terminal" -e SUSPEND_DOCKER=n -e DEBUG_PORT=5006 --mount type=bind,source=/Users/nik-tycoon/.ssh/,target=/Users/nik-tycoon/.ssh/ --mount type=bind,source=/var/log/servant/,target=/var/log/servant/ lzy-servant "--lzy-address" "http://62.84.119.7:8899" "--lzy-mount" "/tmp/lzy" "--private-key" "/Users/nik-tycoon/.ssh/id_rsa" "--host" "localhost" "--port" "9990" --internal-host localhost terminal
+docker push celdwind/lzy:lzy-server
+docker push celdwind/lzy:lzy-servant
+docker push celdwind/lzy:lzy-kharon
