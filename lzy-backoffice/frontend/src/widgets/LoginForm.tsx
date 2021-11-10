cb5dd1ea964e18776ea33c7e655a8156f84c2b69
artolord <artolord@yandex-team.ru>
2021-11-10 11:50:04 +0300

    Fix issues

--- lzy-backoffice/frontend/src/widgets/LoginForm.tsx
+++ lzy-backoffice/frontend/src/widgets/LoginForm.tsx
@@ -1,73 +1,86 @@
-import React from 'react';
+import React from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import { Redirect } from 'react-router-dom';
+import { Redirect, useHistory } from "react-router-dom";
+import { useAuth } from "../logic/Auth";
+import { LzyLogo } from "./LzyLogo";
 
-import Cookies from 'universal-cookie';
-import { LzyLogo } from './LzyLogo';
-
-const cookies = new Cookies()
-
-export interface LoginFormStateInterface{
-    userId: string | null,
-    userIdSet: boolean
+export interface LoginFormStateInterface {
+  userId: string | null;
+  userIdSet: boolean;
 }
 
-export interface LoginFormPropsInterface{
-    onUserIdSet: any
+export interface LoginFormPropsInterface {
+  onUserIdSet: (userId: string) => void;
 }
 
 export class LoginForm extends React.Component<
-    LoginFormPropsInterface,
-    LoginFormStateInterface
->{
-    constructor(props: LoginFormPropsInterface){
-        super(props);
-        this.state = {userId: null, userIdSet: false}
-    }
+  LoginFormPropsInterface,
+  LoginFormStateInterface
+> {
+  constructor(props: LoginFormPropsInterface) {
+    super(props);
+    this.state = { userId: null, userIdSet: false };
+  }
 
-    handleSubmit = (event: any):void =>{
-        if (this.state.userId != null){
-            this.setState({userIdSet: true})
-            this.props.onUserIdSet(this.state.userId)
-        }
+  handleSubmit = (event: any): void => {
+    if (this.state.userId != null) {
+      this.setState({ userIdSet: true });
+      this.props.onUserIdSet(this.state.userId);
     }
+  };
 
-    handleChange = (event: any):void =>{
-        this.setState({userId: event.target.value})
-    }
+  handleChange = (event: any): void => {
+    this.setState({ userId: event.target.value });
+  };
 
-    render() {
-        if (!this.state.userIdSet){
-            return (
-                <div>
-                <LzyLogo />
-                <Container className="loginForm" fluid>
-                    <Form>
-                        <Form.Group className="mb-3" controlId="loginFormUserId">
-                            <Form.Label>Username</Form.Label>
-                            <Form.Control type="text" placeholder="Enter username" onChange={this.handleChange} />
-                        </Form.Group>
-                        <Button variant="primary" type="submit" onClick={this.handleSubmit}>
-                            Ok
-                        </Button>
-                    </Form>
-                </Container>
-                </div>
-            )
-        }
-        return (
-            <Redirect to="/" />
-        )
+  render() {
+    if (!this.state.userIdSet) {
+      return (
+        <div>
+          <div className="App">
+            <div className="App__Svg">
+              <LzyLogo width="322" height="193" />
+            </div>
+          </div>
+          <Container className="loginForm" fluid>
+            <Form>
+              <Form.Group className="mb-3" controlId="loginFormUserId">
+                <Form.Label>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username"
+                  onChange={this.handleChange}
+                />
+              </Form.Group>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleSubmit}
+              >
+                Ok
+              </Button>
+            </Form>
+          </Container>
+        </div>
+      );
     }
+    return <Redirect to="/" />;
+  }
 }
 
-export const LoginFormFC : React.FC<{}> = () =>{
-    return (
-        <LoginForm onUserIdSet = {(s: string) => {
-            cookies.set("userId", s);
-            console.log("Signed in")
-            window.location.href = '/';
-        }}/>
-    )
-}
-
+export const LoginFormFC: React.FC<{}> = () => {
+  let auth = useAuth();
+  let history = useHistory();
+  return (
+    <LoginForm
+      onUserIdSet={(s: string) => {
+        auth.signout(() => {
+          auth.signin({ userId: s }, () => {
+            console.log("Signed in");
+            history.push("/");
+          });
+        });
+      }}
+    />
+  );
+};
--- lzy-backoffice/frontend/src/widgets/Router.tsx
+++ lzy-backoffice/frontend/src/widgets/Router.tsx
@@ -1,18 +1,25 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { LoginFormFC } from './LoginForm';
-import { UserTableFC } from './UsersTable';
-import Cookies from 'universal-cookie';
-import { BACKEND_HOST } from '../config';
-
-const cookies = new Cookies()
+import { Switch, Route, Redirect } from "react-router-dom";
+import { LoginFormFC } from "./LoginForm";
+import { UserTableFC } from "./UsersTable";
+import Cookies from "universal-cookie";
+import { BACKEND_HOST } from "../config";
+import { PrivateRoute } from "../logic/Auth";
+import { AddToken } from "./AddToken";
 
 export const MainRouter = () => (
   <main>
     <Switch>
-      <Route exact path='/login' component={LoginFormFC}/>
-      {cookies.get('userId') != null && <Route path='/users' render={(props: any) => <UserTableFC host={BACKEND_HOST}/>}/>}
-      {cookies.get('userId') == null && <Redirect to='/login'/>}
-      {cookies.get('userId') != null && <Redirect to='/users'/>}
+      <Route exact path="/login" component={LoginFormFC} />
+      <PrivateRoute path="/users" exact>
+        <UserTableFC host={BACKEND_HOST} />{" "}
+      </PrivateRoute>
+      <PrivateRoute path="/add_token" exact>
+        <AddToken host={BACKEND_HOST} />{" "}
+      </PrivateRoute>
+      <Route exact path="/">
+        <Redirect to="/users" />
+      </Route>
+      <Redirect to="/" />
     </Switch>
   </main>
-)
+);
