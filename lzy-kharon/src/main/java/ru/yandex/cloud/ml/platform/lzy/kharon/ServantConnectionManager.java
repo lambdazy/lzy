83f3e53c859642cf5901309d695c27d659e7dead
d-kruchinin <d-kruchinin@yandex-team.ru>
2021-11-16 18:40:50 +0300

    lockmanager instead monitors

--- lzy-kharon/src/main/java/ru/yandex/cloud/ml/platform/lzy/kharon/ServantConnectionManager.java
+++ lzy-kharon/src/main/java/ru/yandex/cloud/ml/platform/lzy/kharon/ServantConnectionManager.java
@@ -4,16 +4,20 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.yandex.cloud.ml.platform.model.util.lock.LocalLockManager;
+import ru.yandex.cloud.ml.platform.model.util.lock.LockManager;
 import yandex.cloud.priv.datasphere.v2.lzy.LzyServantGrpc;
 import yandex.cloud.priv.datasphere.v2.lzy.LzyServantGrpc.LzyServantBlockingStub;
 
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
 
 public class ServantConnectionManager {
     private static final Logger LOG = LogManager.getLogger(ServantConnectionManager.class);
     private final Map<URI, Connection> connectionMap = new HashMap<>();
+    private final LockManager lockManager = new LocalLockManager();
 
     private static class Connection {
         private final LzyServantBlockingStub stub;
@@ -46,29 +50,41 @@ public class ServantConnectionManager {
         }
     }
 
-    public synchronized LzyServantBlockingStub getOrCreate(URI uri) {
+    public LzyServantBlockingStub getOrCreate(URI uri) {
         LOG.info("getOrCreate connection for uri " + uri);
-        if (connectionMap.containsKey(uri)) {
-            final Connection connection = connectionMap.get(uri);
-            connection.increaseCounter();
-            return connection.getStub();
-        }
+        final Lock lock = lockManager.getOrCreate(uri.toString());
+        lock.lock();
+        try {
+            if (connectionMap.containsKey(uri)) {
+                final Connection connection = connectionMap.get(uri);
+                connection.increaseCounter();
+                return connection.getStub();
+            }
 
-        final Connection connection = new Connection(uri);
-        connectionMap.put(uri, connection);
-        return connection.getStub();
+            final Connection connection = new Connection(uri);
+            connectionMap.put(uri, connection);
+            return connection.getStub();
+        } finally {
+            lock.unlock();
+        }
     }
 
-    public synchronized void shutdownConnection(URI uri) {
-        if (!connectionMap.containsKey(uri)) {
-            LOG.warn("Attempt to shutdown non-existing connection to URI " + uri);
-        }
-        final Connection connection = connectionMap.get(uri);
-        connection.decreaseCounter();
-        if (connection.getNumReferences() == 0) {
-            LOG.info("Number of references to URI equal to 1. Deleting it.");
-            connection.channel.shutdown();
-            connectionMap.remove(uri);
+    public void shutdownConnection(URI uri) {
+        final Lock lock = lockManager.getOrCreate(uri.toString());
+        lock.lock();
+        try {
+            if (!connectionMap.containsKey(uri)) {
+                LOG.warn("Attempt to shutdown non-existing connection to URI " + uri);
+            }
+            final Connection connection = connectionMap.get(uri);
+            connection.decreaseCounter();
+            if (connection.getNumReferences() == 0) {
+                LOG.info("Number of references to URI equal to 1. Deleting connection to " + uri);
+                connection.channel.shutdown();
+                connectionMap.remove(uri);
+            }
+        } finally {
+            lock.unlock();
         }
     }
 }
