ab5cd626478963f94dbe5770e1d7855e5fe1cb93
artolord <artolord@yandex-team.ru>
2021-11-18 18:28:20 +0300

    Add clickhouse logs

--- lzy-servant/entrypoint.sh
+++ lzy-servant/entrypoint.sh
@@ -5,6 +5,7 @@ java \
 -Dsun.jnu.encoding=UTF-8 \
 -Dfile.encoding=UTF-8 \
 -Dcustom.log.file="$LOG_FILE" \
+-Dcustom.log.servant_id="$LZYTASK" \
 -Dlog4j.configurationFile=/app/resources/log4j2.yaml \
 -XX:+HeapDumpOnOutOfMemoryError \
 -agentlib:jdwp=transport=dt_socket,server=y,suspend="$SUSPEND_DOCKER",address=*:"$DEBUG_PORT" \
---
+++ lzy-servant/src/main/java/ru/yandex/cloud/ml/platform/lzy/servant/logs/ConnectionFactory.java
@@ -0,0 +1,31 @@
+package ru.yandex.cloud.ml.platform.lzy.servant.logs;
+
+import ru.yandex.clickhouse.ClickHouseDataSource;
+import ru.yandex.clickhouse.settings.ClickHouseProperties;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionFactory {
+    private interface Singleton {
+        ConnectionFactory INSTANCE = new ConnectionFactory();
+    }
+
+    private final DataSource dataSource;
+
+    private ConnectionFactory() {
+        String clickhouseHost = System.getenv("LOGS_HOST");
+        String clickhouseUser = System.getenv("LOGS_USER");
+        String clickhousePass = System.getenv("LOGS_PASSWORD");
+        String url = String.format("jdbc:clickhouse://%s:8123/lzy.servant_logs?user=%s&password=%s",
+                clickhouseHost, clickhouseUser, clickhousePass);
+
+        this.dataSource = new ClickHouseDataSource(url);
+    }
+
+    public static Connection getDatabaseConnection() throws SQLException {
+        return Singleton.INSTANCE.dataSource.getConnection();
+    }
+}
