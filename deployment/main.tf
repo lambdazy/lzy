0d84b164d8d6f68d9ccc3731a03eaefa68a87b25
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-19 20:04:21 +0300

    output.tf: deleted old outputs

--- deployment/main.tf
+++ deployment/main.tf
@@ -173,7 +173,7 @@ resource "azurerm_public_ip" "lzy_kharon" {
   resource_group_name = azurerm_resource_group.test.name
   sku                 = "Standard"
   allocation_method   = "Static"
-  domain_name_label   = "lzy-kharon.northeurope.cloudapp.azure.com"
+  domain_name_label   = "lzy.kharon.northeurope.cloudapp.azure.com"
 }
 
 resource "azurerm_role_assignment" "test" {
--- deployment/output.tf
+++ deployment/output.tf
@@ -7,15 +7,3 @@ output "host" {
   value = azurerm_kubernetes_cluster.main.kube_config[0].host
   sensitive = true
 }
-
-output "lzy_server_host" {
-  value = kubernetes_pod.lzy-server.spec[0].host_ipc
-}
-
-output "lzy_kharon_domain_name_label" {
-  value = azurerm_public_ip.test.domain_name_label
-}
-
-output "lzy_kharon_public_ip_address" {
-  value = azurerm_public_ip.test.ip_address
-}
