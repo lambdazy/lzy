d8783264df4492535f2aa2dfc91431541ea1c144
artolord <artolord@yandex-team.ru>
2021-11-19 10:50:43 +0300

    Fix logs

--- lzy-servant/src/main/java/ru/yandex/cloud/ml/platform/lzy/servant/logs/ConnectionFactory.java
+++ lzy-servant/src/main/java/ru/yandex/cloud/ml/platform/lzy/servant/logs/ConnectionFactory.java
@@ -1,5 +1,7 @@
 package ru.yandex.cloud.ml.platform.lzy.servant.logs;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.yandex.clickhouse.ClickHouseDataSource;
 import ru.yandex.clickhouse.settings.ClickHouseProperties;
 
@@ -9,6 +11,8 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionFactory {
+
+    private static final Logger LOG = LogManager.getLogger(ConnectionFactory.class);
     private interface Singleton {
         ConnectionFactory INSTANCE = new ConnectionFactory();
     }
@@ -16,6 +20,7 @@ public class ConnectionFactory {
     private final DataSource dataSource;
 
     private ConnectionFactory() {
+        LOG.info("Creating connection factory");
         String clickhouseHost = System.getenv("LOGS_HOST");
         String clickhouseUser = System.getenv("LOGS_USER");
         String clickhousePass = System.getenv("LOGS_PASSWORD");
@@ -23,9 +28,11 @@ public class ConnectionFactory {
                 clickhouseHost, clickhouseUser, clickhousePass);
 
         this.dataSource = new ClickHouseDataSource(url);
+        LOG.info("Creating connection factory");
     }
 
     public static Connection getDatabaseConnection() throws SQLException {
+        LOG.info("Getting clickhouse connection");
         return Singleton.INSTANCE.dataSource.getConnection();
     }
 }
