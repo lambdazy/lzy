dfbc96f3f4a6f08a8b7ddc09838c9ef56538b61a
artolord <artolord@yandex-team.ru>
2021-11-19 10:50:43 +0300

    Fix logs

--- lzy-servant/entrypoint.sh
+++ lzy-servant/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+export LOGS_APPENDER="${LOGS_APPENDER:-LogFile}"
+
 java \
 -Xmx4G \
 -Dsun.jnu.encoding=UTF-8 \
 -Dfile.encoding=UTF-8 \
 -Dcustom.log.file="$LOG_FILE" \
 -Dcustom.log.servant_id="$LZYTASK" \
+-Dcustom.log.appender="$LOGS_APPENDER" \
 -Dlog4j.configurationFile=/app/resources/log4j2.yaml \
 -XX:+HeapDumpOnOutOfMemoryError \
 -agentlib:jdwp=transport=dt_socket,server=y,suspend="$SUSPEND_DOCKER",address=*:"$DEBUG_PORT" \
