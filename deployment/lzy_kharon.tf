1cec3fe11a0b6b90dc9619d8de0eeaa1f6bf740c
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-11-26 14:56:10 +0300

    main.tf: extracted kubernetes pod and services to new tf files

---
+++ deployment/lzy_kharon.tf
@@ -0,0 +1,92 @@
+resource "kubernetes_pod" "lzy_kharon" {
+  metadata {
+    name   = "lzy-kharon"
+    labels = {
+      app : "lzy-kharon"
+    }
+  }
+  spec {
+    container {
+      name              = "lzy-kharon"
+      image             = "celdwind/lzy:lzy-kharon"
+      image_pull_policy = "Always"
+      env {
+        name = "LZY_HOST"
+        value_from {
+          field_ref {
+            field_path = "status.podIP"
+          }
+        }
+      }
+      env {
+        name  = "LZY_SERVER_IP"
+        value = kubernetes_service.lzy_server.spec[0].cluster_ip
+      }
+      port {
+        container_port = 8899
+      }
+      port {
+        container_port = 8900
+      }
+      args              = [
+        "--lzy-server-address",
+        "http://$(LZY_SERVER_IP):8888",
+        "--host",
+        "$(LZY_HOST)",
+        "--port",
+        "8899",
+        "--servant-proxy-port",
+        "8900"
+      ]
+    }
+    affinity {
+      pod_anti_affinity {
+        required_during_scheduling_ignored_during_execution {
+          label_selector {
+            match_expressions {
+              key      = "app"
+              operator = "In"
+              values   = [
+                "lzy-servant",
+                "lzy-server",
+                "lzy-kharon",
+                "lzy-backoffice"
+              ]
+            }
+          }
+          topology_key = "kubernetes.io/hostname"
+        }
+      }
+    }
+    host_network = true
+    dns_policy   = "ClusterFirstWithHostNet"
+  }
+
+  depends_on = [
+    kubernetes_pod.lzy_server
+  ]
+}
+
+resource "kubernetes_service" "lzy_kharon" {
+  metadata {
+    name        = "lzy-kharon-load-balancer"
+    annotations = {
+      "service.beta.kubernetes.io/azure-load-balancer-resource-group" = azurerm_resource_group.test.name
+    }
+  }
+  spec {
+    load_balancer_ip = azurerm_public_ip.lzy_kharon.ip_address
+    type             = "LoadBalancer"
+    port {
+      port = 8899
+    }
+    selector         = {
+      app = "lzy-kharon"
+    }
+  }
+
+  depends_on = [
+    kubernetes_pod.lzy_kharon,
+    azurerm_public_ip.lzy_kharon
+  ]
+}
