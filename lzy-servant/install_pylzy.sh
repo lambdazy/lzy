cb415570247af5546d2a39cf579f1384588ac968
Aleksey Zubakov <aleks.zubakov@gmail.com>
2021-12-02 18:31:02 +0300

    Refactor conda environment setup and pylzy install from sources in servant docker

--- lzy-servant/install_pylzy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-conda_env="$1"
-lzy_py_path="$2"
-
-eval "$(conda shell.bash hook)" && \
-  conda activate "$conda_env" && \
-  cd "$lzy_py_path" && pip install .
--- lzy-servant/prepare_pylzy_envs.sh
+++ lzy-servant/prepare_pylzy_envs.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+create_env_safely() {
+    lzy_py_path="$3"
+    cd "$lzy_py_path"
+    create_env "$1" "2"
+    cd -
+}
+
+create_env() {
+  name="$1"
+  version="$2"
+  eval "$(conda shell.bash hook)" && \
+    conda create --name "$name" "python=$version" pip && \
+    conda activate "$name" && \
+    python -m ensurepip && \
+    # probably it's not needed to install requirements
+    pip install -r requirements.txt && \
+    pip install . && \
+    return 0
+}
+
 lzy_py_path="$1"
-./install_pylzy.sh "default" "$lzy_py_path" \
-    && ./install_pylzy.sh "py37" "$lzy_py_path" \
-    && ./install_pylzy.sh "py38" "$lzy_py_path" \
-    && ./install_pylzy.sh "py39" "$lzy_py_path" \
-#    && ./create_env.sh "py310" "3.10.0"
+create_env_safely "default" "3.9.7" "$lzy_py_path" \
+    && create_env_safely "py37" "3.7.11" "$lzy_py_path" \
+    && create_env_safely "py38" "3.8.12" "$lzy_py_path" \
+    && create_env_safely "py39" "3.9.7" "$lzy_py_path" \
+#    && create_env_safely "py310" "3.10.0" "$lzy_py_path"
