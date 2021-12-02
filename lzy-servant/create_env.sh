064e35849372e6ab85cec4645064842233b8d3f1
Aleksey Zubakov <aleks.zubakov@gmail.com>
2021-12-02 18:31:02 +0300

    Refactor conda environment setup and pylzy install from sources in servant docker

--- lzy-servant/create_env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-name="$1"
-version="$2"
-eval "$(conda shell.bash hook)" && \
-  conda create --name "$name" "python=$version" pip && \
-  conda activate "$name" && \
-  python -m ensurepip && \
-  pip install -r requirements.txt
