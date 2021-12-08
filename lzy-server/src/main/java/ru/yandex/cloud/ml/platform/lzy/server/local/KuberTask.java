bdef10956c9905d51ca9a964cf8e472e63c952fa
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-12-08 13:51:18 +0300

    refactored KuberTask

--- lzy-server/src/main/java/ru/yandex/cloud/ml/platform/lzy/server/local/KuberTask.java
+++ lzy-server/src/main/java/ru/yandex/cloud/ml/platform/lzy/server/local/KuberTask.java
@@ -1,6 +1,5 @@
 package ru.yandex.cloud.ml.platform.lzy.server.local;
 
-import io.kubernetes.client.custom.Quantity;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
@@ -8,11 +7,11 @@ import io.kubernetes.client.openapi.apis.CoreV1Api;
 import io.kubernetes.client.openapi.models.*;
 import io.kubernetes.client.util.ClientBuilder;
 import io.kubernetes.client.util.Yaml;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.yandex.cloud.ml.platform.lzy.model.Slot;
 import ru.yandex.cloud.ml.platform.lzy.model.Zygote;
-import ru.yandex.cloud.ml.platform.lzy.model.graph.AtomicZygote;
 import ru.yandex.cloud.ml.platform.lzy.server.ChannelsManager;
 import ru.yandex.cloud.ml.platform.lzy.model.snapshot.SnapshotMeta;
 import ru.yandex.qe.s3.util.Environment;
@@ -25,18 +24,11 @@ import java.util.stream.Collectors;
 
 public class KuberTask extends BaseTask {
     private static final Logger LOG = LoggerFactory.getLogger(KuberTask.class);
-    public static final String LZY_SERVANT_POD_TEMPLATE_FILE_PROPERTY = "lzy.servant.pod.template.file";
-    public static final String DEFAULT_LZY_SERVANT_POD_TEMPLATE_FILE = "/app/resources/kubernetes/lzy-servant-pod-template.yaml";
-    public static final V1Toleration GPU_SERVANT_POD_TOLERATION = new V1TolerationBuilder()
-            .withKey("sku")
-            .withOperator("Equal")
-            .withValue("gpu")
-            .withEffect("NoSchedule")
-            .build();
-    public static final List<V1Toleration> GPU_SERVANT_POD_TOLERATIONS = List.of(
-            GPU_SERVANT_POD_TOLERATION
-    );
-    public static final V1ResourceRequirements GPU_SERVANT_POD_RESOURCE = new V1ResourceRequirementsBuilder().addToLimits("nvidia.com/gpu", Quantity.fromString("1")).build();
+    private static final String LZY_SERVANT_POD_TEMPLATE_FILE_PROPERTY = "lzy.servant.pod.template.file";
+    private static final String DEFAULT_LZY_SERVANT_POD_TEMPLATE_FILE = "/app/resources/kubernetes/lzy-servant-pod-template.yaml";
+    private static final String LZY_SERVANT_CONTAINER_NAME = "lzy-servant";
+
+    private final ProvisioningPodFactory provisioningPodFactory = new ProvisioningPodFactoryImpl();
 
     KuberTask(String owner, UUID tid, Zygote workload, Map<Slot, String> assignments,
               SnapshotMeta meta, ChannelsManager channels, URI serverURI) {
@@ -55,48 +47,26 @@ public class KuberTask extends BaseTask {
                     DEFAULT_LZY_SERVANT_POD_TEMPLATE_FILE
             );
             final File file = new File(lzyServantPodTemplatePath);
-            final V1Pod servantPodDescription = (V1Pod) Yaml.load(file);
-            Objects.requireNonNull(servantPodDescription.getSpec());
-            Objects.requireNonNull(servantPodDescription.getMetadata());
+            V1Pod servantPod = (V1Pod) Yaml.load(file);
+            Objects.requireNonNull(servantPod.getSpec());
+            Objects.requireNonNull(servantPod.getMetadata());
 
-            servantPodDescription.getSpec().getContainers().get(0).addEnvItem(
-                new V1EnvVar().name("LZYTASK").value(tid.toString())
-            ).addEnvItem(
-                new V1EnvVar().name("LZYTOKEN").value(token)
-            ).addEnvItem(
-                new V1EnvVar().name("LZY_SERVER_URI").value(serverURI.toString())
-            ).addEnvItem(
-                new V1EnvVar().name("BUCKET_NAME").value(Environment.getBucketName())
-            ).addEnvItem(
-                new V1EnvVar().name("ACCESS_KEY").value(Environment.getAccessKey())
-            ).addEnvItem(
-                new V1EnvVar().name("SECRET_KEY").value(Environment.getSecretKey())
-            ).addEnvItem(
-                new V1EnvVar().name("REGION").value(Environment.getRegion())
-            ).addEnvItem(
-                new V1EnvVar().name("SERVICE_ENDPOINT").value(Environment.getServiceEndpoint())
-            ).addEnvItem(
-                new V1EnvVar().name("PATH_STYLE_ACCESS_ENABLED").value(Environment.getPathStyleAccessEnabled())
-            ).addEnvItem(
-                new V1EnvVar().name("LZYWHITEBOARD").value(Environment.getLzyWhiteboard())
-            );
-            final String podName = "lzy-servant-" + tid.toString().toLowerCase(Locale.ROOT);
-            servantPodDescription.getMetadata().setName(podName);
-
-            final String typeLabelValue;
-            if (isNeedGpu()) {
-                typeLabelValue = "gpu";
-                servantPodDescription.getSpec().setTolerations(GPU_SERVANT_POD_TOLERATIONS);
-                servantPodDescription.getSpec().getContainers().get(0).setResources(GPU_SERVANT_POD_RESOURCE);
-            } else {
-                typeLabelValue = "cpu";
+            final Optional<V1Container> containerOptional = findContainerByName(servantPod, LZY_SERVANT_CONTAINER_NAME);
+            if (containerOptional.isEmpty()) {
+                LOG.error("lzy servant pod spec doesn't contain {} container", LZY_SERVANT_CONTAINER_NAME);
+                return;
             }
-            final Map<String, String> nodeSelector = Map.of("type", typeLabelValue);
-            servantPodDescription.getSpec().setNodeSelector(nodeSelector);
+            final V1Container container = containerOptional.get();
+            addEnvVars(container, token);
+
+            final String podName = "lzy-servant-" + tid.toString().toLowerCase(Locale.ROOT);
+            servantPod.getMetadata().setName(podName);
+
+            servantPod = provisioningPodFactory.fillPodSpecWithProvisioning(servantPod, workload());
 
             final CoreV1Api api = new CoreV1Api();
             final String namespace = "default";
-            final V1Pod pod = api.createNamespacedPod(namespace, servantPodDescription, null, null, null);
+            final V1Pod pod = api.createNamespacedPod(namespace, servantPod, null, null, null);
             LOG.info("Created servant pod in Kuber: {}", pod);
             while (true) {
                 //noinspection BusyWait
@@ -113,14 +83,7 @@ public class KuberTask extends BaseTask {
                     null,
                     Boolean.FALSE
                 );
-                final Optional<V1Pod> queriedPod =
-                    listNamespacedPod
-                        .getItems()
-                        .stream()
-                        .filter(v1pod -> podName.equals(Objects.requireNonNull(v1pod.getMetadata()).getName()))
-                        .collect(Collectors.toList())
-                        .stream()
-                        .findFirst();
+                final Optional<V1Pod> queriedPod = findPodByName(listNamespacedPod, podName);
                 if (queriedPod.isEmpty()) {
                     LOG.error("Not found pod " + podName);
                     break;
@@ -147,7 +110,47 @@ public class KuberTask extends BaseTask {
         }
     }
 
-    private boolean isNeedGpu() {
-        return ((AtomicZygote) workload()).provisioning().tags().anyMatch(tag -> tag.tag().contains("GPU"));
+    private void addEnvVars(V1Container container, String token) {
+        container.addEnvItem(
+                new V1EnvVar().name("LZYTASK").value(tid.toString())
+        ).addEnvItem(
+                new V1EnvVar().name("LZYTOKEN").value(token)
+        ).addEnvItem(
+                new V1EnvVar().name("LZY_SERVER_URI").value(serverURI.toString())
+        ).addEnvItem(
+                new V1EnvVar().name("BUCKET_NAME").value(Environment.getBucketName())
+        ).addEnvItem(
+                new V1EnvVar().name("ACCESS_KEY").value(Environment.getAccessKey())
+        ).addEnvItem(
+                new V1EnvVar().name("SECRET_KEY").value(Environment.getSecretKey())
+        ).addEnvItem(
+                new V1EnvVar().name("REGION").value(Environment.getRegion())
+        ).addEnvItem(
+                new V1EnvVar().name("SERVICE_ENDPOINT").value(Environment.getServiceEndpoint())
+        ).addEnvItem(
+                new V1EnvVar().name("PATH_STYLE_ACCESS_ENABLED").value(Environment.getPathStyleAccessEnabled())
+        ).addEnvItem(
+                new V1EnvVar().name("LZYWHITEBOARD").value(Environment.getLzyWhiteboard())
+        );
+    }
+
+    @NotNull
+    private static Optional<V1Container> findContainerByName(V1Pod servantPodDescription, String name) {
+        return Objects.requireNonNull(servantPodDescription.getSpec())
+                .getContainers()
+                .stream()
+                .filter(c -> name.equals(c.getName()))
+                .findFirst();
+    }
+
+    @NotNull
+    private static Optional<V1Pod> findPodByName(V1PodList listNamespacedPod, String podName) {
+        return listNamespacedPod
+            .getItems()
+            .stream()
+            .filter(v1pod -> podName.equals(Objects.requireNonNull(v1pod.getMetadata()).getName()))
+            .collect(Collectors.toList())
+            .stream()
+            .findFirst();
     }
 }
