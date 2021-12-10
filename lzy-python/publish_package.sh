4187955fd59014b068ea32bb8860ec8be9d554df
tomato <tomato@yandex-team.ru>
2021-12-10 12:40:16 +0300

    Revert "Update publishing script"
    This reverts commit 53a720ef91cd8e261fe03b8e9e83d9462a73220e.

--- lzy-python/publish_package.sh
+++ lzy-python/publish_package.sh
@@ -5,13 +5,10 @@
 python -m pip install --upgrade build twine
 # TODO: pass jar path to script as parameter?
 cp ../lzy-servant/target/lzy-servant-1.0-SNAPSHOT.jar lzy/lzy-servant.jar
-cp ../.git/HEAD ./
 python -m build # builds both wheel dist and source dist
 
 # currently publish to testpypi repo later should be changed to real pypi
 # token has to be provided by person who runs the script
 python -m twine upload dist/* --verbose
-
-rm ./HEAD lzy/lzy-servant.jar
 
 
