ea6cf946845427e4e94e6cd83abe8ea1ce8b1521
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-12-24 14:24:11 +0300

    added MetricEvent and MetricEventLogger

---
+++ lzy-model/src/main/java/ru/yandex/cloud/ml/platform/lzy/model/logs/MetricEventLogger.java
@@ -0,0 +1,12 @@
+package ru.yandex.cloud.ml.platform.lzy.model.logs;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MetricEventLogger {
+    private final static Logger LOG = LogManager.getLogger("MetricEventLogs");
+
+    public static void log(MetricEvent event){
+        LOG.info(String.format("{\"event\": %s }", event.toJson()));
+    }
+}
