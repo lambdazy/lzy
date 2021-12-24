071e60ad04dbb8e939488fff98e05a1bdcc57359
Nikita Siniachenko <ntpmosakk070601@gmail.com>
2021-12-24 14:24:11 +0300

    added MetricEvent and MetricEventLogger

---
+++ lzy-model/src/main/java/ru/yandex/cloud/ml/platform/lzy/model/logs/MetricEvent.java
@@ -0,0 +1,17 @@
+package ru.yandex.cloud.ml.platform.lzy.model.logs;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+
+public class MetricEvent {
+    public String toJson() {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            return ow.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return "{\"error\":\"Cannot convert event to json\"}";
+        }
+    }
+}
