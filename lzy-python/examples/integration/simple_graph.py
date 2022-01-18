a00f2a8b4760733491ceba4eb9a55e00cc1a7af8
Artem Trofimov <tomato@yandex-team.ru>
2022-01-18 21:49:11 +0300

    update simple graph test: add local module importing (#97)

--- lzy-python/examples/integration/simple_graph.py
+++ lzy-python/examples/integration/simple_graph.py
@@ -1,27 +1,31 @@
 from lzy.api import op, LzyEnv
+import base
+from base import Base
+import cloudpickle
 
 
 def main():
     @op
     def str_gen() -> str:
-        return "More meaningful str than ever before"
+        return "More meaningful str than ever "
 
     @op
-    def foo(a: int) -> int:
-        return a + 1
+    def foo(a: int) -> Base:
+        return Base(a, "before")
 
     @op
-    def bar(a: int, v: str) -> str:
+    def bar(bs: Base, sp: str) -> str:
         # noinspection PyTypeChecker
-        return v + "" + str(a)
+        return sp + bs.b + str(bs.a)
 
     with LzyEnv(user="test_user", server_url="localhost:8899"):
         s = str_gen()
-        f = foo(2)
+        f = foo(3)
         b = bar(f, s)
 
     print(b)
 
 
 if __name__ == '__main__':
+    cloudpickle.register_pickle_by_value(base)
     main()
