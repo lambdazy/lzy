2554233b5c66f53e7ab4cd268d06368a331c7dc9
Artem Trofimov <tomato@yandex-team.ru>
2022-02-01 16:14:06 +0300

    add python tests for wb and fix minor bugs (#117)

--- lzy-python/lzy/api/whiteboard/wb.py
+++ lzy-python/lzy/api/whiteboard/wb.py
@@ -6,10 +6,12 @@ from lzy.api.utils import is_lazy_proxy
 from lzy.api.whiteboard import is_whiteboard
 
 ALREADY_WRAPPED = '_already_wrapped'
+
+
 def wrap_whiteboard(
-    instance: Any,
-    whiteboard_api: WhiteboardApi,
-    whiteboard_id_getter: Callable[[], Optional[str]],
+        instance: Any,
+        whiteboard_api: WhiteboardApi,
+        whiteboard_id_getter: Callable[[], Optional[str]],
 ):
     if not is_whiteboard(instance):
         raise RuntimeError("Whiteboard must be a dataclass and have a @whiteboard decorator")
@@ -24,15 +26,22 @@ def wrap_whiteboard(
     }
 
     def __setattr__(self: Any, key: str, value: Any):
+        if not hasattr(self, ALREADY_WRAPPED):
+            super(type(self), self).__setattr__(key, value)
+            return
+
         if key not in fields_dict:
             raise AttributeError(f"No such attribute: {key}")
 
         if not is_lazy_proxy(value):
-            super(type(instance), self).__setattr__(key, value)
-            return
+            raise ValueError('Only @op return values can be assigned to whiteboard')
 
-        # pylint: disable=protected-access
-        return_entry_id = value._op.return_entry_id()
+        prev_value = super(type(instance), self).__getattribute__(key)
+        if is_lazy_proxy(prev_value):
+            raise ValueError('Whiteboard field can be assigned only once')
+
+        # noinspection PyProtectedMember
+        return_entry_id = value._op.return_entry_id()  # pylint: disable=protected-access
         whiteboard_id = whiteboard_id_getter()
         if return_entry_id is None or whiteboard_id is None:
             raise RuntimeError("Cannot get entry_id from op")
--- lzy-python/tests/test_whiteboard.py
+++ lzy-python/tests/test_whiteboard.py
@@ -1,8 +1,8 @@
 import dataclasses
 from unittest import TestCase
 
-from lzy.api import LzyLocalEnv
-from lzy.api.whiteboard import whiteboard, view
+from lzy.api import LzyLocalEnv, op
+from lzy.api.whiteboard import whiteboard
 
 
 @dataclasses.dataclass
@@ -12,10 +12,44 @@ class WB:
     b: int
 
 
+@op
+def num() -> int:
+    return 5
+
+
 class WhiteboardTests(TestCase):
     def test_many_wb(self):
         wb = WB(1, 1)
         with LzyLocalEnv(whiteboard=wb):
-            pass
+            wb.a = num()
+            wb.b = num()
+        self.assertEqual(5, wb.a)
+        self.assertEqual(5, wb.b)
+
+    def test_multiple_instances(self):
+        wb = WB(1, 1)
         with LzyLocalEnv(whiteboard=wb):
-            pass
+            wb.a = num()
+
+        wb2 = WB(1, 1)
+        with LzyLocalEnv(whiteboard=wb):
+            wb2.b = num()
+
+        self.assertEqual(5, wb.a)
+        self.assertEqual(5, wb2.b)
+
+    def test_non_op_assign(self):
+        with self.assertRaises(ValueError) as context:
+            wb = WB(1, 1)
+            with LzyLocalEnv(whiteboard=wb):
+                wb.a = num()
+                wb.b = 5
+        self.assertTrue('Only @op return values can be assigned to whiteboard' in str(context.exception))
+
+    def test_multiple_assigns(self):
+        with self.assertRaises(ValueError) as context:
+            wb = WB(1, 1)
+            with LzyLocalEnv(whiteboard=wb):
+                wb.a = num()
+                wb.a = num()
+        self.assertTrue('Whiteboard field can be assigned only once' in str(context.exception))
