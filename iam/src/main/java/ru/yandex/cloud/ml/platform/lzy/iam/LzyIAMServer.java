5bd0c5a0bd06a4190e6f6db7cbadfaa6b42e6b7b
atem11 <atem11@yandex-team.ru>
2022-02-03 12:31:17 +0300

    rename API

---
+++ iam/src/main/java/ru/yandex/cloud/ml/platform/lzy/iam/LzyIAMServer.java
@@ -0,0 +1,24 @@
+package ru.yandex.cloud.ml.platform.lzy.iam;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+
+public class LzyIAMServer {
+
+    public static final Logger LOG;
+
+    static{
+        // This is to avoid this bug: https://issues.apache.org/jira/browse/LOG4J2-2375
+        // KafkaLogsConfiguration will fall, so then we must call reconfigure
+        ProducerConfig.configNames();
+        LoggerContext ctx = (LoggerContext) LogManager.getContext();
+        ctx.reconfigure();
+        LOG = LogManager.getLogger(LzyIAMServer.class);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
