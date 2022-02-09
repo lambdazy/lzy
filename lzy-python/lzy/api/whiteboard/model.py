deae3d87b8b0c676466839dcb7c663d78d7ee53d
lsunko <sunko.elena2001@gmail.com>
2022-02-09 16:00:28 +0300

    provide default value for date

--- lzy-python/lzy/api/whiteboard/model.py
+++ lzy-python/lzy/api/whiteboard/model.py
@@ -175,7 +175,7 @@ class InMemWhiteboardApi(WhiteboardApi):
     def get(self, wb_id: str) -> WhiteboardDescription:
         return self.__whiteboards[wb_id]
 
-    def list(self, namespace: str, tags: List[str], from_date: Optional[str] = None, to_date: Optional[str] = None) \
+    def list(self, namespace: str, tags: List[str], from_date: str = '0001-01-01', to_date: str = '9999-12-31') \
             -> List[WhiteboardDescription]:
         from_local = datetime.strptime(from_date, '%Y-%m-%d')
         from_utc = datetime.fromtimestamp(from_local.timestamp(), tz=timezone.utc)
