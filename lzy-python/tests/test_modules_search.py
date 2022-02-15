b7adb55ef662095fd6b5dadfa0c470a5d11a5d84
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-02-15 19:10:14 +0300

    Fix local modules order for parent modules (#142)

--- lzy-python/tests/test_modules_search.py
+++ lzy-python/tests/test_modules_search.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
 from lzy.api.pkg_info import select_modules
-from tests.test_modules.level1.level1 import Level1
 
+from tests.test_modules.level1.level1 import Level1
+from tests.test_modules.level1.level2_nb import level_foo
 
 class ModulesSearchTests(TestCase):
     def test_modules_search(self):
@@ -31,3 +32,21 @@ class ModulesSearchTests(TestCase):
                          ],
                          module_names)
         self.assertEqual({"PyYAML", "s3fs"}, set(remote.keys()))
+
+    def test_modules_search_2(self):
+        _, local = select_modules({
+            'level_foo': level_foo
+        })
+        module_names = []
+        for module in local:
+            module_names.append(module.__name__)
+        self.assertEqual(['tests', 
+                         'tests.test_modules', 
+                         'tests.test_modules.level1', 
+                         'tests.test_modules.level1.level2', 
+                         'tests.test_modules.level1.level2.level3', 
+                         'tests.test_modules.level1.level2.level3.level3', 
+                         'tests.test_modules.level1.level2.level2', 
+                         'tests.test_modules.level1.level2_nb'
+                         ],
+                         module_names)
