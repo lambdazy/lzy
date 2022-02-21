4772de316b48000e49744b30a03971263531ddec
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-21 12:39:22 +0300

    version-docker.yml: removes step 'extract docker tags' push-docker.sh: added wget for resolving docker tags of services

--- deployment/push-docker.sh
+++ deployment/push-docker.sh
@@ -12,36 +12,32 @@ if [[ $2 == "rebuild" ]]; then
     docker build -t lzydock/lzy-servant-base:"$INSTALLATION" -f lzy-servant/BaseDockerfile .
     docker push lzydock/lzy-servant-base:"$INSTALLATION"
   fi
-#    mvn clean install -DskipTests
-#    docker build -t lzydock/lzy-backoffice-backend:"$INSTALLATION" lzy-backoffice/Dockerfile
-#    docker build -t lzydock/lzy-backoffice-frontend:"$INSTALLATION" lzy-backoffice/frontend/Dockerfile
+  mvn clean install -DskipTests
+#  docker build -t lzydock/lzy-backoffice-backend:"$INSTALLATION" lzy-backoffice/Dockerfile
+#  docker build -t lzydock/lzy-backoffice-frontend:"$INSTALLATION" lzy-backoffice/frontend/Dockerfile
 fi
 
 LAST_ARG=$(echo "$@" | awk '{print $NF}')
+SERVICES="lzy-server lzy-servant lzy-kharon lzy-whiteboard"
+for SERVICE in $SERVICES; do
+  if [[ $LAST_ARG == "update-version" ]]; then
+    echo "here $SERVICE"
+    MAX_TAG=-1
+    wget -q https://registry.hub.docker.com/v1/repositories/lzydock/$SERVICE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F ":" '{print $3}' |
+      while read -r TAG; do
+        echo "$TAG"
+        if [[ "$TAG" =~ [0-9]* && "$MAX_TAG" -lt "$TAG" ]]; then
+          MAX_TAG="$TAG"
+        fi
+      done
+    NEW_TAG=$(($MAX_TAG + 1))
+    docker tag "$SERVICE" "lzydock/$SERVICE:$NEW_TAG"
+    docker push "lzydock/$SERVICE:$NEW_TAG"
+  else
+    docker tag "$SERVICE" "lzydock/$SERVICE:$INSTALLATION"
+    docker push "lzydock/$SERVICE:$INSTALLATION"
+  fi
+done
 
-if [[ $LAST_ARG == "update-version" ]]; then
-  echo "HERE1"
-  MAX_TAG=0
-  while read TAG; do
-    echo "$TAG"
-    if [[ "$TAG" =~ [0-9]* && "$MAX_TAG" -lt "$TAG" ]]; then
-      MAX_TAG="$TAG"
-    fi
-  done
-  NEW_TAG=$(($MAX_TAG + 1))
-  echo "new tag is $NEW_TAG"
-fi
-
-exit 0
-
-docker tag lzy-server lzydock/lzy-server:"$INSTALLATION"
-docker tag lzy-servant lzydock/lzy-servant:"$INSTALLATION"
-docker tag lzy-kharon lzydock/lzy-kharon:"$INSTALLATION"
-docker tag lzy-whiteboard lzydock/lzy-whiteboard:"$INSTALLATION"
-
-docker push lzydock/lzy-server:"$INSTALLATION"
-docker push lzydock/lzy-servant:"$INSTALLATION"
-docker push lzydock/lzy-kharon:"$INSTALLATION"
-docker push lzydock/lzy-whiteboard:"$INSTALLATION"
 #docker push lzydock/lzy-backoffice-backend:"$INSTALLATION"
 #docker push lzydock/lzy-backoffice-frontend:"$INSTALLATION"
