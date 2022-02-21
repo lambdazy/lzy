a44306100ced77950aa0dd3c944be7432c96d741
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-21 14:45:40 +0300

    push-docker.sh: fixed setting MAX_TAG in subprocess instead of own process

--- deployment/push-docker.sh
+++ deployment/push-docker.sh
@@ -20,23 +20,24 @@ fi
 LAST_ARG=$(echo "$@" | awk '{print $NF}')
 SERVICES="lzy-server lzy-servant lzy-kharon lzy-whiteboard"
 for SERVICE in $SERVICES; do
+  echo "pushing docker for $SERVICE"
   if [[ $LAST_ARG == "update-version" ]]; then
-    echo "here $SERVICE"
     MAX_TAG=-1
-    wget -q https://registry.hub.docker.com/v1/repositories/lzydock/$SERVICE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F ":" '{print $3}' |
-      while read -r TAG; do
-        echo "$TAG"
-        if [[ "$TAG" =~ [0-9]* && "$MAX_TAG" -lt "$TAG" ]]; then
-          MAX_TAG="$TAG"
-        fi
-      done
-    NEW_TAG=$(($MAX_TAG + 1))
-    docker tag "$SERVICE" "lzydock/$SERVICE:$NEW_TAG"
+    for TAG in $(wget -q https://registry.hub.docker.com/v1/repositories/lzydock/$SERVICE/tags -O - | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n' | awk -F ":" '{print $3}'); do
+      if [[ "$TAG" =~ [0-9]* && "$MAX_TAG" -lt "$TAG" ]]; then
+        MAX_TAG="$TAG"
+      fi
+    done
+    NEW_TAG=$((MAX_TAG + 1))
+    echo "pushing lzydock/$SERVICE:$NEW_TAG"
+    docker tag "$SERVICE" lzydock/$SERVICE:$NEW_TAG
     docker push "lzydock/$SERVICE:$NEW_TAG"
   else
+    echo "pushing lzydock/$SERVICE:$INSTALLATION"
     docker tag "$SERVICE" "lzydock/$SERVICE:$INSTALLATION"
     docker push "lzydock/$SERVICE:$INSTALLATION"
   fi
+  echo ""
 done
 
 #docker push lzydock/lzy-backoffice-backend:"$INSTALLATION"
