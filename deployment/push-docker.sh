5fdb77aaaba17d2c1e3189ee9851c416620fad11
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-23 00:35:07 +0300

    added version-docker workflow and refactored push-docker

--- deployment/push-docker.sh
+++ deployment/push-docker.sh
@@ -1,33 +1,78 @@
 #!/bin/bash
 
-if [[ $# -lt 1 || -z $1 ]]
-then
-  echo "Usage: $0 <installation-tag> [rebuild [base]]"
+echo "flags: $*"
+exit 0
+
+if [[ $# -lt 1 || -z $1 ]]; then
+  echo "Usage: $0 <installation-tag> [--rebuild [--base [--update [--major]]]]"
   exit
 fi
 
+BASE=false
+REBUILD=false
+UPDATE=false
+MAJOR=false
+
+for ARG in "$@"; do
+  case "$ARG" in
+  --rebuild)
+    REBUILD=true
+    ;;
+  --base)
+    BASE=true
+    ;;
+  --update)
+    UPDATE=true
+    ;;
+  --major)
+    MAJOR=true
+    ;;
+  esac
+done
+
+SERVICES="lzy-server lzy-servant lzy-kharon lzy-whiteboard"
+if [[ $BASE = true ]]; then
+  SERVICES="lzy-servant-base $SERVICES"
+fi
 INSTALLATION=$1
 
-if [[ $2 == "rebuild" ]]
-then
-    if [[ $3 == "base" ]]
-        then
-            docker build -t lzydock/lzy-servant-base:"$INSTALLATION" -f lzy-servant/BaseDockerfile .
-            docker push lzydock/lzy-servant-base:"$INSTALLATION"
-    fi
-    mvn clean install -DskipTests
-#    docker build -t lzydock/lzy-backoffice-backend:"$INSTALLATION" lzy-backoffice/Dockerfile
-#    docker build -t lzydock/lzy-backoffice-frontend:"$INSTALLATION" lzy-backoffice/frontend/Dockerfile
+if [[ $REBUILD = true ]]; then
+  if [[ $BASE = true ]]; then
+    docker build -f lzy-servant/BaseDockerfile .
+  fi
+  mvn clean install -DskipTests
+#  docker build -t lzydock/lzy-backoffice-backend:"$INSTALLATION" lzy-backoffice/Dockerfile
+#  docker build -t lzydock/lzy-backoffice-frontend:"$INSTALLATION" lzy-backoffice/frontend/Dockerfile
 fi
 
-docker tag lzy-server lzydock/lzy-server:"$INSTALLATION"
-docker tag lzy-servant lzydock/lzy-servant:"$INSTALLATION"
-docker tag lzy-kharon lzydock/lzy-kharon:"$INSTALLATION"
-docker tag lzy-whiteboard lzydock/lzy-whiteboard:"$INSTALLATION"
+for SERVICE in $SERVICES; do
+  echo "pushing docker for $SERVICE"
+  if [[ $UPDATE = true ]]; then
+    MAJOR=1
+    MINOR=-1
+    for TAG in $(wget -q "https://registry.hub.docker.com/v1/repositories/lzydock/$SERVICE/tags" -O - | jq -r '.[].name'); do
+      if [[ "$TAG" =~ [0-9]+\.[0-9]+ ]]; then
+        CUR_MAJOR=$(echo "$TAG" | awk -F. '{print $1}')
+        CUR_MINOR=$(echo "$TAG" | awk -F. '{print $2}')
+        if [[ "$MAJOR" -lt "$CUR_MAJOR" || ("$MAJOR" = "$CUR_MAJOR" && "$MINOR" -lt "$CUR_MINOR") ]]; then
+          MAJOR="$CUR_MAJOR"
+          MINOR="$CUR_MINOR"
+        fi
+      fi
+    done
+    MINOR=$((MINOR + 1))
+    if [[ $MAJOR = true ]]; then
+      MAJOR=$((MAJOR + 1))
+    fi
+    TAG="$MAJOR.$MINOR"
+  else
+    TAG="$INSTALLATION"
+  fi
+  echo "pushing lzydock/$SERVICE:$TAG"
+  docker tag "$SERVICE" "lzydock/$SERVICE:$TAG"
+  docker push "lzydock/$SERVICE:$TAG"
+  echo ""
+done
 
-docker push lzydock/lzy-server:"$INSTALLATION"
-docker push lzydock/lzy-servant:"$INSTALLATION"
-docker push lzydock/lzy-kharon:"$INSTALLATION"
-docker push lzydock/lzy-whiteboard:"$INSTALLATION"
 #docker push lzydock/lzy-backoffice-backend:"$INSTALLATION"
 #docker push lzydock/lzy-backoffice-frontend:"$INSTALLATION"
