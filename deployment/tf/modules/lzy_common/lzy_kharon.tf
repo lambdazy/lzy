3c2a04911ed8e00345a7a99bf9c91255d8926438
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-24 15:54:30 +0300

    removed pod anti affinity expression "app.kubernetes.io/managed-by": "Helm"

--- deployment/tf/modules/lzy_common/lzy_kharon.tf
+++ deployment/tf/modules/lzy_common/lzy_kharon.tf
@@ -83,13 +83,6 @@ resource "kubernetes_deployment" "kharon" {
                     "clickhouse"
                   ]
                 }
-                match_expressions {
-                  key      = "app.kubernetes.io/managed-by"
-                  operator = "In"
-                  values = [
-                    "Helm"
-                  ]
-                }
               }
               topology_key = "kubernetes.io/hostname"
             }
