5b2d8c53b55ec6af4eb6cde48a238bb0a10d16c7
ArtoLord <artolord@yandex.ru>
2022-02-25 16:34:55 +0300

    Update python version

--- lzy-server/src/main/resources/db/migrations/V1_2__Create_tokens_table.sql
+++ lzy-server/src/main/resources/db/migrations/V1_2__Create_tokens_table.sql
@@ -1,11 +1,10 @@
-CREATE TABLE tokens
-(
-    name    text,
-    value   text,
-    user_id text,
-    FOREIGN KEY (user_id) references users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    PRIMARY KEY (name, user_id)
+CREATE TABLE tokens (
+   name text,
+   value text,
+   user_id text,
+   FOREIGN KEY (user_id) references users (user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+   PRIMARY KEY (name, user_id)
 );
 
 ALTER TABLE users
-    DROP COLUMN public_token;
+DROP COLUMN public_token;
