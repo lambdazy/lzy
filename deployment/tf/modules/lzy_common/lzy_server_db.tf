641ebfd4ea8946a68ddad4d4b9dcee546f9d9da4
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-03-10 10:43:19 +0300

    server_db, whiteboard_db: updated helm glob parameters names

--- deployment/tf/modules/lzy_common/lzy_server_db.tf
+++ deployment/tf/modules/lzy_common/lzy_server_db.tf
@@ -24,27 +24,27 @@ resource "helm_release" "lzy_server_db" {
   repository = "https://charts.bitnami.com/bitnami"
 
   set {
-    name  = "global.postgresql.postgresqlDatabase"
+    name  = "global.postgresql.auth.database"
     value = "serverDB"
   }
 
   set {
-    name  = "global.postgresql.postgresqlUsername"
+    name  = "global.postgresql.auth.username"
     value = "server"
   }
 
   set {
-    name  = "global.postgresql.existingSecret"
+    name  = "global.postgresql.auth.existingSecret"
     value = kubernetes_secret.lzy_server_db.metadata[0].name
   }
 
   set_sensitive {
-    name  = "global.postgresql.postgresqlPassword"
+    name  = "global.postgresql.auth.password"
     value = random_password.lzy_server_db_password[0].result
   }
 
   set {
-    name  = "global.postgresql.servicePort"
+    name  = "global.postgresql.service.ports.postgresql"
     value = "5432"
   }
 }
