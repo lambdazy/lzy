686aa2ebcdf6af249ff070047c57ebe0eee4ad85
tomato <tomato@yandex-team.ru>
2022-04-01 18:46:28 +0300

    update tutorials

--- docs/tutorials/3-environment.md
+++ docs/tutorials/3-environment.md
@@ -3,13 +3,15 @@
 ### Default behavior
 
 ʎzy automatically captures the python version with all imported modules and generates a conda environment specification.
-All local modules are uploaded to the internal storage.
-Before an @op-function runs on a remote VM, ʎzy applies the prepared conda environment and downloads all local modules.
-Therefore, **there is no need for manual environment setup in a general case**.
+All local modules are uploaded to the internal storage. Before an @op-function runs on a remote VM, ʎzy applies the
+prepared conda environment and downloads all local modules. Therefore, **there is no need for manual environment setup
+in a general case**.
 
 ### Manual customization
 
-For more complex scenarios (e.g., if dependencies beyond python are required), you can manually set up conda environment using its [yaml representation](https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#create-env-file-manually):
+For more complex scenarios (e.g., if dependencies beyond python are required), you can manually set up a conda
+environment using
+its [YAML representation](https://docs.conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#create-env-file-manually):
 
 ```python
 env = LzyRemoteEnv()
@@ -25,3 +27,6 @@ with env.workflow("train", local_module_paths=['/path/to/local/module']):
     ...
 ```
 
+---
+
+In the [**next**](4-cache.md) part, we will touch the data caching policies.
---
+++ lzy-python/examples/catboost_whiteboard.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import Dict, Optional
+
+from sklearn import datasets
+from sklearn.model_selection import GridSearchCV
+from sklearn.utils import Bunch
+
+from catboost import CatBoostClassifier
+from lzy.api import op, LzyRemoteEnv, Gpu
+from lzy.api.whiteboard import whiteboard, view
+
+
+@dataclass
+@whiteboard(tags=['best_model'])
+class BestModel:
+    model: CatBoostClassifier = None
+    params: Dict[str, int] = None
+    score: float = 0.0
+
+    @view
+    def model_view(self) -> CatBoostClassifier:
+        return self.model
+
+
+@op
+def dataset() -> Bunch:
+    data_set = datasets.load_breast_cancer()
+    return data_set
+
+
+@op(gpu=Gpu.any())
+def search_best_model(data_set: Bunch) -> GridSearchCV:
+    grid = {'max_depth': [3, 4, 5], 'n_estimators': [100, 200, 300]}
+    cb_model = CatBoostClassifier()
+    search = GridSearchCV(estimator=cb_model, param_grid=grid, scoring='accuracy', cv=5)
+    search.fit(data_set.data, data_set.target)
+    return search
+
+
+env = LzyRemoteEnv()
+wb = BestModel()
+with env.workflow("training", whiteboard=wb):
+    data_set = dataset()
+    search = search_best_model(data_set)
+    wb.model = search.best_estimator_
+    wb.params = search.best_params_
+    wb.score = search.best_score_
+    print(wb.__id__)
+
+loaded_wb = env.whiteboard(wb.__id__, BestModel)
+print(loaded_wb.params['max_depth'])
+
+wbs = env.whiteboards([BestModel])
+print(wbs)
+
+views = env.whiteboards([BestModel]).views(CatBoostClassifier)
+print(views)
