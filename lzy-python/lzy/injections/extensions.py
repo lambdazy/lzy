e9f6e38af20d42b32082fb1fe04c8ce4dc3a2da2
tomato <tomato@yandex-team.ru>
2022-04-15 18:52:29 +0300

    remove wrapper

--- lzy-python/lzy/injections/extensions.py
+++ lzy-python/lzy/injections/extensions.py
@@ -1,16 +1,11 @@
-from functools import wraps
 from typing import Tuple
 
 
 def extend(classes: Tuple[type, ...], original_prefix: str = 'original_'):
     def decorator(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            return func(self, *args, **kwargs)
-
         for cls in classes:
             original = getattr(cls, func.__name__)
-            setattr(cls, func.__name__, wrapper)
+            setattr(cls, func.__name__, func)
             setattr(cls, original_prefix + func.__name__, original)
         return func
 
