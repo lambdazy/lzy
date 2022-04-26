5adbf29d7bad706d945785edf4ef763e2f089a07
KruchDmitriy <d-kruchinin@yandex-team.ru>
2022-04-26 13:53:46 +0300

    added second test

--- lzy-python/tests/stress/test.py
+++ lzy-python/tests/stress/test.py
@@ -19,5 +19,23 @@ def test_only_one_task_in_parallel():
     runner.join()
 
 
+def test_two_task_in_parallel():
+    task_generator = TaskGenerator(
+        min_task_duration_minutes=1,
+        max_task_duration_minutes=3
+    )
+
+    runner = RandomRunner(
+        task_generator,
+        max_task_in_parallel=2,
+        max_rand_sleep_period_sec=10,
+        num_task_to_execute=6
+    )
+
+    runner.start()
+    runner.join()
+
+
 if __name__ == "__main__":
     test_only_one_task_in_parallel()
+    test_two_task_in_parallel()
