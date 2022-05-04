4006304ae1a150870d9615ee8f430b981bcaa685
ArtoLord <artolord@yandex.ru>
2022-05-04 16:41:20 +0300

    Fix problem with PipedOutputStream

--- lzy-servant/src/main/java/ru/yandex/cloud/ml/platform/lzy/servant/snapshot/SlotSnapshotImpl.java
+++ lzy-servant/src/main/java/ru/yandex/cloud/ml/platform/lzy/servant/snapshot/SlotSnapshotImpl.java
@@ -2,32 +2,22 @@ package ru.yandex.cloud.ml.platform.lzy.servant.snapshot;
 
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.protobuf.ByteString;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
+
+import java.io.*;
 import java.net.URI;
+import java.nio.channels.Channels;
+import java.nio.channels.Pipe;
 import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.yandex.cloud.ml.platform.lzy.model.Slot;
 import ru.yandex.cloud.ml.platform.lzy.servant.agents.LzyExecution;
-import ru.yandex.cloud.ml.platform.lzy.servant.fs.LzyInputSlot;
 import ru.yandex.cloud.ml.platform.lzy.servant.storage.StorageClient;
-import ru.yandex.qe.s3.transfer.StreamSuppliers;
-import ru.yandex.qe.s3.transfer.download.DownloadRequestBuilder;
 import ru.yandex.qe.s3.transfer.meta.Metadata;
 import ru.yandex.qe.s3.transfer.upload.UploadRequestBuilder;
 import ru.yandex.qe.s3.transfer.upload.UploadState;
-import ru.yandex.qe.s3.util.function.ThrowingConsumer;
 
 public class SlotSnapshotImpl implements SlotSnapshot {
     private static final Logger LOG = LogManager.getLogger(LzyExecution.class);
@@ -37,19 +27,26 @@ public class SlotSnapshotImpl implements SlotSnapshot {
     private final String bucket;
     private final Slot slot;
     private final AtomicBoolean nonEmpty = new AtomicBoolean(false);
-    private final PipedOutputStream out = new PipedOutputStream();
+    private final OutputStream out;
+    private final Pipe pipe;
     private final ListenableFuture<UploadState> future;
 
     public SlotSnapshotImpl(String taskId, String bucket, Slot slot, StorageClient storage) {
         this.bucket = bucket;
         this.taskId = taskId;
+        try {
+            this.pipe = Pipe.open();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot create pipe ", e);
+        }
+        this.out = Channels.newOutputStream(pipe.sink());
         this.slot = slot;
         this.storage = storage;
         future = storage.transmitter().upload(new UploadRequestBuilder()
             .bucket(bucket)
             .key(generateKey(slot))
             .metadata(Metadata.empty())
-            .stream(() -> new PipedInputStream(out))
+            .stream(() -> Channels.newInputStream(pipe.source()))
             .build()
         );
     }
