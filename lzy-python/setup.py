efb394cf639dab621ca5e3da81caa19eeb15caa5
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-05-11 17:15:53 +0300

    Added pure-protobuf as git dependency for a while

--- lzy-python/setup.py
+++ lzy-python/setup.py
@@ -18,65 +18,70 @@ class RunMixin(Command, ABC):
         self.set_distr_version_and_name(self.distribution, self.dev is not None)
         super().run()
 
-    def set_distr_version_and_name(self, distribution: Distribution,
-                                   is_dev: bool):
+    def set_distr_version_and_name(self, distribution: Distribution, is_dev: bool):
         distribution.metadata.version = read_version()
         if is_dev:
             suffix = f".dev{datetime.today().strftime('%Y%m%d')}"
             distribution.metadata.version += suffix  # type: ignore
-            distribution.metadata.name = 'pylzy-nightly'
+            distribution.metadata.name = "pylzy-nightly"
 
 
 class _bdist_wheel(RunMixin, bdist_wheel):
-    user_options = bdist_wheel.user_options + [
-        ('dev', None, "Build nightly package")
-    ]
+    user_options = bdist_wheel.user_options + [("dev", None, "Build nightly package")]
 
 
 class _sdist(RunMixin, sdist):
-    user_options = sdist.user_options + [
-        ('dev', None, "Build nightly package")
-    ]
+    user_options = sdist.user_options + [("dev", None, "Build nightly package")]
 
 
 class _install(RunMixin, install):
-    user_options = install.user_options + [
-        ('dev', None, "Build nightly package")
-    ]
+    user_options = install.user_options + [("dev", None, "Build nightly package")]
 
 
-def read_version(path='version'):
+def read_version(path="version"):
     with open(path) as file:
         return file.read().rstrip()
 
 
 def read_requirements() -> List[str]:
     requirements = []
-    with open('requirements.txt', 'r') as file:
+    with open("requirements.txt", "r") as file:
         for line in file:
             requirements.append(line.rstrip())
+    requirements.append(
+        "pure-protobuf @ git+ssh://git@github.com/eigenein/protobuf@2699b92fafc212ab3e7278aaa3931e9f4d326308#egg=pure-protobuf",
+    )
     return requirements
 
 
 setuptools.setup(
-    name='pylzy',
+    name="pylzy",
     version=read_version(),
-    author='ʎzy developers',
+    author="ʎzy developers",
     include_package_data=True,
-    package_data={
-        'lzy': ['lzy/lzy-servant.jar']
-    },
+    package_data={"lzy": ["lzy/lzy-servant.jar"]},
     install_requires=read_requirements(),
-    packages=['lzy', 'lzy/api', 'lzy/api/v1', 'lzy/api/v1/whiteboard', 'lzy/_proxy', 'lzy/storage',
-              'lzy/servant/model', 'lzy/servant', 'lzy/pkg_info', 'lzy/cli', 'lzy/serialization', 'lzy/injections'],
-    python_requires='>=3.7',
+    packages=[
+        "lzy",
+        "lzy/api",
+        "lzy/api/v1",
+        "lzy/api/v1/whiteboard",
+        "lzy/_proxy",
+        "lzy/storage",
+        "lzy/servant/model",
+        "lzy/servant",
+        "lzy/pkg_info",
+        "lzy/cli",
+        "lzy/serialization",
+        "lzy/injections",
+    ],
+    python_requires=">=3.7",
     cmdclass={
-        'install': _install,
-        'bdist_wheel': _bdist_wheel,
-        'sdist': _sdist,
+        "install": _install,
+        "bdist_wheel": _bdist_wheel,
+        "sdist": _sdist,
     },
-    entry_points = {
-        'console_scripts': ['lzy-terminal=lzy.cli.terminal_runner:console_main'],
-    }
+    entry_points={
+        "console_scripts": ["lzy-terminal=lzy.cli.terminal_runner:console_main"],
+    },
 )
-
