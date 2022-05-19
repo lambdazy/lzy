540ab878f42cc7c678b2e3cf295fc56f76132242
atem11 <atem11@yandex-team.ru>
2022-05-19 17:34:55 +0300

    Add SubjectService tests

--- iam/src/main/java/ru/yandex/cloud/ml/platform/lzy/iam/authorization/SubjectService.java
+++ iam/src/main/java/ru/yandex/cloud/ml/platform/lzy/iam/authorization/SubjectService.java
@@ -1,14 +1,20 @@
 package ru.yandex.cloud.ml.platform.lzy.iam.authorization;
 
+import ru.yandex.cloud.ml.platform.lzy.iam.authorization.exceptions.AuthException;
+import ru.yandex.cloud.ml.platform.lzy.iam.resources.credentials.UserCredentials;
 import ru.yandex.cloud.ml.platform.lzy.iam.resources.subjects.Subject;
 
 public interface SubjectService {
 
-    Subject createSubject(String name, String authProvider, String providerName);
+    Subject createSubject(String id, String authProvider, String providerUserName) throws AuthException;
 
-    void removeSubject(Subject subject);
+    Subject subject(String id) throws AuthException;
 
-    void addCredentials(Subject subject, String name, String value, String type);
+    void removeSubject(Subject subject) throws AuthException;
 
-    void removeCredentials(Subject subject, String name, String type);
+    void addCredentials(Subject subject, String name, String value, String type) throws AuthException;
+
+    UserCredentials credentials(Subject subject, String name) throws AuthException;
+
+    void removeCredentials(Subject subject, String name) throws AuthException;
 }
