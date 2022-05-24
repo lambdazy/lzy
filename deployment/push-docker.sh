5264b8300049983479d74d65cbf5417887ca1526
lindvv <lindvv@yandex.ru>
2022-05-24 13:44:10 +0300

    switch deployment to new docker scheme

--- deployment/push-docker.sh
+++ deployment/push-docker.sh
@@ -34,15 +34,13 @@ CUSTOM_TAG=$2
 
 if [[ $REBUILD = true ]]; then
   if [[ $BASE = true ]]; then
-    # docker build -t lzydock/default-env-base:master -f lzy-servant/docker/DefaultEnv.Base.Dockerfile .
-    # docker build -t lzydock/default-env:from-tar -f lzy-servant/docker/DefaultEnv.Dockerfile .
-    # docker save -o lzy-servant/docker/default-env-image.tar lzydock/default-env:from-tar
-    # docker build -t lzy-servant-base -t lzydock/lzy-servant-base:master -f lzy-servant/docker/System.Base.Dockerfile .
-
-    docker build -t lzy-servant-base -t lzydock/lzy-servant-base:old-scheme -f lzy-servant/docker/old-scheme/Base.Dockerfile .
+    docker build -t lzydock/default-env-base:master -f lzy-servant/docker/DefaultEnv.Base.Dockerfile .
+    docker build -t lzydock/default-env:from-tar -f lzy-servant/docker/DefaultEnv.Dockerfile .
+    docker save -o lzy-servant/docker/default-env-image.tar lzydock/default-env:from-tar
+    docker build -t lzy-servant-base -t lzydock/lzy-servant-base:master -f lzy-servant/docker/System.Base.Dockerfile .
   fi
   mvn clean install -DskipTests
-  docker build -t lzy-servant -f lzy-servant/docker/old-scheme/Dockerfile .
+  docker build -t lzy-servant -f lzy-servant/docker/System.Dockerfile .
   docker build -t lzy-server -f lzy-server/Dockerfile lzy-server
   docker build -t lzy-whiteboard -f lzy-whiteboard/Dockerfile lzy-whiteboard
   docker build -t lzy-kharon -f lzy-kharon/Dockerfile lzy-kharon
