801b71705ee6489da91f5a9b2d24cc75761b783c
lsunko <sunko.elena2001@gmail.com>
2022-05-31 17:07:52 +0300

    make load_arg and load_args private

--- lzy-python/lzy/api/v2/grpc/grpc_runtime.py
+++ lzy-python/lzy/api/v2/grpc/grpc_runtime.py
@@ -20,7 +20,7 @@ class GrpcRuntime(Runtime):
     def from_credentials(cls, credentials: StorageCredentials, bucket: str) -> 'GrpcRuntime':
         return cls(from_credentials(credentials), bucket)
 
-    def load_arg(self, entry_id: str, data: Any, serializer: Serializer):
+    def _load_arg(self, entry_id: str, data: Any, serializer: Serializer):
         with tempfile.NamedTemporaryFile('wb', delete=True) as write_file:
             serializer.serialize_to_file(data, write_file)
             write_file.flush()
@@ -29,14 +29,14 @@ class GrpcRuntime(Runtime):
                 uri = self._storage_client.write(self._bucket, entry_id, read_file)
                 # TODO: make a call to snapshot component to store entry_id and uri
 
-    def load_args(self, graph: Graph, serializer: Serializer):
+    def _load_args(self, graph: Graph, serializer: Serializer):
         for call in graph.calls():
             for name, arg in call.named_arguments():
                 if not is_lazy_proxy(arg):
                     # TODO: make a call to snapshot component to get entry_id for this argument
                     #  instead of manually generating it
                     entry_id = str(uuid.uuid4())
-                    self.load_arg(entry_id, arg, serializer)
+                    self._load_arg(entry_id, arg, serializer)
 
     def exec(self, graph: Graph, snapshot: Snapshot, progress: Callable[[], None]) -> None:
         pass
--- lzy-python/tests/v2/test_grpc_runtime.py
+++ lzy-python/tests/v2/test_grpc_runtime.py
@@ -58,7 +58,7 @@ class GrpcRuntimeTests(TestCase):
             f = foo("24")
             b = bar("42", f)
             graph: Graph = GraphBuilder().snapshot_id(str(uuid.uuid4())).add_call(b.lzy_call).build()
-            self._runtime.load_args(graph, DefaultSerializer())
+            self._runtime._load_args(graph, DefaultSerializer())
             self.assertTrue(len(self._storage_client.storage) == 2)
             values = list(map(lambda x: self._serializer.deserialize_from_string(x, str),
                               list(self._storage_client.storage.values())))
