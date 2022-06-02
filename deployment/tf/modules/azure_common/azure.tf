f4c7f0d147b7c503b7d94130ca38857093bcd907
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-06-02 17:38:25 +0300

    azure k8s cluster :: role_based_access_control_enabled = false

--- deployment/tf/modules/azure_common/azure.tf
+++ deployment/tf/modules/azure_common/azure.tf
@@ -29,6 +29,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   dns_prefix          = var.installation_name
+  role_based_access_control_enabled = false
 
   default_node_pool {
     name       = "agentpool"
