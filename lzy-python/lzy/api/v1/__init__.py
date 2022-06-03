a4f04ad07b807f82ec97061c616b81040a7ab90e
lsunko <sunko.elena2001@gmail.com>
2022-06-03 16:30:14 +0300

    import fix

--- lzy-python/lzy/api/v1/__init__.py
+++ lzy-python/lzy/api/v1/__init__.py
@@ -10,6 +10,7 @@ from lzy._proxy.result import Nothing
 from lzy.api.v1.servant.model.zygote import Gpu, Provisioning
 from lzy.api.v1.whiteboard.model import UUIDEntryIdGenerator
 from lzy.api.v1.utils import infer_return_type, lazy_proxy, infer_call_signature
+from lzy.api.v1.env import LzyRemoteEnv
 
 logging.root.setLevel(logging.INFO)
 handler = logging.StreamHandler(sys.stdout)
