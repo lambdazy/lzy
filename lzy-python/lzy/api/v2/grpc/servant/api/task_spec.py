adc0e30da9eb6f36f73d4ebf75da3e32a42e633d
lsunko <sunko.elena2001@gmail.com>
2022-06-06 11:49:14 +0300

    implement grpc_runtime

--- lzy-python/lzy/api/v2/grpc/servant/api/task_spec.py
+++ lzy-python/lzy/api/v2/grpc/servant/api/task_spec.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import List
 
 from lzy.api.v2.servant.model.channel import Bindings
 from lzy.api.v2.servant.model.zygote import Zygote
@@ -7,8 +6,6 @@ from lzy.api.v2.servant.model.zygote import Zygote
 
 @dataclass
 class TaskSpec:
-    call_id: str
-    operation_name: str
+    task_id: str
     zygote: Zygote
     bindings: Bindings
-    dependent_calls: List[str]
