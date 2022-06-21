b4d976d1122bf8a051145661888e7bebb6f99100
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-06-21 03:31:11 +0300

    Update and separate build/publish scripts with nix

---
+++ pylzy/build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env nix-shell
+#! nix-shell build.nix -A shell-publish -i bash
+
+echo "Building pylzy package"
+# TODO: pass jar path to script as parameter?
+cp ../lzy-servant/target/lzy-servant-1.0-SNAPSHOT.jar lzy/lzy-servant.jar
+
+# instead of
+# python -m build
+# which builds both wheel dist and source dist
+# 
+# build by directly executing setup.py
+# I couldn't find the better way to pass arguments to build
+python setup.py sdist $@ bdist_wheel $@ # pass --dev flag if needed
