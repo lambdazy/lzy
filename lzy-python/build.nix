09e18489a48db8a8fd38007f9ae40661c9b4f23a
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-06-21 16:34:52 +0300

    Move nix files to separate dir

--- lzy-python/build.nix
+++ lzy-python/build.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/360d84a26f0cfca4941d92a89e2b359d425a6d3d.tar.gz") {} }:
 let
-  lzy = ps: ps.callPackage ./lzy.nix {};
-  python = pkgs.callPackage ./python.nix {};
+  lzy = ps: ps.callPackage ./nix/lzy.nix {};
+  python = pkgs.callPackage ./nix/python.nix {};
   python-default = python.withPackages(ps: with ps; [
     boto3
     cloudpickle
@@ -21,7 +21,7 @@ let
     build
     twine
   ]);
-  mkEnv = custom-python: pkgs.callPackage ./mk-python-env.nix {
+  mkEnv = custom-python: pkgs.callPackage ./nix/mk-python-env.nix {
     inherit custom-python;
   };
 in {
