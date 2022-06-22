e7e9dfc0affb20f9642bba50a753edf4a388ecd4
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-06-22 19:02:23 +0300

    Protobuf generation for python

--- lzy-python/build.nix
+++ lzy-python/build.nix
@@ -1,7 +1,9 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/360d84a26f0cfca4941d92a89e2b359d425a6d3d.tar.gz") {} }:
 let
   lzy = ps: ps.callPackage ./nix/lzy.nix {};
+
   python = pkgs.callPackage ./nix/python.nix {};
+
   python-dev = python.withPackages(ps: with ps; [
     boto3
     cloudpickle
@@ -18,14 +20,17 @@ let
     # it's here just to get protoc
     grpcio-tools
   ]);
+
   python-lzy = python.withPackages(ps: with ps; [
     (lzy ps)
   ]);
+
   python-publish = python.withPackages(ps: with ps; [
     wheel
     build
     twine
   ]);
+
   python-lint = python.withPackages(ps: with ps; [
     mypy
     pyright
@@ -37,6 +42,7 @@ let
     boto3-stubs
     types-pyyaml
   ]);
+
   mkEnv = custom-python: pkgs.callPackage ./nix/mk-python-env.nix {
     inherit custom-python;
   };
