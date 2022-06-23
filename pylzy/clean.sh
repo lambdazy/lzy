92136548bcc034b53dad9519abedae9ecac49510
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-06-23 14:37:13 +0300

    Clean up scripts a bit, add script for running v2 tests

--- pylzy/clean.sh
+++ pylzy/clean.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
 echo "Clean up"
-# clean package build artefacts
+
+# package build artefacts
 rm -rvf *.egg-info dist/ build/ lzy/lzy-servant-*.jar
 
-# clean linter and test outputs
+# linter and test outputs
 rm -rvf .mypy_cache test_output
 
-# clean pip installed libs in nix env
+# pip installed libs in nix env
 rm -rvf _build/
 
-# clean mvn build artefacts
+# mvn build artefacts
 rm -rvf target/ 
 
-# remove generated proto files
+# generated proto files
 rm -rvf "$proto_out"
--- pylzy/run_v2_tests.sh
+++ pylzy/run_v2_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env nix-shell
 #! nix-shell build.nix -A shell-lzy -i bash
 
-python -m unittest discover -s ./tests | tee ./test_output
+python -m unittest discover -s ./tests/v2 | tee ./test_output
