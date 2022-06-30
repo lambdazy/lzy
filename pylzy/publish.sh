999d3ea9a7ce32699952a9e7cb63adacb6347fe0
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-06-30 17:29:16 +0300

    Clean up scripts a bit more

--- pylzy/publish.sh
+++ pylzy/publish.sh
@@ -8,6 +8,6 @@ export TWINE_USERNAME="__token__"
 # **token has to be provided by person or bot who runs the script**
 # otherwise script will stop and ask for token
 #
-# export TWINE_PASSWORD="<provide token here>" will stop and ask for token
+# export TWINE_PASSWORD="<provide token here>"
 
 python -m twine upload dist/* --verbose
