40e36ef9b2fc3c6ac69e861a7032c717e06b91ca
Nikita Sinyachenko <53365187+T-y-c-o-o-n@users.noreply.github.com>
2022-07-01 14:58:06 +0300

    Add iam in test bootstrap (#322)
    * LzyIAM: added constructor from context
    
    * added IAMThreadContext to LocalRunner
    
    * reduced channel shutdown time
    
    Co-authored-by: Nikita Siniachenko <ntpmsoakk070601@gmail.com>

--- iam/src/main/java/ai/lzy/iam/LzyIAM.java
+++ iam/src/main/java/ai/lzy/iam/LzyIAM.java
@@ -1,5 +1,13 @@
 package ai.lzy.iam;
 
+import ai.lzy.iam.configs.ServiceConfig;
+import ai.lzy.iam.grpc.interceptors.AuthServerInterceptor;
+import ai.lzy.iam.grpc.service.LzyABSService;
+import ai.lzy.iam.grpc.service.LzyASService;
+import ai.lzy.iam.grpc.service.LzyAuthService;
+import ai.lzy.iam.grpc.service.LzySubjectService;
+import ai.lzy.iam.storage.impl.DbAuthService;
+import ai.lzy.model.grpc.ChannelBuilder;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.netty.NettyServerBuilder;
@@ -9,14 +17,6 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
-import ai.lzy.iam.configs.ServiceConfig;
-import ai.lzy.iam.grpc.interceptors.AuthServerInterceptor;
-import ai.lzy.iam.grpc.service.LzyABSService;
-import ai.lzy.iam.grpc.service.LzyASService;
-import ai.lzy.iam.grpc.service.LzyAuthService;
-import ai.lzy.iam.grpc.service.LzySubjectService;
-import ai.lzy.iam.storage.impl.DbAuthService;
-import ai.lzy.model.grpc.ChannelBuilder;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -32,33 +32,51 @@ public class LzyIAM {
         LOG = LogManager.getLogger(LzyIAM.class);
     }
 
+    private final Server iamServer;
+
+    public LzyIAM(ApplicationContext context) {
+        ServiceConfig config = context.getBean(ServiceConfig.class);
+        ServerBuilder<?> builder = NettyServerBuilder.forPort(config.getServerPort())
+                .permitKeepAliveWithoutCalls(true)
+                .permitKeepAliveTime(ChannelBuilder.KEEP_ALIVE_TIME_MINS_ALLOWED, TimeUnit.MINUTES);
+        AuthServerInterceptor internalAuthInterceptor =
+                new AuthServerInterceptor(context.getBean(DbAuthService.class));
+        LzyASService accessService = context.getBean(LzyASService.class);
+        LzyABSService accessBindingService = context.getBean(LzyABSService.class);
+        LzySubjectService subjectService = context.getBean(LzySubjectService.class);
+        LzyAuthService authService = context.getBean(LzyAuthService.class);
+        builder.intercept(internalAuthInterceptor);
+        builder.addService(accessService);
+        builder.addService(accessBindingService);
+        builder.addService(subjectService);
+        builder.addService(authService);
+
+        this.iamServer = builder.build();
+    }
+
+    public void start() throws IOException {
+        iamServer.start();
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("gRPC server is shutting down!");
+            iamServer.shutdown();
+        }));
+    }
+
+    public void close() {
+        iamServer.shutdownNow();
+    }
+
+    public void awaitTermination() throws InterruptedException {
+        iamServer.awaitTermination();
+    }
+
     public static void main(String[] args) throws IOException, InterruptedException {
         try (ApplicationContext context = ApplicationContext.run()) {
             try {
-                ServiceConfig config = context.getBean(ServiceConfig.class);
-                ServerBuilder<?> builder = NettyServerBuilder.forPort(config.getServerPort())
-                        .permitKeepAliveWithoutCalls(true)
-                        .permitKeepAliveTime(ChannelBuilder.KEEP_ALIVE_TIME_MINS_ALLOWED, TimeUnit.MINUTES);
-                AuthServerInterceptor internalAuthInterceptor =
-                        new AuthServerInterceptor(context.getBean(DbAuthService.class));
-                LzyASService accessService = context.getBean(LzyASService.class);
-                LzyABSService accessBindingService = context.getBean(LzyABSService.class);
-                LzySubjectService subjectService = context.getBean(LzySubjectService.class);
-                LzyAuthService authService = context.getBean(LzyAuthService.class);
-                builder.intercept(internalAuthInterceptor);
-                builder.addService(accessService);
-                builder.addService(accessBindingService);
-                builder.addService(subjectService);
-                builder.addService(authService);
+                final LzyIAM lzyIAM = new LzyIAM(context);
 
-                final Server server = builder.build();
-                server.start();
-                Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-                    System.out.println("gRPC server is shutting down!");
-                    server.shutdown();
-                }));
-
-                server.awaitTermination();
+                lzyIAM.start();
+                lzyIAM.awaitTermination();
             } catch (NoSuchBeanException e) {
                 LOG.info("Shutdown");
             }
---
+++ test/src/test/java/ai/lzy/test/impl/IAMThreadContext.java
@@ -0,0 +1,117 @@
+package ai.lzy.test.impl;
+
+import ai.lzy.iam.LzyIAM;
+import ai.lzy.iam.configs.ServiceConfig;
+import ai.lzy.model.grpc.ChannelBuilder;
+import ai.lzy.priv.v1.LzyAccessServiceGrpc;
+import ai.lzy.priv.v1.LzySubjectServiceGrpc;
+import ai.lzy.test.LzyIAMTestContext;
+import ai.lzy.v1.LzyAccessBindingServiceGrpc;
+import ai.lzy.v1.LzyAuthenticateServiceGrpc;
+import ai.lzy.whiteboard.api.SnapshotApi;
+import com.google.common.net.HostAndPort;
+import io.grpc.ConnectivityState;
+import io.grpc.ManagedChannel;
+import io.micronaut.context.ApplicationContext;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+@SuppressWarnings("UnstableApiUsage")
+public class IAMThreadContext implements LzyIAMTestContext {
+
+    private static final Duration IAM_STARTUP_TIME = Duration.ofSeconds(10);
+    private static final Duration CHANNEL_SHUTDOWN_TIME = Duration.ofSeconds(5);
+    private static final int IAM_PORT = 8443;
+    private static final int USER_LIMIT = 60;
+
+    private LzyAccessServiceGrpc.LzyAccessServiceBlockingStub lzyAccessServiceClient;
+    private LzySubjectServiceGrpc.LzySubjectServiceBlockingStub lzySubjectServiceClient;
+    private LzyAccessBindingServiceGrpc.LzyAccessBindingServiceBlockingStub lzyAccessBindingServiceBlockingStub;
+    private LzyAuthenticateServiceGrpc.LzyAuthenticateServiceBlockingStub lzyAuthenticateServiceBlockingStub;
+    private LzyIAM lzyIAM;
+    private ManagedChannel channel;
+
+    @Override
+    public HostAndPort address() {
+        return HostAndPort.fromParts("localhost", IAM_PORT);
+    }
+
+    @Override
+    public LzyAccessServiceGrpc.LzyAccessServiceBlockingStub accessServiceClient() {
+        return lzyAccessServiceClient;
+    }
+
+    @Override
+    public LzySubjectServiceGrpc.LzySubjectServiceBlockingStub subjectServiceClient() {
+        return lzySubjectServiceClient;
+    }
+
+    @Override
+    public LzyAccessBindingServiceGrpc.LzyAccessBindingServiceBlockingStub accessBindingServiceClient() {
+        return lzyAccessBindingServiceBlockingStub;
+    }
+
+    @Override
+    public LzyAuthenticateServiceGrpc.LzyAuthenticateServiceBlockingStub authenticateServiceClient() {
+        return lzyAuthenticateServiceBlockingStub;
+    }
+
+    @Override
+    public void init() {
+        final ServiceConfig serviceConfig = new ServiceConfig();
+        serviceConfig.setServerPort(IAM_PORT);
+        serviceConfig.setUserLimit(USER_LIMIT);
+        Map<String, Object> appProperties = Map.of(
+                "serviceConfig", serviceConfig
+        );
+        try (ApplicationContext context = ApplicationContext.run(appProperties)) {
+            var logger = LogManager.getLogger(SnapshotApi.class);
+            logger.info("Starting LzyIAM on port {}...", IAM_PORT);
+
+            try {
+                lzyIAM = new LzyIAM(context);
+                lzyIAM.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        channel = ChannelBuilder
+                .forAddress("localhost", IAM_PORT)
+                .usePlaintext()
+                .enableRetry(LzySubjectServiceGrpc.SERVICE_NAME)
+                .enableRetry(LzyAccessServiceGrpc.SERVICE_NAME)
+                .build();
+        lzySubjectServiceClient = LzySubjectServiceGrpc.newBlockingStub(channel)
+                .withWaitForReady()
+                .withDeadlineAfter(IAM_STARTUP_TIME.getSeconds(), TimeUnit.SECONDS);
+        lzyAccessServiceClient = LzyAccessServiceGrpc.newBlockingStub(channel)
+                .withWaitForReady()
+                .withDeadlineAfter(IAM_STARTUP_TIME.getSeconds(), TimeUnit.SECONDS);
+        lzyAccessBindingServiceBlockingStub = LzyAccessBindingServiceGrpc.newBlockingStub(channel)
+                .withWaitForReady()
+                .withDeadlineAfter(IAM_STARTUP_TIME.getSeconds(), TimeUnit.SECONDS);
+        lzyAuthenticateServiceBlockingStub = LzyAuthenticateServiceGrpc.newBlockingStub(channel)
+                .withWaitForReady()
+                .withDeadlineAfter(IAM_STARTUP_TIME.getSeconds(), TimeUnit.SECONDS);
+        while (channel.getState(true) != ConnectivityState.READY) {
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100));
+        }
+    }
+
+    @Override
+    public void close() {
+        channel.shutdown();
+        try {
+            channel.awaitTermination(CHANNEL_SHUTDOWN_TIME.getSeconds(), TimeUnit.SECONDS);
+            lzyIAM.close();
+            lzyIAM.awaitTermination();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
