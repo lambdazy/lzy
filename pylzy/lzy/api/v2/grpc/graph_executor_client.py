e92843de5ea89b22d047c17e443801ecd7366a1e
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-03 01:25:35 +0300

    Delete grpc_ prefix in some files names

--- pylzy/lzy/api/v2/grpc/graph_executor_client.py
+++ pylzy/lzy/api/v2/grpc/graph_executor_client.py
@@ -1,69 +1,30 @@
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
 from typing import List, Optional
 
-# -------------------------------------------- Graph status
-from lzy.proto.bet.priv.v2 import TaskSpec
+from lzy.api.v2.grpc.graph_executor_client import (
+    GraphExecutionStatus,
+    GraphExecutorClient,
+    TaskSpec,
+)
 
 
-class Status(ABC):
-    pass
-
-
-class Waiting(Status):
-    pass
-
-
-class TaskStatus:
-    # Info about task from scheduler
-    pass
-
-
-class TaskExecutionStatus:
-    task_description_id: str
-    progress: TaskStatus
-
-
-class Executing(Status):
-    executing_tasks: List[TaskExecutionStatus]
-
-
-class Completed(Status):
-    pass
-
-
-class Failed(Status):
-    pass
-
-
-# -------------------------------------------- Graph status
-
-
-@dataclass
-class GraphExecutionStatus:
-    workflow_id: str
-    graph_id: str
-    status: Status
-
-
-class GraphExecutorClient(ABC):
-    @abstractmethod
+class GrpcGraphExecutorClient(GraphExecutorClient):
     def execute(
         self,
         workflow_id: str,
         tasks: List[TaskSpec],
         parent_graph_id: Optional[str] = None,
     ) -> GraphExecutionStatus:
+        # TODO: implement
         pass
 
-    @abstractmethod
     def status(self, workflow_id: str, graph_id: str) -> GraphExecutionStatus:
+        # TODO: implement
         pass
 
-    @abstractmethod
     def stop(self, workflow_id: str, graph_id: str, issue: str) -> GraphExecutionStatus:
+        # TODO: implement
         pass
 
-    @abstractmethod
     def list(self, workflow_id: str) -> List[GraphExecutionStatus]:
+        # TODO: implement
         pass
