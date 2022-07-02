bdf47e138f2d934d4c590c8df7d4f35e5d9857bb
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-02 03:10:57 +0300

    Use generated dataclasses where needed, drop custom ones

--- pylzy/lzy/api/v2/servant/model/slot.py
+++ pylzy/lzy/api/v2/servant/model/slot.py
@@ -1,82 +1,9 @@
-import json
-from abc import ABC, abstractmethod
-from dataclasses import dataclass
-from enum import Enum
+from lzy.proto.bet.priv.v2 import SlotDirection
 
 
-class Media(Enum):
-    FILE = 1
-    PIPE = 2
-    ARG = 3
-
-    def to_json(self) -> str:
-        return self.name
-
-
-class Direction(Enum):
-    INPUT = 1
-    OUTPUT = 2
-
-    def to_json(self) -> str:
-        return self.value
-
-    @staticmethod
-    def opposite(direction):
-        map_to_opposite = {
-            Direction.INPUT: Direction.OUTPUT,
-            Direction.OUTPUT: Direction.INPUT,
-        }
-        return map_to_opposite[direction]
-
-
-class DataSchema:
-    # noinspection PyMethodMayBeStatic
-    # pylint: disable=no-self-use
-    def to_json(self) -> str:
-        return "not implemented yet"
-
-
-# actually Slot should be just marked as
-# @dataclass(frozen=True)
-# but mypy is broken here a bit, so workaround with mixin is needed:
-# https://stackoverflow.com/questions/69330256/how-to-get-an-abstract-dataclass-to-pass-mypy
-# https://github.com/python/mypy/issues/5374#issuecomment-568335302
-@dataclass(frozen=True)
-class SlotDataclassMixin:
-    name: str
-
-
-class Slot(SlotDataclassMixin, ABC):
-    @property
-    @abstractmethod
-    def direction(self) -> Direction:
-        pass
-
-    @property
-    @abstractmethod
-    def media(self) -> Media:
-        pass
-
-    @property
-    def content_type(self) -> DataSchema:
-        return DataSchema()
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "media": self.media.to_json(),
-            "direction": self.direction.to_json(),
-            "contentType": self.content_type.to_json(),
-        }
-
-    def to_json(self):
-        return json.dumps(
-            {
-                # "name": "",  # FIXME: this is for touch
-                "media": self.media.to_json(),
-                "direction": self.direction.to_json(),
-                "contentType": self.content_type.to_json(),
-            },
-            sort_keys=True,
-            indent=3,
-        )
+def opposite(direction: SlotDirection):
+    map_to_opposite = {
+        SlotDirection.INPUT: SlotDirection.OUTPUT,
+        SlotDirection.OUTPUT: SlotDirection.INPUT,
+    }
+    return map_to_opposite[direction]
