31ac15e5e6f9b04c1bf78bfa4a5547dbf5a9aa52
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-03 00:37:42 +0300

    Refactor and drop not needed things

--- pylzy/lzy/api/v2/grpc/grpc_graph_executor_client.py
+++ pylzy/lzy/api/v2/grpc/grpc_graph_executor_client.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from lzy.api.v2.grpc.servant.api.graph_executor_client import (
+from lzy.api.v2.grpc.graph_executor_client import (
     GraphExecutionStatus,
     GraphExecutorClient,
     TaskSpec,
