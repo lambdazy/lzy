3f9a2489d20d0530dd228af0f1762e514c6525de
atem11 <atem11@yandex-team.ru>
2022-07-07 17:54:03 +0300

    Init InternalUserInserter

---
+++ iam/src/main/java/ai/lzy/iam/configs/InternalUserConfig.java
@@ -0,0 +1,43 @@
+package ai.lzy.iam.configs;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+
+@ConfigurationProperties("internal")
+public class InternalUserConfig {
+    private String userName;
+    private String credentialName;
+    private String credentialValue;
+    private String credentialType;
+
+    public String userName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String credentialName() {
+        return credentialName;
+    }
+
+    public void setCredentialName(String credentialName) {
+        this.credentialName = credentialName;
+    }
+
+    public String credentialValue() {
+        return credentialValue;
+    }
+
+    public void setCredentialValue(String credentialValue) {
+        this.credentialValue = credentialValue;
+    }
+
+    public String credentialType() {
+        return credentialType;
+    }
+
+    public void setCredentialType(String credentialType) {
+        this.credentialType = credentialType;
+    }
+}
---
+++ iam/src/main/java/ai/lzy/iam/storage/db/InternalUserInserter.java
@@ -0,0 +1,56 @@
+package ai.lzy.iam.storage.db;
+
+import ai.lzy.iam.configs.InternalUserConfig;
+import ai.lzy.iam.storage.Storage;
+import ai.lzy.iam.utils.UserVerificationType;
+import io.micronaut.context.annotation.Requires;
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Singleton
+@Requires(beans = Storage.class)
+public class InternalUserInserter {
+
+    @Inject
+    private Storage storage;
+
+    public void addOrUpdateInternalUser(InternalUserConfig config) {
+        try (final Connection connection = storage.connect()) {
+            PreparedStatement st = connection.prepareStatement(
+                    "INSERT INTO users (user_id, auth_provider, provider_user_id, access_type) "
+                            + "VALUES (?, ?, ?, ?) "
+                            + "ON CONFLICT DO NOTHING;"
+            );
+            int parameterIndex = 0;
+            st.setString(++parameterIndex, config.userName());
+            st.setString(++parameterIndex, "INTERNAL_AGENT");
+            st.setString(++parameterIndex, config.userName());
+            st.setString(++parameterIndex, UserVerificationType.ACCESS_ALLOWED.toString());
+            st.executeUpdate();
+
+            st = connection.prepareStatement(
+                    "INSERT INTO credentials (name, \"value\", user_id, type) "
+                            + "VALUES (?, ?, ?, ?) "
+                            + "ON CONFLICT (name, user_id) DO UPDATE SET "
+                            + "name = ?, "
+                            + "\"value\" = ?, "
+                            + "user_id = ?, "
+                            + "type = ? "
+                            + ";"
+            );
+            parameterIndex = 0;
+            for (int i = 0; i < 2; i++) {
+                st.setString(++parameterIndex, config.credentialName());
+                st.setString(++parameterIndex, config.credentialValue());
+                st.setString(++parameterIndex, config.userName());
+                st.setString(++parameterIndex, config.credentialType());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
