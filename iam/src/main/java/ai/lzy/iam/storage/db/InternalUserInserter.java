6e50fa5a32f4a9f88598df811172d109712e1fb8
atem11 <atem11@yandex-team.ru>
2022-07-08 11:44:48 +0300

    Add bindings

--- iam/src/main/java/ai/lzy/iam/storage/db/InternalUserInserter.java
+++ iam/src/main/java/ai/lzy/iam/storage/db/InternalUserInserter.java
@@ -1,6 +1,8 @@
 package ai.lzy.iam.storage.db;
 
 import ai.lzy.iam.configs.InternalUserConfig;
+import ai.lzy.iam.resources.Role;
+import ai.lzy.iam.resources.impl.Root;
 import ai.lzy.iam.storage.Storage;
 import ai.lzy.iam.utils.UserVerificationType;
 import io.micronaut.context.annotation.Requires;
@@ -24,16 +26,8 @@ public class InternalUserInserter {
                     "INSERT INTO users (user_id, auth_provider, provider_user_id, access_type) "
                             + "VALUES (?, ?, ?, ?) "
                             + "ON CONFLICT DO NOTHING;"
-            );
-            int parameterIndex = 0;
-            st.setString(++parameterIndex, config.userName());
-            st.setString(++parameterIndex, "INTERNAL_AGENT");
-            st.setString(++parameterIndex, config.userName());
-            st.setString(++parameterIndex, UserVerificationType.ACCESS_ALLOWED.toString());
-            st.executeUpdate();
 
-            st = connection.prepareStatement(
-                    "INSERT INTO credentials (name, \"value\", user_id, type) "
+                            + "INSERT INTO credentials (name, \"value\", user_id, type) "
                             + "VALUES (?, ?, ?, ?) "
                             + "ON CONFLICT (name, user_id) DO UPDATE SET "
                             + "name = ?, "
@@ -41,14 +35,30 @@ public class InternalUserInserter {
                             + "user_id = ?, "
                             + "type = ? "
                             + ";"
+
+                            + "INSERT INTO user_resource_roles (user_id, resource_id, resource_type, role) "
+                            + "VALUES (?, ?, ?, ?) "
+                            + "ON CONFLICT DO NOTHING;"
             );
-            parameterIndex = 0;
+            int parameterIndex = 0;
+            st.setString(++parameterIndex, config.userName());
+            st.setString(++parameterIndex, "INTERNAL_AGENT");
+            st.setString(++parameterIndex, config.userName());
+            st.setString(++parameterIndex, UserVerificationType.ACCESS_ALLOWED.toString());
+
             for (int i = 0; i < 2; i++) {
                 st.setString(++parameterIndex, config.credentialName());
                 st.setString(++parameterIndex, config.credentialValue());
                 st.setString(++parameterIndex, config.userName());
                 st.setString(++parameterIndex, config.credentialType());
             }
+
+            Root root = new Root();
+            st.setString(++parameterIndex, config.userName());
+            st.setString(++parameterIndex, root.resourceId());
+            st.setString(++parameterIndex, root.type());
+            st.setString(++parameterIndex, Role.LZY_INTERNAL_USER.role());
+            st.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
