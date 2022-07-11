029065ad95d3c438ef68c79552bf8673c942bade
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-07-11 13:28:08 +0300

    fixed required pod anti affinity

--- deployment/tf/modules/lzy_common/lzy_server.tf
+++ deployment/tf/modules/lzy_common/lzy_server.tf
@@ -221,6 +221,11 @@ resource "kubernetes_deployment" "server" {
                   operator = "In"
                   values   = local.all-services-k8s-app-labels
                 }
+              }
+              topology_key = "kubernetes.io/hostname"
+            }
+            required_during_scheduling_ignored_during_execution {
+              label_selector {
                 match_expressions {
                   key      = "app.kubernetes.io/managed-by"
                   operator = "In"
--- deployment/tf/modules/lzy_common/whiteboard.tf
+++ deployment/tf/modules/lzy_common/whiteboard.tf
@@ -93,6 +93,11 @@ resource "kubernetes_deployment" "whiteboard" {
                   operator = "In"
                   values   = local.all-services-k8s-app-labels
                 }
+              }
+              topology_key = "kubernetes.io/hostname"
+            }
+            required_during_scheduling_ignored_during_execution {
+              label_selector {
                 match_expressions {
                   key      = "app.kubernetes.io/managed-by"
                   operator = "In"
