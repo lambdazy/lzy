7960d80a40f62492262f71409a0b979849a1775c
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-07-11 12:20:56 +0300

    fixed parsing UUID from sessionId

--- server/src/main/java/ai/lzy/server/mem/SimpleInMemAuthenticator.java
+++ server/src/main/java/ai/lzy/server/mem/SimpleInMemAuthenticator.java
@@ -82,7 +82,7 @@ public class SimpleInMemAuthenticator implements Authenticator {
     }
 
     @Override
-    public boolean checkBackOfficeSession(UUID sessionId, String userId) {
+    public boolean checkBackOfficeSession(String sessionId, String userId) {
         return true;
     }
 
