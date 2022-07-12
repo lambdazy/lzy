c60123b54f12c7a46d8bf245a64a2493f567157b
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-13 01:19:06 +0300

    Generate stubs for lzy-graph-executor.proto

--- pylzy/gen_bet_proto.sh
+++ pylzy/gen_bet_proto.sh
@@ -15,16 +15,15 @@ print_green "Generating betterproto dataclasses stubs in $out"
 cd "$proto_path"
 # generation of some proto files is disabled for now
 find . -iname "*.proto" -type f \
-       ! -name "lzy-graph-executor.proto" \
        ! -name "lzy-server.proto" \
        ! -name "lzy-kharon.proto" \
        -exec python -m grpc_tools.protoc -I . \
                     --python_betterproto_out="$out" \
                     '{}' +
 
-find "$proto_out/bet" -type f \( -iname "*.py" -o -iname "*.pyi" \) -print
-
 cd "$out"
 mv lzy/proto/* ./
 find . -name "__init__.py" -exec rm -v {} +
 rmdir -p "lzy/proto"
+
+find "$proto_out/bet" -type f \( -iname "*.py" -o -iname "*.pyi" \) -print
