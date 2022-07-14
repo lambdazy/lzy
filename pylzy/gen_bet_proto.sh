cdf4fba119ef8718bb00bc4f8699f184016544b2
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-14 18:01:17 +0300

    Fix proto generation, hardcode better proto dataclasses

---
+++ pylzy/gen_bet_proto.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env nix-shell
+#! nix-shell build.nix -A shell -i bash
+
+set -eux
+
+source ./util.sh
+
+
+out="$proto_out/bet"
+print_green "Generating betterproto dataclasses stubs in $out"
+
+
+[ ! -d "$out" ] && mkdir -p "$out"
+
+cd "$proto_path"
+# generation of some proto files is disabled for now
+find . -iname "*.proto" -type f \
+       ! -name "lzy-graph-executor.proto" \
+       ! -name "lzy-server.proto" \
+       ! -name "lzy-kharon.proto" \
+       -exec python -m grpc_tools.protoc -I . \
+                    --python_betterproto_out="$out" \
+                    '{}' +
+
+find "$proto_out/bet" -type f \( -iname "*.py" -o -iname "*.pyi" \) -print
+
+cd "$out"
+mv lzy/proto/* ./
+find . -name "__init__.py" -exec rm -v {} +
+rmdir -p "lzy/proto"
--- pylzy/gen_proto.sh
+++ pylzy/gen_proto.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env nix-shell
 #! nix-shell build.nix -A shell -i bash
 
-set -ux
+set -eux
 
 source ./util.sh
 
@@ -9,48 +9,27 @@ source ./util.sh
 # proto_out="lzy/proto"
 # proto_path="../model/src/main/proto/"
 
-[ ! -d "$proto_out" ] && mkdir -p "$proto_out/bet"
+[ -d "$proto_out" ] || mkdir -p "$proto_out"
 
 # check mypy, it's hack actually but for some reason it's not installed
 # ok with nix
-python -m mypy_protobuf 1>/dev/null 2>&1
-[ $? -ne 0 ] && pip install mypy-protobuf
+python -m mypy_protobuf 1>/dev/null 2>&1\
+    || pip install mypy-protobuf
 
 print_green "Generating protobuf, grpclib and mypy proto stubs"
 
-cd "$proto_path" && \
-    find . -iname "*.proto" -type f \
-        -print \
-        -exec python -m grpc_tools.protoc \
-                     -I . \
-                     --python_out="$proto_out" \
-                     --mypy_out="$proto_out" \
-                     --grpclib_python_out="$proto_out" \
-                     --proto_path="$proto_path" \
-                     '{}' +
+cd "$proto_path"
+find . -iname "*.proto" -type f \
+       -exec python -m grpc_tools.protoc -I . \
+                    --python_out="$proto_out" \
+                    --mypy_out="$proto_out" \
+                    --grpclib_python_out="$proto_out" \
+                    --proto_path="$proto_path" \
+                    '{}' +
 
-cd "$proto_out" && \
-   mv lzy/proto/* ./ && \
-   rmdir -p "lzy/proto"
-
-out="$proto_out/bet"
-print_green "Generating betterproto dataclasses stubs in $out"
-
-cd "$proto_path" && \
-    find . \
-        -iname "*.proto" -type f \
-        ! -name "lzy-graph-executor.proto" \
-        ! -name "lzy-server.proto" \
-        ! -name "lzy-kharon.proto" \
-        -exec python -m grpc_tools.protoc \
-                     -I . \
-                     --python_betterproto_out="$out" \
-                     '{}' +
-
-cd "$out" && \
-   mv lzy/proto/* ./ && \
-   find . -name "__init__.py" -exec rm -v {} + && \
-   rmdir -p "lzy/proto"
+cd "$proto_out"
+mv lzy/proto/* ./
+rmdir -p "lzy/proto"
 
 print_green "Generated next proto stubs:"
 println "$proto_out"
