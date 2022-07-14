08eb3693afcb1e190eb31d6cff4d80eb583368dc
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-14 17:57:33 +0300

    Add some grpc calls to rewrite Run.java

---
+++ pylzy/lzy/api/v2/grpc/servant/grpc_calls.py
@@ -0,0 +1,100 @@
+import time
+from typing import Tuple
+
+import dataclass
+from grpclib.client import Channel
+
+from lzy.proto.bet.priv.v2 import (
+    Auth,
+    ChannelCommad,
+    SlotAssignment,
+    TaskProgress,
+    TaskSpec,
+    Zygote,
+)
+from lzy.proto.priv.v2.lzy_fs_grpc import LzyFsStub
+from lzy.proto.priv.v2.lzy_server_grpc import LzyServerStub
+
+
+@dataclass.dataclass
+class Measurement:
+    time_ns: int = 0
+
+
+def creds():
+    raise NotImplementedError("not implemented yet")
+    return "localhost", "7777"
+
+
+class Run:
+    def __init__(self):
+        self._chan = Channel(*creds())
+        self.auth = Auth("", "")  # TODO
+        self.server = LzyServerStub(self._chan)
+        self.servant_fs = LzyFsStub(self._chan)
+        self.pid = -1
+
+    async def exec(self, zygote: Zygote) -> Measurement:
+        raise NotImplementedError("not implemented yet")
+
+        # preparing
+        zygote.name += f"_{self.pid}"
+        assignments = [
+            SlotAssignment(
+                slot=slot,
+                binding=self.resolve_slot(slot),
+            )
+            for slot in zygote.slots
+        ]
+        task_spec = TaskSpec(
+            auth=self.auth,
+            zygote=zygote,
+            assignments=assignments,
+        )
+
+        # run
+        # TODO[ottergottaott]: measure time
+        rc, description = await self.start(task_spec)
+
+        #  MetricEventLogger.log(
+        #      new MetricEvent(
+        #          "time from Task start to Task finish",
+        #          Map.of("metric_type", "task_metric"),
+        #          finishTimeMillis - startTimeMillis
+        #      )
+        #  );
+        #  LOG.info("Run:: Task finished RC = {}, Description = {}", rc, description);
+        #  if (rc != 0) {
+        #      System.err.print(description);
+        #  } else {
+        #      communicationLatch.await(); // waiting for slots to finish communication
+        #  }
+        #  channels.forEach(this::destroyChannel);
+        #  return rc;
+        # }
+
+        return Measurement()
+
+    async def start(self, task_spec: TaskSpec) -> Tuple[int, str]:
+        exit: int = 0
+        description: str = ""
+
+        with self.server.Start.open() as stream:
+            await stream.send_request()
+            await stream.send_message(task_spec)
+
+            async for task_progress in stream:
+                task_progress: TaskProgress = task_progress
+                if (
+                    task_progress.status is TaskProgress.ERROR
+                    or task_progress.status is TaskProgress.SUCCESS
+                ):
+                    # TODO[ottergottaott]: pretty print here
+                    print(task_progress)
+                    rc_ = task_progress.rc
+                    description = task_progress.description
+
+            return rc_, description
+
+    def close(self):
+        self._chan.close()
