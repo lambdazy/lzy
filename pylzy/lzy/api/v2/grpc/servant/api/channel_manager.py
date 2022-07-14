cbf8dfce2d5f9146ca3d59ca10e6a9c6269849a7
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-14 17:57:33 +0300

    Add some grpc calls to rewrite Run.java

--- pylzy/lzy/api/v2/grpc/servant/api/channel_manager.py
+++ pylzy/lzy/api/v2/grpc/servant/api/channel_manager.py
@@ -1,23 +1,134 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Dict, Optional
 
-from lzy.api.v2.servant.model.channel import Channel
+from lzy.api.v2.grpc.servant.grpc_calls import Run
+from lzy.api.v2.servant.model.channel import (
+    Channel,
+    DirectChannelSpec,
+    SnapshotChannelSpec,
+)
 from lzy.api.v2.servant.model.slot import Slot
+from lzy.proto.bet.priv.v2 import (
+    Auth,
+    Channel,
+    ChannelCommand,
+    ChannelCreate,
+    ChannelDestroy,
+    ChannelStatus,
+    CreateSlotCommand,
+    DataScheme,
+    DirectChannelSpec,
+    SlotCommand,
+    SlotCommandStatus,
+    SnapshotChannelSpec,
+)
+from lzy.proto.priv.v2.lzy_fs_grpc import LzyFsStub
+from lzy.proto.priv.v2.lzy_server_grpc import LzyServerStub
 
 
-class ChannelManager(ABC):
-    @abstractmethod
-    def direct_channel(self, name: str) -> Channel:
-        pass
+class ChannelManager:
+    def __init__(
+        self,
+        auth: Auth,
+        server: LzyServerStub,
+        servant_fs: LzyFsStub,
+    ):
+        self.server = server
+        self.servant_fs = servant_fs
+        self.auth = Auth("", "")  # TODO
+        self.pid = -1
 
-    @abstractmethod
-    def snapshot_channel(self, snapshot_id: str, entry_id: str) -> Channel:
-        pass
+    async def create_direct_channel(
+        self,
+        name: str,
+        content_type: DataScheme,
+    ) -> ChannelStatus:
+        return await self.create_channel(
+            name,
+            content_type,
+            direct=DirectChannelSpec(),
+        )
 
-    @abstractmethod
-    def destroy(self, channel_name: Optional[str] = None) -> bool:
-        pass
+    async def create_snapshot_channel(
+        self,
+        name: str,
+        content_type: DataScheme,
+        snapshot_id: str,
+        entry_id: str,
+    ) -> ChannelStatus:
+        snapshot = SnapshotChannelSpec(snapshot_id, entry_id)
+        return await self.create_channel(
+            name,
+            content_type,
+            snapshot=snapshot,
+        )
 
-    @abstractmethod
-    def touch(self, slot: Slot, channel: Channel):
-        pass
+    # kwargs trick:
+    async def create_channel(
+        self,
+        name: str,
+        content_type: DataScheme,
+        **type_,
+    ) -> ChannelStatus:
+        # LOG.info("Create channel `{}` for slot `{}`.", channelName, slot.name());
+        create = ChannelCreate(
+            content_type=content_type,
+            **type_,
+        )
+        channel_cmd = ChannelCommand(
+            auth=self.auth,
+            channel_name=name,
+            create=create,
+        )
+        return await self.server.Channel(channel_cmd)
+
+    async def destroy_channel(self, channel_name: str):
+        destroy = ChannelDestroy()
+        channel_cmd = ChannelCommand(
+            auth=self.auth,
+            channel_name=channel_name,
+            destroy=destroy,
+        )
+        # TODO[aleksZubakov]: try/catch here?
+        return await self.server.Channel(channel_cmd)
+
+    async def create_slot(
+        self,
+        slot: Slot,
+        pid: int,
+        name: str,
+        pipe: bool,
+        channel_id: str,
+    ) -> SlotCommandStatus:
+        create = CreateSlotCommand(
+            slot=slot,
+            channel_id=channel_id,
+            is_pipe=pipe,
+        )
+        # LOG.info("Create {}slot `{}` ({}) for channel `{}` with taskId {}.",
+        #         pipe ? "pipe " : "", slotName, name, channelId, pid);
+        slot_cmd = SlotCommand(
+            tid=str(pid),
+            slot=name,
+            create=create,
+        )
+        return await self.servant_fs.ConfigureSlot(slot_cmd)
+
+    async def resolve_slot(self, slot: Slot) -> str:
+        # LOG.info("Resolving slot " + slot.name());
+        #
+        # final String binding;
+        # if (slot.media() == Slot.Media.ARG) {
+        #     binding = String.join(" ", command.getArgList().subList(1, command.getArgList().size()));
+        # } else if (bindings.containsKey(slot.name())) {
+        #     binding = "channel:" + bindings.get(slot.name());
+        # } else {
+        #     binding = "channel:" + resolveChannel(slot);
+        # }
+        # LOG.info("Slot " + slot.name() + " resolved to " + binding);
+        #
+        binding = ""
+        return binding
+
+    def close(self):
+        self._chan.close()
