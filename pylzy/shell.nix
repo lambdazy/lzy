227723e30e50f5376d03691cd8188a940849bf45
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-14 17:59:20 +0300

    Reorganise .proto files, add type stubs for generated proto fyles, refactor generation script

--- pylzy/shell.nix
+++ pylzy/shell.nix
@@ -1,5 +1,2 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/360d84a26f0cfca4941d92a89e2b359d425a6d3d.tar.gz") {} }:
-let
-  build = pkgs.callPackage ./build.nix {};
-in
-  build.shell
+{ }:
+(import ./build.nix {}).shell
