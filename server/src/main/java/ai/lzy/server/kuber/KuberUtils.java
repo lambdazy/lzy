c4b449d90efe4fd8d1e4abd992c99f90a137d7a8
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-07-15 14:58:09 +0300

    KuberServantsAllocator deletes lock pods by sessionId

--- server/src/main/java/ai/lzy/server/kuber/KuberUtils.java
+++ server/src/main/java/ai/lzy/server/kuber/KuberUtils.java
@@ -3,6 +3,8 @@ package ai.lzy.server.kuber;
 import io.kubernetes.client.openapi.models.V1Container;
 import io.kubernetes.client.openapi.models.V1Pod;
 import io.kubernetes.client.openapi.models.V1PodList;
+
+import java.util.Locale;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -12,7 +14,6 @@ public class KuberUtils {
     private KuberUtils() {
     }
 
-    @NotNull
     public static Optional<V1Pod> findPodByName(V1PodList podList, String name) {
         return podList
             .getItems()
@@ -23,12 +24,16 @@ public class KuberUtils {
             .findFirst();
     }
 
-    @NotNull
     public static Optional<V1Container> findContainerByName(V1Pod pod, String name) {
         return Objects.requireNonNull(pod.getSpec())
             .getContainers()
             .stream()
             .filter(c -> name.equals(c.getName()))
             .findFirst();
+    }
+
+    public static String kuberValidName(String name) {
+        // k8s pod name can only contain symbols [-a-z0-9]
+        return name.toLowerCase(Locale.ROOT).replaceAll("[^-a-z0-9]", "-");
     }
 }
