139b95f8347ec1e4ea029d5babc312c3366a976a
lindvv <lindvv@yandex.ru>
2022-07-25 16:23:14 +0300

    fix push-docker.sh

--- deployment/push-docker.sh
+++ deployment/push-docker.sh
@@ -53,7 +53,7 @@ if [[ $REBUILD = true ]]; then
 
     TEST_ENV_BASE="$(deployment/latest-docker-image-on-branches.sh test-env-base $BRANCH dev)"
     docker pull "$TEST_ENV_BASE"
-    TEST_ENV_BASE_TAG="$(echo $TEST_ENV_BASE_TAG | awk -F: '{print $2}')"
+    TEST_ENV_BASE_TAG="$(echo $TEST_ENV_BASE | awk -F: '{print $2}')"
   fi
   cd pylzy/ && ./gen_proto.sh && ./gen_bet_proto.sh && cd ..
   mvn clean install -DskipTests
