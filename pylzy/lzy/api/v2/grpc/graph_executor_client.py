d7d9f873035e33ab941bc9cd876b1d9eecfc539f
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-26 04:06:27 +0300

    Enable graph executor stubs, write up interface

--- pylzy/lzy/api/v2/grpc/graph_executor_client.py
+++ pylzy/lzy/api/v2/grpc/graph_executor_client.py
@@ -1,30 +1,53 @@
 from typing import List, Optional
 
-from lzy.api.v2.grpc.graph_executor_client import (
+from grpclib.client import Channel
+
+from ai.lzy.v1.graph import (
     GraphExecutionStatus,
-    GraphExecutorClient,
-    TaskSpec,
+    GraphExecuteRequest,
+    GraphListRequest,
+    TaskDesc,
+    GraphStatusRequest,
+    GraphStopRequest,
 )
+from ai.lzy.v1.graph.graph_executor_grpc import GraphExecutorStub
 
 
-class GrpcGraphExecutorClient(GraphExecutorClient):
-    def execute(
+class GraphExecutorClient:
+    def __init__(self, channel: Channel):
+        self.stub = GraphExecutorStub(channel)
+
+    async def execute(
         self,
         workflow_id: str,
-        tasks: List[TaskSpec],
+        tasks: List[TaskDesc],
         parent_graph_id: Optional[str] = None,
     ) -> GraphExecutionStatus:
-        # TODO: implement
-        pass
+        request = GraphExecuteRequest(
+            workflow_id,
+            workflow_id,
+            tasks,
+            parent_graph_id,
+            None,
+        )
+
+        return await self.stub.Execute(request)
 
     def status(self, workflow_id: str, graph_id: str) -> GraphExecutionStatus:
-        # TODO: implement
-        pass
+        request = GraphStatusRequest(
+            workflow_id,
+            graph_id,
+        )
+        return await self.stub.Status(request)
 
     def stop(self, workflow_id: str, graph_id: str, issue: str) -> GraphExecutionStatus:
-        # TODO: implement
-        pass
+        request = GraphStopRequest(
+            workflow_id,
+            graph_id,
+            issue,
+        )
+        return await self.stub.Stop(request)
 
     def list(self, workflow_id: str) -> List[GraphExecutionStatus]:
-        # TODO: implement
-        pass
+        request = GraphListRequest(workflow_id)
+        return (await self.stub.List(request)).graphs
