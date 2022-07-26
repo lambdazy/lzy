9434aaf93126310e2f6d6b7211ee8dfc4dff9681
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-07-26 16:53:23 +0300

    Clean up after rebase

--- pylzy/lint.sh
+++ pylzy/lint.sh
@@ -16,10 +16,7 @@ run() {
     println "Calling $type: $cmd_name"
 
     $cmd_name lzy/ tests/
-<<<<<<< HEAD
 
-=======
->>>>>>> 2db51cc7 (Fully working py stub generation scripts)
     _ex=$?
     print_cmd_exit $_ex $cmd_name
 
