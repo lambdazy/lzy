dfafc76c0ccb4347c8b20b41b6a67713ab8dce5f
Aleksey Zubakov <aleks.zubakov@gmail.com>
2022-08-09 18:10:48 +0300

    drom better proto generation script, fix clean.sh

--- pylzy/clean.sh
+++ pylzy/clean.sh
@@ -3,7 +3,7 @@
 echo "Clean up"
 
 # __pycache__
-find . -iname '__pycache__' -type d -exec rm -rvf '{}' +
+find . \( -iname '__pycache__' -o -iname '*.pyc' \) -type d -exec rm -rvf '{}' +
 
 # package build artefacts
 rm -rvf *.egg-info dist/ build/ lzy/lzy-servant-*.jar
@@ -15,7 +15,10 @@ rm -rvf .mypy_cache test_output
 rm -rvf _build/
 
 # mvn build artefacts
-rm -rvf target/ 
+rm -rvf target/
+
+# coverage report
+rm -vf ./.coverage ./coverage.svg
 
 # generated proto files
 [ -n "$1" ] || rm -rvf "$proto_out/priv" "$proto_out/v1" "ai/"
