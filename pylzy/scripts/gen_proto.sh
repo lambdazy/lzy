cbe6950e2869c0d3fb0f17c6a700dd9b9b34fd8e
imakunin <imakunin@yandex-team.ru>
2022-08-18 23:25:15 +0300

    extract workflow-api module

--- pylzy/scripts/gen_proto.sh
+++ pylzy/scripts/gen_proto.sh
@@ -6,7 +6,7 @@ set -eux
 src_dir="$(dirname $0)"
 source "$src_dir/util.sh"
 
-# this has to be declarared as env variable in mk-python-env.nix
+# this has to be declared as env variable in mk-python-env.nix
 # proto_out="lzy/proto"
 # proto_path="../model/src/main/proto/"
 
@@ -18,14 +18,26 @@ python -m mypy_protobuf 1>/dev/null 2>&1\
     || pip install mypy-protobuf
 
 print_green "Generating protobuf, grpclib and mypy proto stubs"
-cd "$proto_path"
+
+cd "$proto_workflow_path"
 find . -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . \
                     --python_out="$OLDPWD" \
                     --mypy_out="$OLDPWD" \
                     --grpclib_python_out="$OLDPWD" \
-                    --proto_path="$proto_path" \
+                    --proto_path="$proto_workflow_path" \
                     '{}' +
+cd "$OLDPWD"
+
+cd "$proto_model_path"
+find . -iname "*.proto" -type f \
+       -exec python -m grpc_tools.protoc -I . -I "$proto_workflow_path" \
+                    --python_out="$OLDPWD" \
+                    --mypy_out="$OLDPWD" \
+                    --grpclib_python_out="$OLDPWD" \
+                    --proto_path="$proto_model_path" \
+                    '{}' +
+
 cd "$OLDPWD"
 
 find "ai" -type d \
