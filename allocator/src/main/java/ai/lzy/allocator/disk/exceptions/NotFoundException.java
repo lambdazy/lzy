089c97b85af178e6e6318185621957f71a6e5eb3
KruchDmitriy <d-kruchinin@yandex-team.ru>
2022-09-01 11:47:46 +0300

    optimize not found exception + unspecified enum etry for MountType

--- allocator/src/main/java/ai/lzy/allocator/disk/exceptions/NotFoundException.java
+++ allocator/src/main/java/ai/lzy/allocator/disk/exceptions/NotFoundException.java
@@ -6,4 +6,9 @@ public class NotFoundException extends Exception {
     public NotFoundException(String message) {
         super(message);
     }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
 }
