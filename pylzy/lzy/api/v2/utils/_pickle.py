e00a0673e580456da56ab04393054f95ca468db6
ArtoLord <artolord@yandex-team.ru>
2022-09-06 18:07:04 +0300

    Fix tests

--- pylzy/lzy/api/v2/utils/_pickle.py
+++ pylzy/lzy/api/v2/utils/_pickle.py
@@ -12,6 +12,4 @@ def pickle(obj: T) -> str:
 
 def unpickle(base64_str: str, obj_type: Type[T] = None) -> T:
     t = cloudpickle.loads(base64.b64decode(base64_str.encode("ascii")))
-    if obj_type is not None and not isinstance(t, obj_type):
-        raise TypeError(f"cannot upickle value from {base64_str}: wrong type")
     return cast(T, t)
