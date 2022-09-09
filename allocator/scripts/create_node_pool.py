edfc4a6628800117febd589857f276203fd17155
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-09-09 19:03:32 +0300

    code from root extracted to functions

--- allocator/scripts/create_node_pool.py
+++ allocator/scripts/create_node_pool.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
 import os
 import sys
-import argparse
 from dataclasses import dataclass
 
 import yandexcloud
-from yandex.cloud.vpc.v1.subnet_service_pb2_grpc import *
-from yandex.cloud.vpc.v1.subnet_service_pb2 import *
-from yandex.cloud.vpc.v1.security_group_service_pb2_grpc import *
-from yandex.cloud.vpc.v1.security_group_service_pb2 import *
-from yandex.cloud.vpc.v1.security_group_pb2 import *
-from yandex.cloud.k8s.v1.cluster_service_pb2_grpc import *
 from yandex.cloud.k8s.v1.cluster_service_pb2 import *
-from yandex.cloud.k8s.v1.node_group_service_pb2_grpc import *
-from yandex.cloud.k8s.v1.node_group_service_pb2 import *
+from yandex.cloud.k8s.v1.cluster_service_pb2_grpc import *
 from yandex.cloud.k8s.v1.node_group_pb2 import *
+from yandex.cloud.k8s.v1.node_group_service_pb2 import *
+from yandex.cloud.k8s.v1.node_group_service_pb2_grpc import *
 from yandex.cloud.k8s.v1.node_pb2 import *
+from yandex.cloud.vpc.v1.security_group_service_pb2 import *
+from yandex.cloud.vpc.v1.security_group_service_pb2_grpc import *
+from yandex.cloud.vpc.v1.subnet_service_pb2 import *
+from yandex.cloud.vpc.v1.subnet_service_pb2_grpc import *
+
 from common import *
 
 
@@ -41,11 +40,6 @@ class CreateNodePoolConfig:
     memory: int
 
 
-with open('create_node_pool_config.yaml', 'r') as file:
-    data = file.read()
-config = strict_load_yaml(data, CreateNodePoolConfig)
-
-
 def parse_taint(taint_str: str):
     key, part_2 = taint_str.split("=")
     value, effect_str = part_2.split(":")
@@ -62,119 +56,129 @@ def parse_taint(taint_str: str):
     )
 
 
-node_pool_taints = map(parse_taint, config.node_pool_taints[1:-1].split(","))
-memory = config.memory * (1024 ** 3)  # GBs to Bytes
-disc_size = config.disc_size * (1024 ** 3)  # GBs to Bytes
-
-sdk = yandexcloud.SDK(iam_token=os.environ['YC_TOKEN'])
-subnet_service = sdk.client(SubnetServiceStub)
-sg_service = sdk.client(SecurityGroupServiceStub)
-cluster_service = sdk.client(ClusterServiceStub)
-node_group_service = sdk.client(NodeGroupServiceStub)
-
-ans = input("Are you sure you want to create node group with this configuration? (print 'YES!'): ")
-if ans != "YES!":
-    sys.exit()
-
-# ------------ K8S CLUSTER EXISTENCE CHECK ------------ #
-try:
-    cluster = cluster_service.Get(
-        GetClusterRequest(
-            cluster_id=config.cluster_id
+def get_cluster_name(cluster_id: str):
+    try:
+        cluster = cluster_service.Get(
+            GetClusterRequest(
+                cluster_id=cluster_id
+            )
         )
+        name = cluster.name
+    except grpc.RpcError as e:
+        if e.code() is grpc.StatusCode.NOT_FOUND:
+            # TODO: check cluster with same name existence before creating SGs
+            print("k8s cluster with id {} was not found!\n".format(cluster_id))
+            exit(1)
+        else:
+            raise e
+
+
+def get_security_group_id(sg_service: SecurityGroupService, name: str):
+    sgs = list(filter(
+        lambda sg: sg.name == name,
+        sg_service.List(ListSecurityGroupsRequest(folder_id=config.folder_id)).security_groups
+    ))
+    if len(sgs) == 0:
+        raise Exception("{} security group does not exist. (It must be created during script "
+                        "create_cluster.py --cluster-name {} ...)".format(name, cluster_name))
+    public_services_sg_id = sgs[0].id
+
+
+if __name__ == "__main__":
+    with open('create_node_pool_config.yaml', 'r') as file:
+        data = file.read()
+    config = strict_load_yaml(data, CreateNodePoolConfig)
+
+    node_pool_taints = map(parse_taint, config.node_pool_taints[1:-1].split(","))
+    memory = config.memory * (1024 ** 3)  # GBs to Bytes
+    disc_size = config.disc_size * (1024 ** 3)  # GBs to Bytes
+
+    sdk = yandexcloud.SDK(iam_token=os.environ['YC_TOKEN'])
+    subnet_service = sdk.client(SubnetServiceStub)
+    sg_service = sdk.client(SecurityGroupServiceStub)
+    cluster_service = sdk.client(ClusterServiceStub)
+    node_group_service = sdk.client(NodeGroupServiceStub)
+
+    # ------------ K8S CLUSTER NAME ------------ #
+    cluster_name = get_cluster_name(config.cluster_id)
+
+    # ------------ SECURITY GROUPS ------------ #
+    subnet = subnet_service.Get(GetSubnetRequest(subnet_id=config.subnet_id))
+    main_sg_id = get_security_group_id(
+        sg_service,
+        "lzy-{}-main-sg".format(cluster_name)
     )
-    cluster_name = cluster.name
-except grpc.RpcError as e:
-    if e.code() is grpc.StatusCode.NOT_FOUND:
-        # TODO: check cluster with same name existence before creating SGs
-        print("k8s cluster with id {} was not found!\n".format(config.cluster_id))
-        exit(1)
-    else:
-        raise e
+    public_services_sg_id = get_security_group_id(
+        sg_service,
+        "lzy-{}-public-services".format(cluster_name)
+    )
 
-# ------------ SECURITY GROUPS ------------ #
-subnet = subnet_service.Get(GetSubnetRequest(subnet_id=config.subnet_id))
+    ans = input("Are you sure you want to create node group with this configuration? (print 'YES!'): ")
+    if ans != "YES!":
+        sys.exit()
 
-sgs = list(filter(
-    lambda sg: sg.name == "lzy-{}-main-sg".format(cluster_name),
-    sg_service.List(ListSecurityGroupsRequest(folder_id=config.folder_id)).security_groups
-))
-if len(sgs) == 0:
-    raise Exception("lzy-{}-main-sg security group does not exist. (It must be created during script "
-                    "create_cluster.py --cluster-name {} ...)".format(cluster_name, cluster_name))
-main_sg_id = sgs[0].id
-
-sgs = list(filter(
-    lambda sg: sg.name == "lzy-{}-public-services".format(cluster_name),
-    sg_service.List(ListSecurityGroupsRequest(folder_id=config.folder_id)).security_groups
-))
-if len(sgs) == 0:
-    raise Exception("lzy-{}-public-services security group does not exist. (It must be created during script "
-                    "create_cluster.py --cluster-name {} ...)".format(cluster_name, cluster_name))
-public_services_sg_id = sgs[0].id
-
-# ------------ NODE POOL ------------ #
-try:
-    print("trying to create {} k8s node group...\n".format(cluster_name))
-    node_group_service.Create(
-        CreateNodeGroupRequest(
-            cluster_id=config.cluster_id,
-            name=config.node_pool_name,
-            description="",
-            labels={
-                # TODO: forward compute labels for Seva (https://st.yandex-team.ru/CLOUD-111652)
-            },
-            node_template=NodeTemplate(
+    # ------------ NODE POOL ------------ #
+    try:
+        print("trying to create {} k8s node group...\n".format(cluster_name))
+        node_group_service.Create(
+            CreateNodeGroupRequest(
+                cluster_id=config.cluster_id,
+                name=config.node_pool_name,
+                description="",
                 labels={
                     # TODO: forward compute labels for Seva (https://st.yandex-team.ru/CLOUD-111652)
                 },
-                platform_id=config.platform_id,
-                resources_spec=ResourcesSpec(
-                    memory=memory,
-                    cores=config.cpu_count,
-                    gpus=config.gpu_count
+                node_template=NodeTemplate(
+                    labels={
+                        # TODO: forward compute labels for Seva (https://st.yandex-team.ru/CLOUD-111652)
+                    },
+                    platform_id=config.platform_id,
+                    resources_spec=ResourcesSpec(
+                        memory=memory,
+                        cores=config.cpu_count,
+                        gpus=config.gpu_count
+                    ),
+                    boot_disk_spec=DiskSpec(
+                        disk_type_id=config.disc_type,
+                        disk_size=disc_size
+                    ),
+                    metadata={
+                        # TODO: forward compute metadata for Seva (https://st.yandex-team.ru/CLOUD-111652)
+                    },
+                    network_interface_specs=[
+                        NetworkInterfaceSpec(
+                            subnet_ids=[config.subnet_id],
+                            primary_v4_address_spec=NodeAddressSpec(),
+                            primary_v6_address_spec=NodeAddressSpec(),
+                            security_group_ids=[main_sg_id, public_services_sg_id]
+                        )
+                    ]
                 ),
-                boot_disk_spec=DiskSpec(
-                    disk_type_id=config.disc_type,
-                    disk_size=disc_size
-                ),
-                metadata={
-                    # TODO: forward compute metadata for Seva (https://st.yandex-team.ru/CLOUD-111652)
-                },
-                network_interface_specs=[
-                    NetworkInterfaceSpec(
-                        subnet_ids=[config.subnet_id],
-                        primary_v4_address_spec=NodeAddressSpec(),
-                        primary_v6_address_spec=NodeAddressSpec(),
-                        security_group_ids=[main_sg_id, public_services_sg_id]
+                scale_policy=ScalePolicy(
+                    auto_scale=ScalePolicy.AutoScale(
+                        min_size=config.min_pool_size,
+                        max_size=config.max_pool_size,
+                        initial_size=config.min_pool_size
                     )
-                ]
-            ),
-            scale_policy=ScalePolicy(
-                auto_scale=ScalePolicy.AutoScale(
-                    min_size=config.min_pool_size,
-                    max_size=config.max_pool_size,
-                    initial_size=config.min_pool_size
-                )
-            ),
-            node_taints=node_pool_taints,
-            node_labels={
-                "lzy.ai/node-pool-id": config.node_pool_id,
-                "lzy.ai/node-pool-label": config.node_pool_label,
-                "lzy.ai/node-pool-kind": config.node_pool_kind,
-                "lzy.ai/node-pool-az": subnet.zone_id,
-                "lzy.ai/node-pool-state": "ACTIVE"  # or "INACTIVE" ???
-            }
+                ),
+                node_taints=node_pool_taints,
+                node_labels={
+                    "lzy.ai/node-pool-id": config.node_pool_id,
+                    "lzy.ai/node-pool-label": config.node_pool_label,
+                    "lzy.ai/node-pool-kind": config.node_pool_kind,
+                    "lzy.ai/node-pool-az": subnet.zone_id,
+                    "lzy.ai/node-pool-state": "ACTIVE"  # or "INACTIVE" ???
+                }
+            )
         )
-    )
-    print("k8s node group {} in cluster {} ({}) was started creating".format(config.node_pool_name, config.cluster_id,
-                                                                             cluster_name))
-except grpc.RpcError as e:
-    if e.code() is grpc.StatusCode.ALREADY_EXISTS:
-        # TODO: check cluster with same name existence before creating SGs
-        print("k8s node group {} in folder {} is already exist\n".format(config.node_pool_name, config.folder_id))
-        print("k8s node group was NOT created!")
-        exit(1)
-    else:
-        raise e
-    raise e
+        print(
+            "k8s node group {} in cluster {} ({}) was started creating".format(config.node_pool_name, config.cluster_id,
+                                                                               cluster_name))
+    except grpc.RpcError as e:
+        if e.code() is grpc.StatusCode.ALREADY_EXISTS:
+            # TODO: check cluster with same name existence before creating SGs
+            print("k8s node group {} in folder {} is already exist\n".format(config.node_pool_name, config.folder_id))
+            print("k8s node group was NOT created!")
+            exit(1)
+        else:
+            raise e
