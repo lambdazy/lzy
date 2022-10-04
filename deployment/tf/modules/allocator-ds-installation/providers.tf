87bdfcf9592b73352b512aa13e894557032fc15f
ArtoLord <artolord@yandex-team.ru>
2022-10-04 12:49:10 +0300

    Move tf to datasphere

--- deployment/tf/modules/allocator-ds-installation/providers.tf
+++
@@ -1,7 +0,0 @@
-provider "yandex" {
-  cloud_id    = "aoec9cvb8rh7s06sgki2"
-  folder_id   = "aoeo1dfromevbs79kijh"
-  zone        = "ru-central1-a"
-  endpoint = "api.cloud-preprod.yandex.net:443"
-  max_retries = 10
-}
