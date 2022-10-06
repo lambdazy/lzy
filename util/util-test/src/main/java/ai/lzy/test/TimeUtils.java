afc601838a0ec0dbc5bf5cfa67e6410b876503e9
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-10-06 12:38:27 +0300

    returned 100 millis

--- util/util-test/src/main/java/ai/lzy/test/TimeUtils.java
+++ util/util-test/src/main/java/ai/lzy/test/TimeUtils.java
@@ -14,7 +14,7 @@ public class TimeUtils {
             if (flag) {
                 break;
             }
-            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(1000));
+            LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100));
         }
         return flag;
     }
