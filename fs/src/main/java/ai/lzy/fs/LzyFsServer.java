86cd6ee2531a294204a3f63ba5e17b8e610f4318
imakunin <imakunin@yandex-team.ru>
2022-10-19 14:59:12 +0300

    drop bash commands in v2

--- fs/src/main/java/ai/lzy/fs/LzyFsServer.java
+++ fs/src/main/java/ai/lzy/fs/LzyFsServer.java
@@ -1,12 +1,9 @@
 package ai.lzy.fs;
 
-import ai.lzy.fs.commands.BuiltinCommandHolder;
 import ai.lzy.fs.fs.LzyFSManager;
 import ai.lzy.fs.fs.LzyFileSlot;
 import ai.lzy.fs.fs.LzyLinuxFsManagerImpl;
 import ai.lzy.fs.fs.LzyMacosFsManagerImpl;
-import ai.lzy.fs.fs.LzyScript;
-import ai.lzy.model.deprecated.Zygote;
 import ai.lzy.util.auth.credentials.RenewableJwt;
 import ai.lzy.util.grpc.GrpcUtils;
 import ai.lzy.v1.channel.LzyChannelManagerGrpc;
@@ -21,13 +18,9 @@ import ru.serce.jnrfuse.FuseException;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static ai.lzy.model.Constants.LOGS_DIR;
 import static ai.lzy.util.grpc.GrpcUtils.newBlockingClient;
 import static ai.lzy.util.grpc.GrpcUtils.newGrpcChannel;
 import static ai.lzy.util.grpc.GrpcUtils.newGrpcServer;
@@ -94,12 +87,6 @@ public class LzyFsServer {
 
         LOG.info("Starting LzyFs gRPC server at {}.", selfUri);
         localServer.start();
-
-        LOG.info("Registering lzy commands...");
-        for (BuiltinCommandHolder command : BuiltinCommandHolder.values()) {
-            registerBuiltinCommand(Path.of(command.name()), command.name());
-        }
-
         LOG.info("LzyFs started on {}.", selfUri);
     }
 
@@ -133,42 +120,5 @@ public class LzyFsServer {
     public void addSlot(LzyFileSlot slot) {
         LOG.info("Explicitly add slot: {}", slot.name());
         fsManager.addSlot(slot);
-    }
-
-    private void registerBuiltinCommand(Path cmd, String name, String... args) {
-        final List<String> commandParts = new ArrayList<>();
-        commandParts.add(System.getProperty("java.home") + "/bin/java");
-        commandParts.add("-Xmx1g");
-        commandParts.add("-Dcustom.log.file=" + LOGS_DIR + "/" + name + "_$(($RANDOM % 10000))");
-        commandParts.add("-Dlog4j.configurationFile=servant_cmd/log4j2.yaml");
-        commandParts.add("-classpath");
-        commandParts.add('"' + System.getProperty("java.class.path") + '"');
-        commandParts.add(BashApi.class.getCanonicalName());
-        commandParts.addAll(List.of("--channel-manager", channelManagerAddress.toString()));
-        //commandParts.addAll(List.of("--lzy-whiteboard", whiteboardAddress.toString()));
-        commandParts.addAll(List.of("--lzy-mount", mountPoint.toAbsolutePath().toString()));
-        commandParts.addAll(List.of("--agent-id", agentId));
-        commandParts.addAll(List.of("--lzy-fs-port", Integer.toString(selfUri.getPort())));
-        //commandParts.addAll(List.of(
-        //    "--auth",
-        //    new String(Base64.getEncoder().encode(auth.toByteString().toByteArray()))
-        //));
-        commandParts.add(name);
-        commandParts.addAll(Arrays.asList(args));
-        commandParts.add("$@");
-
-        final String script = String.join(" ", commandParts) + "\n";
-        if (fsManager.addScript(new LzyScriptImpl(cmd, script), /* isSystem */ true)) {
-            LOG.debug("Register command `{}`.", name);
-        } else {
-            LOG.warn("Command `{}` already exists.", name);
-        }
-    }
-
-    private record LzyScriptImpl(Path location, String scriptText) implements LzyScript {
-        @Override
-        public Zygote operation() {
-            return null;
-        }
     }
 }
