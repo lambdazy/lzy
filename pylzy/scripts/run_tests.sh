70a818bc4424d4785e5942fd4fd529b516c5dfab
ArtoLord <32140169+ArtoLord@users.noreply.github.com>
2022-10-25 11:40:24 +0300

    Add tox (#572)
    * Add tox

--- pylzy/scripts/run_tests.sh
+++ pylzy/scripts/run_tests.sh
@@ -1,20 +1,5 @@
-#!/usr/bin/env nix-shell
-#! nix-shell build.nix -A tests -i bash
+#!/bin/bash
 
-src_dir="$(dirname $0)"
-source "$src_dir/util.sh"
-
-pip install catboost
-
-start
-
-run $_c \
-    coverage run -a --source=./lzy -m unittest discover ./tests
-
-run $_c \
-    coverage report
-
-run $_c \
-    coverage-badge -o coverage.svg -f
-
-finish
+./scripts/gen_proto.sh
+pip install tox
+tox
