0ec9b097af446e870138f7e47b1400f9045f4665
ArtoLord <artolord@yandex-team.ru>
2022-11-25 11:40:46 +0300

    Fix checkstyle

--- test/src/test/java/ai/lzy/test/scenarios/PyApiTest.java
+++ test/src/test/java/ai/lzy/test/scenarios/PyApiTest.java
@@ -8,7 +8,6 @@ import ai.lzy.test.impl.v2.PythonContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
