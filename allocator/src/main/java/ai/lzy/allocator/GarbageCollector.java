e947734070679e946b29b1e054bf46125b3d957c
imakunin <imakunin@yandex-team.ru>
2022-11-28 10:30:33 +0300

    gc preparation

--- allocator/src/main/java/ai/lzy/allocator/GarbageCollector.java
+++ allocator/src/main/java/ai/lzy/allocator/GarbageCollector.java
@@ -3,6 +3,7 @@ package ai.lzy.allocator;
 import ai.lzy.allocator.alloc.VmAllocator;
 import ai.lzy.allocator.alloc.dao.VmDao;
 import ai.lzy.allocator.configs.ServiceConfig;
+import ai.lzy.allocator.disk.dao.DiskOpDao;
 import ai.lzy.allocator.model.Vm;
 import ai.lzy.allocator.storage.AllocatorDataSource;
 import ai.lzy.iam.grpc.client.SubjectServiceGrpcClient;
@@ -28,22 +29,24 @@ public class GarbageCollector extends TimerTask {
 
     public static final AtomicBoolean ENABLED = new AtomicBoolean(true);
 
-    private final VmDao dao;
-    private final OperationDao operations;
+    private final VmDao vmDao;
+    private final DiskOpDao diskOpDao;
+    private final OperationDao operationsDao;
     private final VmAllocator allocator;
     private final SubjectServiceGrpcClient subjectClient;
     private final Timer timer = new Timer("gc-timer", true);
     private final Storage storage;
 
-    public GarbageCollector(VmDao dao, VmAllocator allocator, ServiceConfig config, AllocatorDataSource storage,
-                            @Named("AllocatorIamGrpcChannel") ManagedChannel iamChannel,
+    public GarbageCollector(VmDao dao, DiskOpDao diskOpDao, VmAllocator allocator, ServiceConfig config,
+                            AllocatorDataSource storage, @Named("AllocatorIamGrpcChannel") ManagedChannel iamChannel,
                             @Named("AllocatorOperationDao") OperationDao operationDao,
                             @Named("AllocatorIamToken") RenewableJwt iamToken)
     {
-        this.dao = dao;
+        this.vmDao = dao;
+        this.diskOpDao = diskOpDao;
         this.allocator = allocator;
         this.storage = storage;
-        this.operations = operationDao;
+        this.operationsDao = operationDao;
         this.subjectClient = new SubjectServiceGrpcClient(AllocatorMain.APP, iamChannel, iamToken::get);
 
         timer.scheduleAtFixedRate(this, config.getGcPeriod().toMillis(), config.getGcPeriod().toMillis());
@@ -56,47 +59,67 @@ public class GarbageCollector extends TimerTask {
         }
 
         try {
-            var vms = dao.listExpired(100);
-            LOG.debug("Found {} expired entries", vms.size());
-            vms.forEach(vm -> {
-                try {
-                    LOG.info("Vm {} is expired", vm);
-                    try (var tr = TransactionHandle.create(storage)) {
-                        var status = com.google.rpc.Status.newBuilder()
-                            .setCode(io.grpc.Status.DEADLINE_EXCEEDED.getCode().value())
-                            .setMessage("Vm is expired")
-                            .build();
-
-                        var op = operations.updateError(vm.allocOpId(), status.toByteArray(), tr);
-                        if (op == null) {
-                            LOG.warn("Op with id={} not found", vm.allocOpId());
-                        }
-                        tr.commit();
-                    }
-
-                    var vmSubjectId = vm.allocateState().vmSubjectId();
-                    if (vmSubjectId != null && !vmSubjectId.isEmpty()) {
-                        subjectClient.removeSubject(new ai.lzy.iam.resources.subjects.Vm(vmSubjectId));
-                    }
-
-                    allocator.deallocate(vm.vmId());
-                    //will retry deallocate if it fails
-                    dao.setStatus(vm.vmId(), Vm.Status.DEAD, null);
-                } catch (SQLException e) {
-                    if ("42P01".equals(e.getSQLState())) {
-                        // ERROR: relation <xxx> does not exist
-                        // not an issue in tests
-                        LOG.debug("Error during clean up Vm {}", vm);
-                    } else {
-                        LOG.error("Error during clean up Vm {}", vm, e);
-                    }
-                } catch (Exception e) {
-                    LOG.error("Error during clean up Vm {}", vm, e);
-                }
-            });
+            cleanExpiredVms();
         } catch (Exception e) {
             LOG.error("Error during GC: " + e.getMessage(), e);
         }
+
+        try {
+            cleanExpiredDiskOps();
+        } catch (Exception e) {
+            LOG.error("Error during GC: " + e.getMessage(), e);
+        }
+    }
+
+    private void cleanExpiredVms() throws SQLException {
+        var expiredVms = vmDao.listExpired(100);
+        LOG.debug("Found {} expired entries", expiredVms.size());
+        expiredVms.forEach(vm -> {
+            try {
+                LOG.info("Vm {} is expired", vm);
+                try (var tr = TransactionHandle.create(storage)) {
+                    var status = com.google.rpc.Status.newBuilder()
+                        .setCode(io.grpc.Status.DEADLINE_EXCEEDED.getCode().value())
+                        .setMessage("Vm is expired")
+                        .build();
+
+                    var op = operationsDao.updateError(vm.allocOpId(), status.toByteArray(), tr);
+                    if (op == null) {
+                        LOG.warn("Op with id={} not found", vm.allocOpId());
+                    }
+                    tr.commit();
+                }
+
+                var vmSubjectId = vm.allocateState().vmSubjectId();
+                if (vmSubjectId != null && !vmSubjectId.isEmpty()) {
+                    subjectClient.removeSubject(new ai.lzy.iam.resources.subjects.Vm(vmSubjectId));
+                }
+
+                allocator.deallocate(vm.vmId());
+                //will retry deallocate if it fails
+                vmDao.setStatus(vm.vmId(), Vm.Status.DEAD, null);
+            } catch (SQLException e) {
+                if ("42P01".equals(e.getSQLState())) {
+                    // ERROR: relation <xxx> does not exist
+                    // not an issue in tests
+                    LOG.debug("Error during clean up Vm {}", vm);
+                } else {
+                    LOG.error("Error during clean up Vm {}", vm, e);
+                }
+            } catch (Exception e) {
+                LOG.error("Error during clean up Vm {}", vm, e);
+            }
+        });
+    }
+
+    private void cleanExpiredDiskOps() throws SQLException {
+        var expiredDiskOps = diskOpDao.getExpiredDiskOps(10, null);
+        LOG.debug("Found {} expired disk operations", expiredDiskOps.size());
+
+        expiredDiskOps.forEach(diskOp -> {
+            LOG.debug("Expired op: {}", diskOp);
+            // TODO: ...
+        });
     }
 
     public void shutdown() {
