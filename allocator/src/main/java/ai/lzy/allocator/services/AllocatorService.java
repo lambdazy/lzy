10c032ada6fca0bd4e099f61261fb5d217429de6
lindvv <lindvv@yandex.ru>
2022-12-05 18:55:18 +0300

    Small refactoring

--- allocator/src/main/java/ai/lzy/allocator/services/AllocatorService.java
+++ allocator/src/main/java/ai/lzy/allocator/services/AllocatorService.java
@@ -122,6 +122,8 @@ public class AllocatorService extends AllocatorGrpc.AllocatorImplBase {
     @PreDestroy
     public void shutdown() {
         LOG.info("Shutdown AllocatorService, active allocations: {}", runningAllocations.get());
+        // TODO cleanup scheduled tasks ?
+        // executor.shutdown();
     }
 
     @VisibleForTesting
