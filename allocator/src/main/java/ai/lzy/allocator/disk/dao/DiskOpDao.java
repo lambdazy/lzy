fc0ab3dcb73cf1d8b4d248b8605a8b4e475942ac
NataliaPonomarevaMM <natpon12@gmail.com>
2022-12-07 15:48:10 +0100

    Fixed merge checkstyle

--- allocator/src/main/java/ai/lzy/allocator/disk/dao/DiskOpDao.java
+++ allocator/src/main/java/ai/lzy/allocator/disk/dao/DiskOpDao.java
@@ -8,7 +8,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
