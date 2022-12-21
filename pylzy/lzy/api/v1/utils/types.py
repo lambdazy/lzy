b9ac571946a45a632e205767a80c3730088d9f56
lindvv <lindvv@yandex.ru>
2022-12-21 19:51:50 +0300

    fix codestyle

--- pylzy/lzy/api/v1/utils/types.py
+++ pylzy/lzy/api/v1/utils/types.py
@@ -1,6 +1,7 @@
 import uuid
 from inspect import getfullargspec
 from itertools import chain
+from types import NoneType
 from typing import (
     Callable,
     Optional,
@@ -62,7 +63,7 @@ def infer_real_type(type_: Type) -> Type:
         if origin == Union:  # type: ignore
             # noinspection PyUnresolvedReferences
             args = type_.__args__  # TODO: what should we do with real Union?
-            if len(args) == 2 and args[1].isinstance(type(None)):  # check typ is Optional
+            if len(args) == 2 and args[1] is NoneType:  # check typ is Optional
                 return infer_real_type(args[0])
         return origin
     return type_
