03819efb2ecc2bafa99352435bc5f72a39099753
ArtoLord <32140169+ArtoLord@users.noreply.github.com>
2022-12-22 16:48:09 +0300

    Add logs to worker (#666)
    * Add logs to worker

--- pylzy/tests/scenarios/none_result/__init__.py
+++ pylzy/tests/scenarios/none_result/__init__.py
@@ -5,7 +5,7 @@ from lzy.api.v1 import op, Lzy
 def just_return_none() -> None:
     return None
 
-with Lzy().workflow(name="wf4", interactive=False):
+with Lzy().workflow(name="wf5", interactive=False):
     # noinspection PyNoneFunctionAssignment
     res = just_return_none()
     print(res)
