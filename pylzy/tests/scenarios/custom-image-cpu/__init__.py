d3f5d539488e3bbf7f95a1dd8ed909b000b557d7
lindvv <lindvv@yandex.ru>
2022-12-27 17:56:49 +0300

    fix custom-image scenario

--- pylzy/tests/scenarios/custom-image-cpu/__init__.py
+++ pylzy/tests/scenarios/custom-image-cpu/__init__.py
@@ -16,10 +16,10 @@ def check_env_var_custom_image() -> str:
 if __name__ == "__main__":
     lzy = Lzy()
 
-    with lzy.workflow(name="wf"):
+    with lzy.workflow(name="wf", interactive=False):
         result = check_env_var_custom_image()
         print("Custom env: " + str(result))
 
-    with lzy.workflow(name="wf", docker_image="lzydock/test-env:custom3"):
+    with lzy.workflow(name="wf", interactive=False, docker_image="lzydock/test-env:custom3"):
         result = check_env_var_default_image()
         print("Custom env: " + str(result))
