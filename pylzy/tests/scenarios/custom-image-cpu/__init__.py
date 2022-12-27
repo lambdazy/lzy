2f112fc7bc1a26879c4f2022ecd57d33d4c5739b
lindvv <lindvv@yandex.ru>
2022-12-27 14:10:43 +0300

    fix custom-image scenario

--- pylzy/tests/scenarios/custom-image-cpu/__init__.py
+++ pylzy/tests/scenarios/custom-image-cpu/__init__.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 
-from lzy.api.v1 import LzyRemoteEnv, op
+from lzy.api.v1 import Lzy, op
 
 
 @op
@@ -14,15 +14,14 @@ def check_env_var_custom_image() -> str:
     return os.environ["CUSTOM_ENV"]
 
 
-WORKFLOW_NAME = "workflow_" + str(uuid.uuid4())
-
 if __name__ == "__main__":
-    with LzyRemoteEnv().workflow(name=WORKFLOW_NAME):
+    lzy = Lzy()
+    WORKFLOW_NAME = "workflow-" + str(uuid.uuid4())
+
+    with lzy.workflow(name=WORKFLOW_NAME):
         result = check_env_var_custom_image()
         print("Custom env: " + str(result))
 
-    with LzyRemoteEnv().workflow(
-        name=WORKFLOW_NAME, docker_image="lzydock/test-env:custom3"
-    ):
+    with lzy.workflow(name=WORKFLOW_NAME, docker_image="lzydock/test-env:custom3"):
         result = check_env_var_default_image()
         print("Custom env: " + str(result))
--- pylzy/tests/scenarios/custom-image-gpu/__init__.py
+++ pylzy/tests/scenarios/custom-image-gpu/__init__.py
@@ -3,7 +3,7 @@ import uuid
 import numpy as np
 from catboost import CatBoostClassifier
 
-from lzy.api.v1 import Gpu, LzyRemoteEnv, op
+from lzy.api.v1 import Lzy, op
 
 
 def train(data: np.ndarray, target: np.ndarray) -> CatBoostClassifier:
@@ -27,15 +27,18 @@ def train_custom_env(data: np.ndarray, target: np.ndarray) -> CatBoostClassifier
 WORKFLOW_NAME = "workflow_" + str(uuid.uuid4())
 
 if __name__ == "__main__":
+    lzy = Lzy()
+    WORKFLOW_NAME = "workflow-" + str(uuid.uuid4())
+
     data = np.array([[0, 3], [4, 1], [8, 1], [9, 1]])
     labels = np.array([0, 0, 1, 1])
 
-    with LzyRemoteEnv().workflow(name=WORKFLOW_NAME):
+    with lzy.workflow(name=WORKFLOW_NAME):
         model = train_custom_env(data, labels)
         result = model.predict(np.array([0, 3]))
         print("Prediction: " + str(result))
 
-    with LzyRemoteEnv().workflow(name=WORKFLOW_NAME):
+    with lzy.workflow(name=WORKFLOW_NAME):
         model = train_default_env(data, labels)
         result = model.predict(np.array([9, 1]))
         print("Prediction: " + str(result))
