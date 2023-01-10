bae3d3ebc121bc7962b6dddd01a8b883d9c3a3be
Igor Makunin <9839826+imakunin@users.noreply.github.com>
2023-01-10 12:52:34 +0300

    fix allocator metrics reporting (#681)
    Co-authored-by: imakunin <imakunin@yandex-team.ru>

--- lzy/allocator/src/main/java/ai/lzy/allocator/disk/DiskMetrics.java
+++ lzy/allocator/src/main/java/ai/lzy/allocator/disk/DiskMetrics.java
@@ -1,11 +1,14 @@
 package ai.lzy.allocator.disk;
 
+import ai.lzy.metrics.MetricReporter;
+import io.micronaut.context.annotation.Requires;
 import io.prometheus.client.Counter;
 import io.prometheus.client.Histogram;
 
 import javax.inject.Singleton;
 
 @Singleton
+@Requires(bean = MetricReporter.class)
 public class DiskMetrics {
 
     private static final String ALLOCATOR = "allocator";
