d323135dcd9176260c3f110dbec0e6bde40cd834
tomato <tomato@yandex-team.ru>
2023-01-11 17:56:09 +0100

    whiteboard service for whiteboards index

--- lzy/whiteboard/src/main/java/ai/lzy/whiteboard/storage/WhiteboardStorage.java
+++ lzy/whiteboard/src/main/java/ai/lzy/whiteboard/storage/WhiteboardStorage.java
@@ -2,7 +2,6 @@ package ai.lzy.whiteboard.storage;
 
 import ai.lzy.model.db.TransactionHandle;
 import ai.lzy.model.db.exceptions.NotFoundException;
-import ai.lzy.whiteboard.model.LinkedField;
 import ai.lzy.whiteboard.model.Whiteboard;
 
 import java.sql.SQLException;
@@ -13,14 +12,8 @@ import javax.annotation.Nullable;
 
 public interface WhiteboardStorage {
 
-    void insertWhiteboard(String userId, Whiteboard whiteboard,
+    void insertWhiteboard(String userId, Whiteboard whiteboard, Instant ts,
                           @Nullable TransactionHandle transaction) throws SQLException;
-
-    void updateField(String whiteboardId, LinkedField field, @Nullable Instant finalizedAt,
-                     @Nullable TransactionHandle transaction) throws SQLException;
-
-    void finalizeWhiteboard(String whiteboardId, Instant finalizedAt,
-                            @Nullable TransactionHandle transaction) throws SQLException;
 
     void deleteWhiteboard(String whiteboardId, @Nullable TransactionHandle transaction) throws SQLException;
 
