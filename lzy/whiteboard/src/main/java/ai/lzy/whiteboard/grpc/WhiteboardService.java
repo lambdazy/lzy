66b8db3130ac108809997b950797e60b31b4d8e1
tomato <tomato@yandex-team.ru>
2023-01-16 16:20:47 +0100

    fix typo

--- lzy/whiteboard/src/main/java/ai/lzy/whiteboard/grpc/WhiteboardService.java
+++ lzy/whiteboard/src/main/java/ai/lzy/whiteboard/grpc/WhiteboardService.java
@@ -321,7 +321,8 @@ public class WhiteboardService extends LzyWhiteboardServiceGrpc.LzyWhiteboardSer
                         newStorage,
                         requestWhiteboard.getNamespace().isBlank() ? whiteboard.namespace() :
                             requestWhiteboard.getNamespace(),
-                        requestWhiteboard.getStatus() == LWB.Whiteboard.Status.UNRECOGNIZED ? whiteboard.status() :
+                        requestWhiteboard.getStatus() == LWB.Whiteboard.Status.WHITEBOARD_STATUS_UNSPECIFIED ?
+                            whiteboard.status() :
                             Whiteboard.Status.valueOf(requestWhiteboard.getStatus().name()),
                         requestWhiteboard.getCreatedAt().getSeconds() == 0 ? whiteboard.createdAt() :
                             ai.lzy.util.grpc.ProtoConverter.fromProto(requestWhiteboard.getCreatedAt())
