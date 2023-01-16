e87e9a6cee173001555e90643347acaa95bea945
tomato <tomato@yandex-team.ru>
2023-01-16 11:20:07 +0100

    fix codestyle for records

--- lzy/whiteboard/src/main/java/ai/lzy/whiteboard/model/Field.java
+++ lzy/whiteboard/src/main/java/ai/lzy/whiteboard/model/Field.java
@@ -3,13 +3,9 @@ package ai.lzy.whiteboard.model;
 import ai.lzy.model.DataScheme;
 
 
-public record Field(String name, DataScheme schema) {
-
-    @Override
-    public String toString() {
-        return "Field{" +
-            "name='" + name + '\'' +
-            ", schema=" + schema() +
-            '}';
-    }
+public record Field(
+    String name,
+    DataScheme schema
+)
+{
 }
