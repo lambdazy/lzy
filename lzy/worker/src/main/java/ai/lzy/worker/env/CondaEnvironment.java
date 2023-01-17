644f3c973dd02f0471440b78c69808b918be4c3a
lindvv <lindvv@yandex.ru>
2023-01-17 15:32:44 +0300

    tmp: pip freeze

--- lzy/worker/src/main/java/ai/lzy/worker/env/CondaEnvironment.java
+++ lzy/worker/src/main/java/ai/lzy/worker/env/CondaEnvironment.java
@@ -177,8 +177,8 @@ public class CondaEnvironment implements AuxEnvironment {
 
     private LzyProcess execInEnv(String command, String[] envp) {
         LOG.info("Executing command " + command);
-        String[] bashCmd = new String[] {"bash", "-c", "eval \"$(conda shell.bash hook)\" && conda activate "
-            + envName + " && " + command};
+        String[] bashCmd = new String[] {"bash", "-c", "pip freeze && eval \"$(conda shell.bash hook)\" " +
+            "&& conda activate " + envName + " && " + command};
         return baseEnv.runProcess(bashCmd, envp);
     }
 
