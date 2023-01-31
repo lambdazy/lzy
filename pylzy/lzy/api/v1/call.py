e264ee8fac354af4ec27b32796f7fcdb2697a2d0
tomato <tomato@yandex-team.ru>
2023-01-31 16:14:18 +0100

    use serializers for type checking

--- pylzy/lzy/api/v1/call.py
+++ pylzy/lzy/api/v1/call.py
@@ -6,6 +6,7 @@ from itertools import chain, zip_longest
 from typing import Any, Callable, Dict, Mapping, Sequence, Tuple, TypeVar, Optional, List, Type
 
 from beartype.door import is_subhint
+from serialzy.api import SerializerRegistry
 # noinspection PyProtectedMember
 from serialzy.types import get_type
 
@@ -15,7 +16,7 @@ from lzy.api.v1.provisioning import Provisioning
 from lzy.api.v1.signatures import CallSignature, FuncSignature
 from lzy.api.v1.snapshot import Snapshot
 from lzy.api.v1.utils.proxy_adapter import lzy_proxy, materialize
-from lzy.api.v1.utils.types import infer_real_types, get_default_args
+from lzy.api.v1.utils.types import infer_real_types, get_default_args, check_types_serialization_compatible
 from lzy.api.v1.workflow import LzyWorkflow
 
 T = TypeVar("T")  # pylint: disable=invalid-name
@@ -147,7 +148,8 @@ def wrap_call(
         env_updated.validate()
 
         signature = infer_and_validate_call_signature(f, output_types, active_workflow.snapshot,
-                                                      active_workflow.entry_index, *args, **kwargs)
+                                                      active_workflow.entry_index,
+                                                      active_workflow.owner.serializer_registry, *args, **kwargs)
         lzy_call = LzyCall(active_workflow, signature, prov, env_updated, description, lazy_arguments)
         active_workflow.register_call(lzy_call)
 
@@ -189,7 +191,11 @@ def wrap_call(
 
 
 def infer_and_validate_call_signature(
-    f: Callable, output_type: Sequence[type], snapshot: Snapshot, entry_index: EntryIndex, *args, **kwargs
+    f: Callable, output_type: Sequence[type],
+    snapshot: Snapshot,
+    entry_index: EntryIndex,
+    serializer_registry: SerializerRegistry,
+    *args, **kwargs
 ) -> CallSignature:
     types_mapping = {}
     args_mapping = {}
@@ -222,7 +228,8 @@ def infer_and_validate_call_signature(
 
         if name in argspec.annotations:
             typ = entry_type if entry_type else get_type(arg)
-            if not is_subhint(typ, argspec.annotations[name]):
+            compatible = check_types_serialization_compatible(argspec.annotations[name], typ, serializer_registry)
+            if not compatible or not is_subhint(typ, argspec.annotations[name]):
                 raise TypeError(
                     f"Invalid types: argument {name} has type {argspec.annotations[name]} "
                     f"but passed type {typ}")
--- pylzy/lzy/api/v1/whiteboards.py
+++ pylzy/lzy/api/v1/whiteboards.py
@@ -4,6 +4,7 @@ import uuid
 from dataclasses import dataclass
 from typing import Optional, Type, Dict, Any, Iterable, Set, TYPE_CHECKING, Sequence
 
+from beartype.door import is_subhint
 # noinspection PyPackageRequirements
 from google.protobuf.timestamp_pb2 import Timestamp
 from serialzy.api import Schema
@@ -12,6 +13,7 @@ from serialzy.types import get_type
 from ai.lzy.v1.common.data_scheme_pb2 import DataScheme
 from ai.lzy.v1.whiteboard.whiteboard_pb2 import Whiteboard, WhiteboardField, Storage
 from lzy.api.v1.utils.proxy_adapter import lzy_proxy, materialize_if_sequence_of_lzy_proxies
+from lzy.api.v1.utils.types import check_types_serialization_compatible
 from lzy.api.v1.utils.validation import is_name_valid, NAME_VALID_SYMBOLS
 from lzy.proxy.result import Just
 from lzy.utils.event_loop import LzyEventLoop
@@ -178,9 +180,10 @@ class WritableWhiteboard:
             raise AttributeError(f"Whiteboard field {item} is not assigned")
         return self.__fields[item]
 
-    @staticmethod
-    def __validate_types(value_type: Type, field_type: Type, field_name: str) -> None:
-        if value_type != field_type:
+    def __validate_types(self, value_type: Type, field_type: Type, field_name: str) -> None:
+        compatible = check_types_serialization_compatible(field_type, value_type,
+                                                            self.__workflow.owner.serializer_registry)
+        if not compatible or not is_subhint(value_type, field_type):
             raise TypeError(
                 f"Incompatible types: whiteboard field {field_name} has type {field_type}, "
                 f"but assigning value has type {value_type}")
