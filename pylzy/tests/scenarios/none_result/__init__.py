1046eb008a643f2e03e7a6168a95a43ac583e199
Artem Trofimov <tomato@yandex-team.ru>
2023-02-02 10:42:22 +0100

    allow same slots input (#744)
    (cherry picked from commit 1123011c7e7bda3b7e348ca733f44c4b6cc48941)

--- pylzy/tests/scenarios/none_result/__init__.py
+++ pylzy/tests/scenarios/none_result/__init__.py
@@ -2,11 +2,12 @@ from lzy.api.v1 import op, Lzy
 
 
 @op
-def just_return_none() -> None:
+def just_return_none(a: int, b: int) -> None:
+    print(f"a={a},b={b}")
     return None
 
 
 with Lzy().workflow(name="wf", interactive=False):
     # noinspection PyNoneFunctionAssignment
-    res = just_return_none()
+    res = just_return_none(1, 1)
     print(res)
