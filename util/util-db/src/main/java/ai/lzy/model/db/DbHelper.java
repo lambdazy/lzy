b124027fac8efaeac140da1de3efdd0bfbe95729
imakunin <imakunin@yandex-team.ru>
2023-02-03 16:23:21 +0300

    change default jitter in db operations

--- util/util-db/src/main/java/ai/lzy/model/db/DbHelper.java
+++ util/util-db/src/main/java/ai/lzy/model/db/DbHelper.java
@@ -112,12 +112,12 @@ public enum DbHelper {
             }
             var delay = nextDelay;
             nextDelay *= coeff;
-            return delay + (int) ((delay / 5) * (Math.random() - 0.5));
+            return delay + (int) (50 * Math.random());
         }
     }
 
     public static RetryPolicy defaultRetryPolicy() {
-        return new DefaultRetryPolicy(10, 50, 2);
+        return new DefaultRetryPolicy(5, 50, 2);
     }
 
     public static final class RetryCountExceededException extends Exception {
