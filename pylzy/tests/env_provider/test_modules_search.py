624c667b67ed213d884006d13aa45573b57a2aae
tomato <tomato@yandex-team.ru>
2023-02-03 13:31:31 +0100

    fix local modules (single file) migration from sys path

--- pylzy/tests/env_provider/test_modules_search.py
+++ pylzy/tests/env_provider/test_modules_search.py
@@ -1,8 +1,10 @@
 __package__ = None
 
 import os
+from pathlib import Path
 from unittest import TestCase
 
+from lzy.api.v1.utils.files import sys_path_parent
 from lzy.py_env.api import PyEnvProvider
 from lzy.py_env.py_env_provider import AutomaticPyEnvProvider
 
@@ -23,38 +25,49 @@ class ModulesSearchTests(TestCase):
         # Act
         env = self.provider.provide({"level": level, "level1": level1})
         remote = env.libraries
-        local = env.local_modules_path
+        local_modules_path = env.local_modules_path
 
         # Assert
         self.assertEqual("echo", level1.echo())
-        self.assertEqual(2, len(local))
+        self.assertEqual(3, len(local_modules_path))
+
+        for local in local_modules_path:
+            prefix = sys_path_parent(local)
+            path = (Path(local).relative_to(prefix))
+            self.assertIsNotNone(path)
+
         # noinspection DuplicatedCode
-        self.assertTrue(directory + "/modules_for_tests" in local)
-        self.assertTrue(directory + "/modules_for_tests_2" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3/level3.py" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level2.py" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level1.py" in local)
+        self.assertTrue(directory + "/modules_for_tests" in local_modules_path)
+        self.assertTrue(directory + "/modules_for_tests_2" in local_modules_path)
+        self.assertTrue(directory + "/modules_for_tests_2" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3/level3.py" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level2.py" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level1.py" in local_modules_path)
         self.assertEqual({"PyYAML", "cloudpickle"}, set(remote.keys()))
 
     def test_modules_search_2(self):
         from modules_for_tests.level1.level2_nb import level_foo
         env = self.provider.provide({"level_foo": level_foo})
-        local = env.local_modules_path
+        local_modules_path = env.local_modules_path
         os.chdir(os.path.dirname(__file__))
         directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
         # noinspection DuplicatedCode
-        self.assertEqual(1, len(local))
-        self.assertTrue(directory + "/modules_for_tests" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3/level3.py" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2/level2.py" in local)
-        self.assertFalse(directory + "/modules_for_tests/level1/level2_nb" in local)
+        self.assertEqual(2, len(local_modules_path))
+        for local in local_modules_path:
+            prefix = sys_path_parent(local)
+            path = (Path(local).relative_to(prefix))
+            self.assertIsNotNone(path)
+        self.assertTrue(directory + "/modules_for_tests" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level3/level3.py" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2/level2.py" in local_modules_path)
+        self.assertFalse(directory + "/modules_for_tests/level1/level2_nb" in local_modules_path)
 
     def test_exceptional_builtin(self):
         import _functools
