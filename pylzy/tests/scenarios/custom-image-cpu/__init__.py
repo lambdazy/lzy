3c28de76b11c650e49e2a058fc5ca090501ab7a4
lindvv <lindvv@yandex.ru>
2023-02-06 17:31:33 +0300

    tmp

--- pylzy/tests/scenarios/custom-image-cpu/__init__.py
+++ pylzy/tests/scenarios/custom-image-cpu/__init__.py
@@ -4,16 +4,16 @@ from lzy.api.v1 import Lzy, op
 from localq import two
 
 
-@op(docker_image="lzydock/user-test:custom-1")
+@op(docker_image="lzydock/user-default:master-1.1")
 def check_env_var_custom_image() -> str:
     print("op1: " + str(two()))
-    return os.environ["CUSTOM_ENV"]
+    return "true" # os.environ["CUSTOM_ENV"]
 
 
 @op
 def check_env_var_default_image() -> str:
     print("op2: " + str(two()))
-    return os.environ["CUSTOM_ENV"]
+    return "true" # os.environ["CUSTOM_ENV"]
 
 
 if __name__ == "__main__":
@@ -23,6 +23,6 @@ if __name__ == "__main__":
         result = check_env_var_custom_image()
         print("op1 custom env: " + str(result))
 
-    with lzy.workflow(name="wf", interactive=False, docker_image="lzydock/user-test:custom-1"):
+    with lzy.workflow(name="wf", interactive=False, docker_image="lzydock/user-default:master-1.1"):
         result = check_env_var_default_image()
         print("op2 custom env: " + str(result))
