980ae45122927890a4b6dd84b8bd4cbf03fdb6d5
imakunin <imakunin@yandex-team.ru>
2023-02-08 13:32:58 +0300

    update

--- lzy/allocator/src/main/resources/db/allocator/migrations/V1__reqid.sql
+++ lzy/allocator/src/main/resources/db/allocator/migrations/V1__reqid.sql
@@ -1,10 +1,18 @@
 ALTER TABLE vm
     ADD COLUMN allocation_reqid TEXT NOT NULL DEFAULT 'unknown',
-    ADD COLUMN delete_reqid TEXT NULL;
+    ADD COLUMN delete_reqid     TEXT NULL;
 
 ALTER TABLE session
     ADD COLUMN delete_reqid TEXT NULL;
 
 ALTER TABLE disk_op
-    ADD COLUMN reqid TEXT NOT NULL DEFAULT 'unknown',
+    ADD COLUMN reqid       TEXT NOT NULL DEFAULT 'unknown',
     ADD COLUMN description TEXT NOT NULL DEFAULT '';
+
+ALTER TABLE vm
+    DROP CONSTRAINT vm_check2;
+ALTER TABLE vm
+    ADD CONSTRAINT vm_check3 CHECK (
+            (status != 'DELETING') OR
+            ((delete_op_id IS NOT NULL) AND (delete_worker IS NOT NULL) AND (delete_reqid IS NOT NULL)))
+
