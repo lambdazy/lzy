0abbb6a92d9a7d32bcca30da72d4aaf20536ec25
Artem Trofimov <tomato@yandex-team.ru>
2023-02-10 10:00:38 +0100

    import lzy from local modules if exists (#807)

--- lzy/worker/src/main/java/ai/lzy/worker/env/CondaEnvironment.java
+++ lzy/worker/src/main/java/ai/lzy/worker/env/CondaEnvironment.java
@@ -159,8 +159,9 @@ public class CondaEnvironment implements AuxEnvironment {
 
     private LzyProcess execInEnv(String command, String[] envp) {
         LOG.info("Executing command " + command);
-        String[] bashCmd = new String[] {"bash", "-c", "eval \"$(conda shell.bash hook)\" " +
-            "&& conda activate " + envName + " && " + command};
+        String[] bashCmd =
+            new String[] {"bash", "-c", "cd " + localModulesAbsolutePath + " && eval \"$(conda shell.bash hook)\" " +
+                "&& conda activate " + envName + " && " + command};
         return baseEnv.runProcess(bashCmd, envp);
     }
 
