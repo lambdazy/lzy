145d7dd1a20ac7111c3d0ffa111bf31bc08d2f25
Igor Naumov <ignmv@yandex-team.ru>
2023-02-15 13:44:22 +0300

    release management scripts

---
+++ deployment/make-release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+function project_version() {
+    mvn help:evaluate -Dexpression=project.version -q -DforceStdout
+}
+
+git checkout master
+git pull
+
+cd parent
+CURRENT_VERSION=$(project_version | awk -F'-' '{print $1}')
+
+echo "CURRENT VERSION IS $CURRENT_VERSION"
+git branch "R-$CURRENT_VERSION"
+
+# x.y+1-SNAPSHOT
+mvn build-helper:parse-version versions:set \
+  -DnewVersion="\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}\${parsedVersion.qualifier?}"
+
+NEXT_SNAPSHOT_VERSION=$(project_version)
+git add -u ..
+git commit -m "set version $NEXT_SNAPSHOT_VERSION"
+
+git checkout "R-$CURRENT_VERSION"
+
+# x.y.0
+mvn build-helper:parse-version versions:set \
+  -DnewVersion="\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.0"
+
+RELEASE_VERSION=$(project_version)
+echo "$RELEASE_VERSION" > ../pylzy/lzy/version/version
+git add -u ..
+git commit -m "set version $RELEASE_VERSION"
+git tag "R-$RELEASE_VERSION"
+git push origin "R-$CURRENT_VERSION" #branch
+git push origin "R-$RELEASE_VERSION" #tag
+git push origin master
