6ed5ac5d2d6d26eadbcca2c83e072d495ea55ca9
ssokolvyak <zhvkgj@yandex.ru>
2023-02-16 14:33:21 +0300

    Fix requirements versions

--- test/src/test/java/ai/lzy/test/scenarios/PyApiTest.java
+++ test/src/test/java/ai/lzy/test/scenarios/PyApiTest.java
@@ -33,7 +33,7 @@ public class PyApiTest {
                 1. Importing external modules (catboost)
                 2. Functions which accept and return complex objects
          */
-        pythonContext.context().evalAndAssertScenarioResult("catboost_integration_cpu", List.of("catboost"));
+        pythonContext.context().evalAndAssertScenarioResult("catboost_integration_cpu");
     }
 
     @Test
