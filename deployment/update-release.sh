c1a92a18b7635ef56e70b90d23ffc2894154f94e
Hoxo <nia3705@gmail.com>
2023-02-20 11:54:19 +0300

    Fix build scripts (#838)
    * add ssl option to docker scripts
    
    * add release scripts output
    
    * rework worker script
    
    * rework worker script
    
    * add terraform variables for domain
    
    * update usage
    
    * add outputs

--- deployment/update-release.sh
+++ deployment/update-release.sh
@@ -35,5 +35,8 @@ echo "$NEW_VERSION" > ../pylzy/lzy/version/version
 git add -u ..
 git commit -m "set version $NEW_VERSION"
 git tag "R-$NEW_VERSION"
-#git push origin "$BRANCH"
-#git push origin "R-$NEW_VERSION"
+git push origin "$BRANCH"
+git push origin "R-$NEW_VERSION"
+
+echo "release-branch=$BRANCH" >> "$GITHUB_OUTPUT"
+echo "release-version=$NEW_VERSION" >> "$GITHUB_OUTPUT"
