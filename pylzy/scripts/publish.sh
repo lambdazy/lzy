805fe83fbcde9f8f7f0721b9a0f79be7019f4064
Hoxo <nia3705@gmail.com>
2023-02-28 10:18:30 +0100

    clean, build and publish pylzy with makefile (#860)

--- pylzy/scripts/publish.sh
+++ pylzy/scripts/publish.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
+
+if [[ @# -lt 2 ]]; then
+  echo "Usage: $0 <username> <password>"
+  exit 1
+fi
 
 echo "Publishing package"
 
-export TWINE_USERNAME="__token__"
-# **token has to be provided by person or bot who runs the script**
-# otherwise script will stop and ask for token
-#
-# export TWINE_PASSWORD="<provide token here>"
+export TWINE_USERNAME=$1
+export TWINE_PASSWORD=$2
 
 pip install twine -U
 python -m twine upload dist/* --verbose
