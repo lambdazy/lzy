9276d03e87e0b559b0b76327fa70f4b18f536e04
ArtoLord <32140169+ArtoLord@users.noreply.github.com>
2023-03-07 16:24:23 +0300

    Add logs (#872)
    * Add logs

--- deployment/tf/modules/yc/channel-manager.tf
+++ deployment/tf/modules/yc/channel-manager.tf
@@ -1,12 +1,12 @@
 locals {
-  channel-manager-labels   = {
+  channel-manager-labels = {
     app                         = "channel-manager"
     "app.kubernetes.io/name"    = "channel-manager"
     "app.kubernetes.io/part-of" = "lzy"
     "lzy.ai/app"                = "channel-manager"
   }
   channel-manager-k8s-name = "channel-manager"
-  channel-manager-image = var.channel-manager-image
+  channel-manager-image    = var.channel-manager-image
 }
 
 resource "kubernetes_deployment" "channel-manager" {
@@ -43,25 +43,25 @@ resource "kubernetes_deployment" "channel-manager" {
             value = local.channel-manager-metrics-port
           }
           env {
-            name = "CHANNEL_MANAGER_ADDRESS"
+            name  = "CHANNEL_MANAGER_ADDRESS"
             value = "0.0.0.0:${local.channel-manager-port}"
           }
 
           env {
-            name  = "CHANNEL_MANAGER_DATABASE_USERNAME"
+            name = "CHANNEL_MANAGER_DATABASE_USERNAME"
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.db_secret["channel-manager"].metadata[0].name
-                key = "username"
+                key  = "username"
               }
             }
           }
           env {
-            name  = "CHANNEL_MANAGER_DATABASE_PASSWORD"
+            name = "CHANNEL_MANAGER_DATABASE_PASSWORD"
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.db_secret["channel-manager"].metadata[0].name
-                key = "password"
+                key  = "password"
               }
             }
           }
@@ -76,7 +76,7 @@ resource "kubernetes_deployment" "channel-manager" {
             value = "true"
           }
           env {
-            name = "CHANNEL_MANAGER_IAM_ADDRESS"
+            name  = "CHANNEL_MANAGER_IAM_ADDRESS"
             value = "${kubernetes_service.iam.spec[0].cluster_ip}:${local.iam-port}"
           }
           env {
@@ -84,7 +84,7 @@ resource "kubernetes_deployment" "channel-manager" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.iam_internal_user_data.metadata[0].name
-                key = "username"
+                key  = "username"
               }
             }
           }
@@ -93,16 +93,16 @@ resource "kubernetes_deployment" "channel-manager" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.iam_internal_user_data.metadata[0].name
-                key = "key"
+                key  = "key"
               }
             }
           }
           env {
-            name = "CHANNEL_MANAGER_LZY_SERVICE_ADDRESS"
+            name  = "CHANNEL_MANAGER_LZY_SERVICE_ADDRESS"
             value = "${kubernetes_service.lzy_service.spec[0].cluster_ip}:${local.lzy-service-port}"
           }
           env {
-            name = "CHANNEL_MANAGER_WHITEBOARD_ADDRESS"
+            name  = "CHANNEL_MANAGER_WHITEBOARD_ADDRESS"
             value = "http://${kubernetes_service.whiteboard_service.spec[0].cluster_ip}:${local.whiteboard-port}"
           }
         }
@@ -156,14 +156,14 @@ resource "kubernetes_service" "channel_manager_service" {
     name   = "${local.channel-manager-k8s-name}-load-balancer"
     labels = local.channel-manager-labels
     annotations = {
-      "yandex.cloud/load-balancer-type": "internal"
-      "yandex.cloud/subnet-id": yandex_vpc_subnet.custom-subnet.id
+      "yandex.cloud/load-balancer-type" : "internal"
+      "yandex.cloud/subnet-id" : yandex_vpc_subnet.custom-subnet.id
     }
   }
   spec {
     selector = local.channel-manager-labels
     port {
-      port = local.channel-manager-port
+      port        = local.channel-manager-port
       target_port = local.channel-manager-port
     }
     type = "LoadBalancer"
--- deployment/tf/modules/yc/storage.tf
+++ deployment/tf/modules/yc/storage.tf
@@ -1,11 +1,11 @@
 locals {
-  storage-labels   = {
-    app                         = "storage"
-    "app.kubernetes.io/name"    = "storage"
-    "lzy.ai/app"                = "storage"
+  storage-labels = {
+    app                      = "storage"
+    "app.kubernetes.io/name" = "storage"
+    "lzy.ai/app"             = "storage"
   }
   storage-k8s-name = "storage"
-  storage-image = var.storage-image
+  storage-image    = var.storage-image
 }
 
 resource "kubernetes_deployment" "storage" {
@@ -44,25 +44,25 @@ resource "kubernetes_deployment" "storage" {
           }
 
           env {
-            name = "STORAGE_ADDRESS"
+            name  = "STORAGE_ADDRESS"
             value = "0.0.0.0:${local.storage-port}"
           }
 
           env {
-            name  = "STORAGE_DATABASE_USERNAME"
+            name = "STORAGE_DATABASE_USERNAME"
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.db_secret["storage"].metadata[0].name
-                key = "username"
+                key  = "username"
               }
             }
           }
           env {
-            name  = "STORAGE_DATABASE_PASSWORD"
+            name = "STORAGE_DATABASE_PASSWORD"
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.db_secret["storage"].metadata[0].name
-                key = "password"
+                key  = "password"
               }
             }
           }
@@ -77,7 +77,7 @@ resource "kubernetes_deployment" "storage" {
             value = "true"
           }
           env {
-            name = "STORAGE_IAM_ADDRESS"
+            name  = "STORAGE_IAM_ADDRESS"
             value = "${kubernetes_service.iam.spec[0].cluster_ip}:${local.iam-port}"
           }
           env {
@@ -85,7 +85,7 @@ resource "kubernetes_deployment" "storage" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.iam_internal_user_data.metadata[0].name
-                key = "username"
+                key  = "username"
               }
             }
           }
@@ -94,53 +94,53 @@ resource "kubernetes_deployment" "storage" {
             value_from {
               secret_key_ref {
                 name = kubernetes_secret.iam_internal_user_data.metadata[0].name
-                key = "key"
+                key  = "key"
               }
             }
           }
 
           env {
-            name = "STORAGE_YC_ENABLED"
+            name  = "STORAGE_YC_ENABLED"
             value = "true"
           }
 
           env {
-            name = "STORAGE_YC_SERVICE_ACCOUNT_ID"
+            name  = "STORAGE_YC_SERVICE_ACCOUNT_ID"
             value = yandex_iam_service_account.admin-sa.id
           }
 
           env {
-            name = "STORAGE_YC_KEY_ID"
+            name  = "STORAGE_YC_KEY_ID"
             value = yandex_iam_service_account_key.admin-sa-key.id
           }
 
           env {
-            name = "STORAGE_YC_PRIVATE_KEY"
+            name  = "STORAGE_YC_PRIVATE_KEY"
             value = yandex_iam_service_account_key.admin-sa-key.private_key
           }
 
           env {
-            name = "STORAGE_YC_FOLDER_ID"
+            name  = "STORAGE_YC_FOLDER_ID"
             value = var.folder_id
           }
 
           env {
-            name = "STORAGE_S3_YC_ENABLED"
+            name  = "STORAGE_S3_YC_ENABLED"
             value = "true"
           }
 
           env {
-            name = "STORAGE_S3_YC_ENDPOINT"
+            name  = "STORAGE_S3_YC_ENDPOINT"
             value = "https://storage.yandexcloud.net"
           }
 
           env {
-            name = "STORAGE_S3_YC_ACCESS_TOKEN"
+            name  = "STORAGE_S3_YC_ACCESS_TOKEN"
             value = yandex_iam_service_account_static_access_key.admin-sa-static-key.access_key
           }
 
           env {
-            name = "STORAGE_S3_YC_SECRET_TOKEN"
+            name  = "STORAGE_S3_YC_SECRET_TOKEN"
             value = yandex_iam_service_account_static_access_key.admin-sa-static-key.secret_key
           }
         }
@@ -183,7 +183,7 @@ resource "kubernetes_service" "storage_service" {
   spec {
     selector = local.storage-labels
     port {
-      port = local.storage-port
+      port        = local.storage-port
       target_port = local.storage-port
     }
     type = "ClusterIP"
