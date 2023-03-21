f551925828a4065972703617eb9570c4d5df780a
ArtoLord <artolord@yandex-team.ru>
2023-03-21 17:23:56 +0300

    WIP

--- deployment/tf/modules/yc/cluster.tf
+++ deployment/tf/modules/yc/cluster.tf
@@ -66,3 +66,12 @@ provider "kubernetes" {
     manifest_resource = true
   }
 }
+
+
+provider "helm" {
+  kubernetes {
+    host                   = yandex_kubernetes_cluster.main.master.0.external_v4_endpoint
+    cluster_ca_certificate = yandex_kubernetes_cluster.main.master.0.cluster_ca_certificate
+    token                  = data.yandex_client_config.client.iam_token
+  }
+}
