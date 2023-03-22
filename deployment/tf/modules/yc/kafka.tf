abb9a199e03199bdf3d2db1d826c15627a98139e
ArtoLord <artolord@yandex-team.ru>
2023-03-22 11:57:41 +0300

    WIP

--- deployment/tf/modules/yc/kafka.tf
+++ deployment/tf/modules/yc/kafka.tf
@@ -83,12 +83,7 @@ resource "helm_release" "lzy_kafka" {
   }
 
   set {
-    name  = "superUsers[1]"
-    value = "User:${local.kafka_admin_username}"
-  }
-
-  set {
-    name  = "superUsers[0]"
+    name  = "superUsers"
     value = "User:admin"
   }
 
