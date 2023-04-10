665a114697f3c033f02c0efb7e9428e753b44e04
lindvv <lindvv@yandex.ru>
2023-04-10 16:35:27 +0300

    service config: list of hosts

--- lzy/allocator/src/main/java/ai/lzy/allocator/configs/ServiceConfig.java
+++ lzy/allocator/src/main/java/ai/lzy/allocator/configs/ServiceConfig.java
@@ -2,6 +2,7 @@ package ai.lzy.allocator.configs;
 
 import ai.lzy.iam.config.IamClientConfiguration;
 import ai.lzy.model.db.DatabaseConfiguration;
+import com.google.common.net.HostAndPort;
 import io.micronaut.context.annotation.ConfigurationBuilder;
 import io.micronaut.context.annotation.ConfigurationProperties;
 import jakarta.annotation.Nullable;
@@ -18,11 +19,16 @@ import java.util.Map;
 @ConfigurationProperties("allocator")
 public class ServiceConfig {
     private String instanceId;
-    private String address;
+    private List<String> hosts = new ArrayList<>(); // either [<ipv4>] or [<ipv4>,<ipv6>]
+    private Integer port;
     private Duration allocationTimeout;
     private Duration heartbeatTimeout;
     private List<String> serviceClusters = new ArrayList<>();
     private List<String> userClusters = new ArrayList<>();
+
+    public String getAddress() {
+        return HostAndPort.fromParts(hosts.get(hosts.size() - 1), port).toString();
+    }
 
     @ConfigurationBuilder("database")
     private final DatabaseConfiguration database = new DatabaseConfiguration();
