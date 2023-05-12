58a4887e816f3d6f047ec49f8b53639be709e589
ArtoLord <artolord@yandex-team.ru>
2023-05-12 14:52:19 +0300

    Fix checkstyle

--- lzy/s3-sink/src/main/java/ai/lzy/kafka/s3sink/Job.java
+++ lzy/s3-sink/src/main/java/ai/lzy/kafka/s3sink/Job.java
@@ -30,7 +30,6 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
