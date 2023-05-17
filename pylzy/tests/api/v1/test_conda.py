b5b2db6d237583c33e86eef864ea0a70224068f9
Vladimir Lipkin <lipkin@yandex-team.ru>
2023-05-17 19:17:24 +0300

    First refactiring of Env

--- pylzy/tests/api/v1/test_conda.py
+++ pylzy/tests/api/v1/test_conda.py
@@ -1,17 +1,38 @@
+import logging
 import pytest
-from unittest import TestCase
 
-from lzy.api.v1.utils.conda import generate_conda_yaml
+from lzy.api.v1.env import Env
 
 
-class CondaTests(TestCase):
-    def test_conda_generate_in_cache(self):
-        yaml = generate_conda_yaml("3.7.11", {"pylzy": "0.0.0"})
-        self.assertIn("name: py37", yaml)
-        self.assertIn("pylzy==0.0.0", yaml)
+def test_conda_generate_in_cache():
+    env = Env(
+        python_version="3.7.11",
+        libraries={"pylzy": "0.0.0"}
+    )
+    yaml = env.get_conda_yaml()
 
-    def test_conda_generate_not_in_cache(self):
-        with pytest.warns(Warning, match=r"Installed python version.*"):
-            yaml = generate_conda_yaml("3.7.9999", {"pylzy": "0.0.0"})
-        self.assertIn("name: default", yaml)
-        self.assertIn("pylzy==0.0.0", yaml)
+    assert "name: py37" in yaml
+    assert "pylzy==0.0.0" in yaml
+
+
+def test_conda_generate_not_in_cache(caplog):
+    version = "3.7.9999"
+
+    env = Env(
+        python_version=version,
+        libraries={"pylzy": "0.0.0"}
+    )
+
+    with caplog.at_level(logging.WARNING, logger='lzy.api.v1.env'):
+        yaml = env.get_conda_yaml()
+
+    assert any(
+        (
+            f"Installed python version ({version})" in record.message and
+            record.levelno == logging.WARNING
+        )
+        for record in caplog.records
+    )
+
+    assert "name: default" in yaml
+    assert "pylzy==0.0.0" in yaml
