18ecbdb25d66a865814fcf171ba7beb126b6014f
imakunin <imakunin@yandex-team.ru>
2023-05-19 15:44:47 +0300

    logging context

--- util/util-log/src/main/java/ai/lzy/logs/LogContextKey.java
+++ util/util-log/src/main/java/ai/lzy/logs/LogContextKey.java
@@ -2,11 +2,11 @@ package ai.lzy.logs;
 
 public final class LogContextKey {
 
-    public static final String ACTION = "action";
+    public static final String ACTION = "act";
 
-    public static final String OPERATION_ID = "operation_id";
-    public static final String VM_ID = "vm_id";
-    public static final String DYNAMIC_MOUNT_ID = "dynamic_mount_id";
+    public static final String OPERATION_ID = "opid";
+    public static final String VM_ID = "vmid";
+    public static final String DYNAMIC_MOUNT_ID = "dynmntid";
 
 
     private LogContextKey() { }
