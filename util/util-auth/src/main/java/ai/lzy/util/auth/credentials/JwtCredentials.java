ceb56f5513c97f18b23fe389957cebb4dacc4033
Igor Makunin <9839826+imakunin@users.noreply.github.com>
2023-05-19 17:33:08 +0300

    introduce YC IAM token (#1009)
    Co-authored-by: imakunin <imakunin@yandex-team.ru>

--- util/util-auth/src/main/java/ai/lzy/util/auth/credentials/JwtCredentials.java
+++ util/util-auth/src/main/java/ai/lzy/util/auth/credentials/JwtCredentials.java
@@ -1,7 +1,6 @@
 package ai.lzy.util.auth.credentials;
 
 public record JwtCredentials(String token) implements Credentials {
-
     @Override
     public String type() {
         return "PUBLIC_KEY";
