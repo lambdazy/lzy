47d2a0b795b27e446912ea1c13601155f6dd1655
imakunin <imakunin@yandex-team.ru>
2023-06-01 14:03:22 +0300

    YC DS subject type

--- iam-api/src/main/java/ai/lzy/iam/resources/subjects/User.java
+++ iam-api/src/main/java/ai/lzy/iam/resources/subjects/User.java
@@ -10,9 +10,5 @@ public final class User extends Subject {
 
     public User(String id, AuthProvider provider, String providerId, Map<String, String> meta) {
         super(id, SubjectType.USER, provider, providerId, meta);
-
-        if (provider != AuthProvider.INTERNAL && provider != AuthProvider.GITHUB) {
-            throw new IllegalArgumentException(provider.name());
-        }
     }
 }
