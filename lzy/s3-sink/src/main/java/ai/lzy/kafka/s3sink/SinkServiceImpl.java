1b53d4fcbd85c8e67757f0b5ccc2de91e62178e0
ArtoLord <artolord@yandex-team.ru>
2023-06-05 15:01:23 +0300

    Fix issues

--- lzy/s3-sink/src/main/java/ai/lzy/kafka/s3sink/SinkServiceImpl.java
+++ lzy/s3-sink/src/main/java/ai/lzy/kafka/s3sink/SinkServiceImpl.java
@@ -11,63 +11,30 @@ import ai.lzy.v1.kafka.KafkaS3Sink.StopResponse;
 import ai.lzy.v1.kafka.S3SinkServiceGrpc;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
-import io.micronaut.core.util.clhm.ConcurrentLinkedHashMap;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
-
-import java.util.Map;
 
 @Singleton
 public class SinkServiceImpl extends S3SinkServiceGrpc.S3SinkServiceImplBase {
     private final JobExecutor executor;
-    private final KafkaHelper helper;
-    private final S3SinkMetrics metrics;
-    private final IdGenerator idGenerator = new RandomIdGenerator();
-    private final Map<String, String> idempotencyKeyToJobId = new ConcurrentLinkedHashMap.Builder<String, String>()
-        .maximumWeightedCapacity(1000)  // LRU cache to not overflow memory
-        .build();
 
-    public SinkServiceImpl(JobExecutor executor, @Named("S3SinkKafkaHelper") KafkaHelper helper,
-                           S3SinkMetrics metrics)
-    {
+    public SinkServiceImpl(JobExecutor executor) {
         this.executor = executor;
-        this.helper = helper;
-        this.metrics = metrics;
     }
 
     @Override
     public void start(StartRequest request, StreamObserver<StartResponse> responseObserver) {
-        var idempotencyKey = IdempotencyUtils.getIdempotencyKey(request);
+        final String jobId;
 
-        if (idempotencyKey != null) {
-            var jobId = idempotencyKeyToJobId.get(idempotencyKey.token());
-
-            if (jobId != null) {
-                responseObserver.onNext(StartResponse.newBuilder()
-                    .setJobId(jobId)
-                    .build());
-                responseObserver.onCompleted();
-                return;
-            }
-        }
-
-        final Job job;
         try {
-            job = new Job(idGenerator.generate("s3sink-"), helper, request, metrics);
+            jobId = executor.submit(request, IdempotencyUtils.getIdempotencyKey(request));
         } catch (StatusRuntimeException e) {
             responseObserver.onError(e);
             return;
         }
 
-
-        executor.submit(job);
-
-        if (idempotencyKey != null) {
-            idempotencyKeyToJobId.put(idempotencyKey.token(), job.id());
-        }
-
         responseObserver.onNext(StartResponse.newBuilder()
-            .setJobId(job.id())
+            .setJobId(jobId)
             .build());
         responseObserver.onCompleted();
     }
