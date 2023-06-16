22799a21b22b7b3b5847d24fb02b0121e4e1f2bc
imakunin <imakunin@yandex-team.ru>
2023-06-16 12:57:09 +0300

    introduce one more logging context key

--- util/util-log/src/main/java/ai/lzy/logs/LogContextKey.java
+++ util/util-log/src/main/java/ai/lzy/logs/LogContextKey.java
@@ -5,6 +5,8 @@ public final class LogContextKey {
     public static final String REQUEST_ID = "rid";
     public static final String EXECUTION_ID = "exec_id";
 
+    public static final String SUBJECT = "subj";
+
     public static final String ACTION = "action";
 
     public static final String OPERATION_ID = "operation_id";
