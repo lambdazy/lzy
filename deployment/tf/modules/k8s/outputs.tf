5972a8de729eacbcc32d04383b87082d4d772d19
atem11 <atem11@yandex-team.ru>
2023-06-19 17:52:16 +0300

    fix output map

--- deployment/tf/modules/k8s/outputs.tf
+++ deployment/tf/modules/k8s/outputs.tf
@@ -1,13 +1,13 @@
 output "db_data" {
   value = tomap({
-    kubernetes_secret.allocator_db_secret.data.db_name = kubernetes_secret.allocator_db_secret.data.password,
-    kubernetes_secret.channel_manager_db_secret.data.db_name = kubernetes_secret.channel_manager_db_secret.data.password,
-    kubernetes_secret.graph_executor_db_secret.data.db_name = kubernetes_secret.graph_executor_db_secret.data.password,
-    kubernetes_secret.iam_db_secret.data.db_name = kubernetes_secret.iam_db_secret.data.password,
-    kubernetes_secret.lzy_service_db_secret.data.db_name = kubernetes_secret.lzy_service_db_secret.data.password,
-    kubernetes_secret.scheduler_db_secret.data.db_name = kubernetes_secret.scheduler_db_secret.data.password,
-    kubernetes_secret.storage_db_secret.data.db_name = kubernetes_secret.storage_db_secret.data.password,
-    kubernetes_secret.whiteboard_db_secret.data.db_name = kubernetes_secret.whiteboard_db_secret.data.password
+    "${kubernetes_secret.allocator_db_secret.data.db_name}" = kubernetes_secret.allocator_db_secret.data.password,
+    "${kubernetes_secret.channel_manager_db_secret.data.db_name}" = kubernetes_secret.channel_manager_db_secret.data.password,
+    "${kubernetes_secret.graph_executor_db_secret.data.db_name}" = kubernetes_secret.graph_executor_db_secret.data.password,
+    "${kubernetes_secret.iam_db_secret.data.db_name}" = kubernetes_secret.iam_db_secret.data.password,
+    "${kubernetes_secret.lzy_service_db_secret.data.db_name}" = kubernetes_secret.lzy_service_db_secret.data.password,
+    "${kubernetes_secret.scheduler_db_secret.data.db_name}" = kubernetes_secret.scheduler_db_secret.data.password,
+    "${kubernetes_secret.storage_db_secret.data.db_name}" = kubernetes_secret.storage_db_secret.data.password,
+    "${kubernetes_secret.whiteboard_db_secret.data.db_name}" = kubernetes_secret.whiteboard_db_secret.data.password
   })
 }
 
