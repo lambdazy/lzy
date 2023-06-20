ee5ba67062a9be35ed583c1809c4ba8a8df7b705
atem11 <atem11@yandex-team.ru>
2023-06-20 13:50:03 +0300

    fix db settings

--- deployment/tf/modules/yc/db.tf
+++ deployment/tf/modules/yc/db.tf
@@ -40,7 +40,6 @@ resource "yandex_mdb_postgresql_database" "lzy_dbs" {
   cluster_id = yandex_mdb_postgresql_cluster.lzy_postgresql_cluster.id
   name       = each.key
   owner      = each.value
-  depends_on = [yandex_mdb_postgresql_user.lzy_users]
 }
 
 resource "yandex_mdb_postgresql_user" "lzy_users" {
@@ -53,4 +52,5 @@ resource "yandex_mdb_postgresql_user" "lzy_users" {
   permission {
     database_name = each.key
   }
+  depends_on = [yandex_mdb_postgresql_database.lzy_dbs]
 }
