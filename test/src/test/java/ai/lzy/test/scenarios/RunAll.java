efc446e6cba6cf28f6676865c6124b8dca85bc4d
ArtoLord <artolord@yandex-team.ru>
2023-06-26 17:25:25 +0200

    WIP

---
+++ test/src/test/java/ai/lzy/test/scenarios/RunAll.java
@@ -0,0 +1,27 @@
+package ai.lzy.test.scenarios;
+
+import ai.lzy.test.ApplicationContextRule;
+import ai.lzy.test.ContextRule;
+import ai.lzy.test.impl.v2.PythonContext;
+import ai.lzy.worker.env.CondaEnvironment;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+public class RunAll {
+    @ClassRule
+    public static final ApplicationContextRule ctx = new ApplicationContextRule();
+
+    @ClassRule
+    public static final ContextRule<PythonContext> pythonContext = new ContextRule<>(ctx, PythonContext.class);
+
+    static {
+        CondaEnvironment.reconfigureConda(false);  // To optimize conda configuration
+    }
+
+    @Test
+    public void testTwoExecutionOneWorkflow() throws InterruptedException {
+        while (true) {
+            Thread.sleep(1000);
+        }
+    }
+}
