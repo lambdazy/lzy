d1fa439317f55dbca6a45167eede5ccab288f5bd
ArtoLord <artolord@yandex-team.ru>
2023-07-03 16:55:56 +0300

    Fix

--- lzy/fs/src/main/java/ai/lzy/fs/fs/LzyScript.java
+++
@@ -1,9 +0,0 @@
-package ai.lzy.fs.fs;
-
-import java.nio.file.Path;
-
-public interface LzyScript {
-    Path location();
-
-    CharSequence scriptText();
-}
--- util/util-test/src/main/java/ai/lzy/test/GrpcUtils.java
+++ util/util-test/src/main/java/ai/lzy/test/GrpcUtils.java
@@ -1,11 +1,7 @@
 package ai.lzy.test;
 
-import ai.lzy.model.DataScheme;
 import ai.lzy.model.utils.FreePortFinder;
 import ai.lzy.util.grpc.GrpcHeaders;
-import ai.lzy.v1.common.LMS;
-import ai.lzy.v1.common.LMST;
-import ai.lzy.v1.portal.LzyPortal;
 import io.grpc.stub.StreamObserver;
 import lombok.Lombok;
 import org.junit.Assert;
@@ -33,66 +29,6 @@ public class GrpcUtils {
         } catch (Exception e) {
             throw Lombok.sneakyThrow(e);
         }
-    }
-
-    public static LMS.Slot makeInputFileSlot(String slotName) {
-        return LMS.Slot.newBuilder()
-            .setName(slotName)
-            .setMedia(LMS.Slot.Media.FILE)
-            .setDirection(LMS.Slot.Direction.INPUT)
-            .setContentType(ai.lzy.model.grpc.ProtoConverter.toProto(DataScheme.PLAIN))
-            .build();
-    }
-
-    public static LMS.Slot makeOutputFileSlot(String slotName) {
-        return LMS.Slot.newBuilder()
-            .setName(slotName)
-            .setMedia(LMS.Slot.Media.FILE)
-            .setDirection(LMS.Slot.Direction.OUTPUT)
-            .setContentType(ai.lzy.model.grpc.ProtoConverter.toProto(DataScheme.PLAIN))
-            .build();
-    }
-
-    public static LMS.Slot makeInputPipeSlot(String slotName) {
-        return LMS.Slot.newBuilder()
-            .setName(slotName)
-            .setMedia(LMS.Slot.Media.PIPE)
-            .setDirection(LMS.Slot.Direction.INPUT)
-            .setContentType(ai.lzy.model.grpc.ProtoConverter.toProto(DataScheme.PLAIN))
-            .build();
-    }
-
-    public static LMS.Slot makeOutputPipeSlot(String slotName) {
-        return LMS.Slot.newBuilder()
-            .setName(slotName)
-            .setMedia(LMS.Slot.Media.PIPE)
-            .setDirection(LMS.Slot.Direction.OUTPUT)
-            .setContentType(ai.lzy.model.grpc.ProtoConverter.toProto(DataScheme.PLAIN))
-            .build();
-    }
-
-    public static LzyPortal.PortalSlotDesc.Snapshot makeAmazonSnapshot(String key, String bucket, String endpoint) {
-        return LzyPortal.PortalSlotDesc.Snapshot.newBuilder()
-            .setStorageConfig(LMST.StorageConfig.newBuilder()
-                .setUri("s3://" + bucket + "/" + key)
-                .setS3(LMST.S3Credentials.newBuilder()
-                    .setAccessToken("")
-                    .setSecretToken("")
-                    .setEndpoint(endpoint)
-                    .build()))
-            .build();
-    }
-
-    public static LzyPortal.PortalSlotDesc.StdOut makeStdoutStorage(String taskId) {
-        return LzyPortal.PortalSlotDesc.StdOut.newBuilder()
-            .setTaskId(taskId)
-            .build();
-    }
-
-    public static LzyPortal.PortalSlotDesc.StdErr makeStderrStorage(String taskId) {
-        return LzyPortal.PortalSlotDesc.StdErr.newBuilder()
-            .setTaskId(taskId)
-            .build();
     }
 
     public static int rollPort() {
