c5eb2734ee90abfea3300a36b073c3549b10470d
ArtoLord <artolord@yandex-team.ru>
2023-08-16 10:34:29 +0300

    Fix checkstyle

--- lzy/worker/src/main/java/ai/lzy/worker/EnvironmentFactory.java
+++ lzy/worker/src/main/java/ai/lzy/worker/EnvironmentFactory.java
@@ -29,7 +29,6 @@ import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
