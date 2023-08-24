a5934c1d3ee0ec46d86354e8748a74f2de3a2020
Vladimir Lipkin <lipkin@yandex-team.ru>
2023-08-24 19:43:26 +0300

    Futher tries

--- pylzy/scripts/gen_proto.sh
+++ pylzy/scripts/gen_proto.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -eux
+set -eu
+set -o pipefail
 
 src_dir="$(dirname $0)"
 source "$src_dir/util.sh"
 
-# this has to be declared as env variable in mk-python-env.nix
-proto_out="lzy/proto"
+proto_out="$(dirname $src_dir)/ai"
 proto_validation_path="../util/util-grpc/src/main/proto/"
 proto_model_path="../model/src/main/proto/"
 proto_longrunning_path="../long-running-api/src/main/proto/"
@@ -15,21 +15,15 @@ proto_whiteboard_path="../whiteboard-api/src/main/proto/"
 
 [ -d "$proto_out" ] || mkdir -p "$proto_out"
 
-pip install -r proto_requirements.txt
-
-# check mypy, it's hack actually but for some reason it's not installed
-# ok with nix
-python -m mypy_protobuf 1>/dev/null 2>&1\
-    || pip install mypy-protobuf
+pip install -r proto_requirements.txt -q
 
 print_green "Generating protobuf, grpclib and mypy proto stubs"
-print_green "$PWD"
 
 cd "$proto_validation_path"
 find . -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . \
                     --python_out="$OLDPWD" \
-                    --mypy_out="$OLDPWD" \
+                    --mypy_out=quiet:"$OLDPWD" \
                     --grpc_python_out="$OLDPWD" \
                     --proto_path="." \
                     '{}' +
@@ -39,17 +33,17 @@ cd "$proto_model_path"
 find . -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . -I "$OLDPWD/$proto_validation_path" \
                     --python_out="$OLDPWD" \
-                    --mypy_out="$OLDPWD" \
+                    --mypy_out=quiet:"$OLDPWD" \
                     --grpc_python_out="$OLDPWD" \
                     --proto_path="." \
                     '{}' +
 cd "$OLDPWD"
 
 cd "$proto_longrunning_path"
-find . -iname "*.proto" -type f \
+find "ai" -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . \
                     --python_out="$OLDPWD" \
-                    --mypy_out="$OLDPWD" \
+                    --mypy_out=quiet:"$OLDPWD" \
                     --grpc_python_out="$OLDPWD" \
                     --proto_path="." \
                     '{}' +
@@ -60,7 +54,7 @@ find . -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . -I "$OLDPWD/$proto_longrunning_path" -I "$OLDPWD/$proto_model_path" \
        -I "$OLDPWD/$proto_validation_path" \
                     --python_out="$OLDPWD" \
-                    --mypy_out="$OLDPWD" \
+                    --mypy_out=quiet:"$OLDPWD" \
                     --grpc_python_out="$OLDPWD" \
                     --proto_path="." \
                     '{}' +
@@ -70,15 +64,17 @@ cd "$proto_whiteboard_path"
 find . -iname "*.proto" -type f \
        -exec python -m grpc_tools.protoc -I . -I "$OLDPWD/$proto_model_path" -I "$OLDPWD/$proto_validation_path" \
                     --python_out="$OLDPWD" \
-                    --mypy_out="$OLDPWD" \
+                    --mypy_out=quiet:"$OLDPWD" \
                     --grpc_python_out="$OLDPWD" \
                     --proto_path="." \
                     '{}' +
-cd "$OLDPWD"
 
-find "ai" -type d \
-        -exec touch '{}/__init__.py' \;
+cd "$OLDPWD"
 
 print_green "Generated next proto stubs:"
 println "$proto_out"
 find "$proto_out" -type f \( -iname "*.py" -o -iname "*.pyi" \) -print
+
+find "$proto_out" -type d -not -name __pycache__ -not -name ai \
+        -exec touch '{}/__init__.py' \;
+
