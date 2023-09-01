bf96df2f5c42805fe7b927198fd513b2f41c50f8
Vladimir Lipkin <lipkin@yandex-team.ru>
2023-09-01 19:24:18 +0300

    Fix test_types

--- pylzy/tests/conftest.py
+++ pylzy/tests/conftest.py
@@ -2,7 +2,7 @@ import sys
 import json
 import pathlib
 
-from typing import List
+from typing import List, Tulpe
 
 import pytest
 import google.protobuf.json_format
@@ -66,11 +66,11 @@ def with_test_modules(get_test_data_path, monkeypatch):
         yield
 
 
-# scope='class' because we use it via @pytest.mark.usefixtures('vm_pool_specs')
+# scope='class' because we use it via @pytest.mark.usefixtures('vm_proto_specs')
 # on TestCase classes and somewhy it doesn't work with planned scope='session'
-@pytest.fixture(scope='class')
-def vm_pool_specs(request, get_test_data_path):
-    path = get_test_data_path('vm_pool_specs.json')
+@pytest.fixture(scope='session')
+def vm_proto_specs(request, get_test_data_path) -> Tuple[VmPoolSpec]:
+    path = get_test_data_path('vm_proto_specs.json')
 
     with path.open('r') as f_:
         dict_data = json.load(f_)
@@ -80,54 +80,40 @@ def vm_pool_specs(request, get_test_data_path):
         for _ in dict_data
     )
 
-    return set_unittest_fixture(request, data)
+    return data
 
 
-@pytest.fixture(scope='class')
-def vm_specs(vm_pool_specs):
-    return tuple(VmSpec.from_proto(p) for p in vm_pool_specs)
+@pytest.fixture(scope='session')
+def vm_specs(vm_proto_specs) -> Tuple[VmSpec]:
+    return tuple(VmSpec.from_proto(p) for p in vm_proto_specs)
 
 
-@pytest.fixture(scope='class')
-def vm_pool_spec_large(request, vm_pool_specs):
-    result = None
-    for spec in vm_pool_specs:
+@pytest.fixture(scope='session')
+def vm_proto_spec_large(vm_proto_specs) -> VmPoolSpec:
+    for spec in vm_proto_specs:
         if spec.poolSpecName == 'large':
-            result = spec
-            break
-
-    return set_unittest_fixture(request, result)
-
-
-@pytest.fixture(scope='class')
-def vm_spec_large(request, vm_pool_specs):
-    result = None
-    for spec in vm_pool_specs:
-        if spec.poolSpecName == 'large':
-            return VmSpec.from_proto(spec)
+            return spec
 
     assert False, "unreachable"
 
 
-@pytest.fixture(scope='class')
-def vm_pool_spec_small(request, vm_pool_specs):
-    result = None
-    for spec in vm_pool_specs:
+@pytest.fixture(scope='session')
+def vm_spec_large(vm_proto_spec_large) -> VmSpec:
+    return VmSpec.from_proto(vm_proto_spec_large)
+
+
+@pytest.fixture(scope='session')
+def vm_proto_spec_small(vm_proto_specs) -> VmPoolSpec:
+    for spec in vm_proto_specs:
         if spec.poolSpecName == 'small':
-            result = spec
-            break
-
-    return set_unittest_fixture(request, result)
-
-
-@pytest.fixture(scope='class')
-def vm_spec_small(request, vm_pool_specs):
-    result = None
-    for spec in vm_pool_specs:
-        if spec.poolSpecName == 'small':
-            return VmSpec.from_proto(spec)
+            return spec
 
     assert False, "unreachable"
+
+
+@pytest.fixture(scope='session')
+def vm_spec_small(vm_proto_spec_small) -> VmSpec:
+    return VmSpec.from_proto(vm_proto_spec_small)
 
 
 @pytest.fixture(scope='session')
